e0b55ba39548fe50657c4f4709cd4a6ece4906a6
EVOFORGE\dimay <dimay@evoforge.org>
2019-02-19 13:56:07 -0500

    #8: styling changes

--- frontend/src/components/projects/ProjectSettings.vue
+++ frontend/src/components/projects/ProjectSettings.vue
@@ -1,32 +1,37 @@
 <template>
   <div>
-    <div class="columns skills-underline-container">
+    <div class="columns">
       <div class="column">
         <span class="title is-3">Settings</span>
       </div>
-      <div class="column has-text-right">
-        <a class="button is-outlined is-info" v-on:click="save"
-           :disabled="errors.any()">
-          <span>Save</span>
-          <span class="icon is-small">
-            <i class="fas fa-arrow-circle-right"/>
-            </span>
-        </a>
-      </div>
     </div>
-    <b-loading :is-full-page="false" :active.sync="isLoading" :can-cancel="false"></b-loading>
-    <div class="columns">
-      <div class="column is-full">
-         <span>
-           <b-tooltip label="Change to true to calculate levels based on explicit point values instead of percentages."
-                      position="is-top" size="is-small" animanted="true" type="is-light" multilined>
-          <span><i class="fas fa-question-circle"></i></span>
-          </b-tooltip>
-          Use Points For Levels:
-          <b-switch v-model="levelPointsSetting.value">
-            {{ levelPointsSetting.value }}
-          </b-switch>
-        </span>
+    <div class="skills-bordered-component">
+      <b-loading :is-full-page="false" :active.sync="isLoading" :can-cancel="false"></b-loading>
+      <div class="columns">
+        <div class="column is-full">
+           <span>
+             <b-tooltip label="Change to true to calculate levels based on explicit point values instead of percentages."
+                        position="is-top" size="is-small" animanted="true" type="is-light" multilined>
+            <span><i class="fas fa-question-circle"></i></span>
+            </b-tooltip>
+            Use Points For Levels:
+            <b-switch v-model="levelPointsSetting.value">
+              {{ levelPointsSetting.value }}
+            </b-switch>
+          </span>
+        </div>
+      </div>
+
+      <div class="columns skills-pad-top-1-rem">
+        <div class="column">
+          <a class="button is-outlined is-info" v-on:click="save"
+             :disabled="errors.any()">
+            <span>Save</span>
+            <span class="icon is-small">
+              <i class="fas fa-arrow-circle-right"/>
+              </span>
+          </a>
+        </div>
       </div>
     </div>
   </div>
