cefc8260b080806dd90104622f2c71f5937fce4c
EVOFORGE\dimay <dimay@evoforge.org>
2019-03-18 10:27:14 -0400

    #32: various look and feel improvements

--- client-display/generateUserToken.sh
+++ client-display/generateUserToken.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PROJECT_NAME=MyProject
-PROJECT_SECRET=U78afU3HSYa061V7Pnt0n7Ur4x4GU1sK
-PROXY_USER=m@m.com
+PROJECT_NAME=test1
+PROJECT_SECRET=9D7GRQNBXVa4KjQ7clwj3f6290M4Vt91
+PROXY_USER=user1
 
 COMMAND="curl $PROJECT_NAME:$PROJECT_SECRET@localhost:8080/oauth/token -d \"grant_type=client_credentials&proxy_user=$PROXY_USER\" | jq"
 
