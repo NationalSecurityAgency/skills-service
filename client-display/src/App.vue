e3132034eac18740383c2a8e740ca5614f254902
Matthew Kirkley <mtkirkl@evoforge.org>
2019-03-21 12:00:39 -0400

    #21 fixing eslint

--- client-display/src/App.vue
+++ client-display/src/App.vue
@@ -16,15 +16,7 @@
   import 'bootstrap/dist/css/bootstrap.css';
 
   const getDocumentHeight = () => {
-    const { body } = document;
     const html = document.documentElement;
-
-    console.log('body.scrollHeight ',body.scrollHeight);
-    console.log('body.offsetHeight ',body.offsetHeight);
-    console.log('html.clientHeight ',html.clientHeight);
-    console.log('*** html.scrollHeight ',html.scrollHeight);
-    console.log('html.scrollHeight ',html.offsetHeight);
-
     return html.offsetHeight;
   };
 
@@ -38,7 +30,7 @@
         serviceUrl: 'http://localhost:8080',
         projectId: 'MyProject',
         // eslint-disable-next-line max-len
-        token: null,//'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsic2tpbGxzLXNlcnZpY2Utb2F1dGgiXSwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sInByb3h5X3VzZXIiOiJtQG0uY29tIiwiZXhwIjoxNTUzMTQ1NTE3LCJhdXRob3JpdGllcyI6WyJST0xFX1RSVVNURURfQ0xJRU5UIl0sImp0aSI6ImIyNzBlYWI3LTIxMGMtNGFmOS1iZmU5LTIwZjk5MTU0ZTg2ZCIsImNsaWVudF9pZCI6Ik15UHJvamVjdCJ9.CWj7dcWGKFzy5qn8J8mxGbY3lUS05t-SWc9KdcjDSmyTm2MxlR9e6lwLwFsuvKHoY5Lz0orkXMcStu6ojCaDClg4DhgZoD1S4SdQVvHDN_84XMp5ppVLPBmadbc_hzW9p9Hz7iSNuQlotC2jNmzeRv5GZuYTyecnviGp-UusEPRJHE7S66ALifv-ogSMfbb-CX9gKkLEeV-D64YE6Ku7rcXRqC1eaw7ICyEvfTCh0M6yvvTdRr6fZElS1d3AY_QrpvRx0180eRrxWZVCNp82fgNjLx8qbHZV1wKWaqzF6Oe5dGhDHEPCcWiQCq52uhbJQUGY1IklOrStr2_s3OHDpg',
+        token: null,
       };
     },
     mounted() {
@@ -58,7 +50,6 @@
         const payload = {
           contentHeight: getDocumentHeight(),
         };
-        console.log('sending contentHeight', payload.contentHeight);
         window.parent.postMessage(`skills::frame-loaded::${JSON.stringify(payload)}`, '*');
       },
     },
