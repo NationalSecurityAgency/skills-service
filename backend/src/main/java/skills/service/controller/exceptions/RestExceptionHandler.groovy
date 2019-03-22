33a306bab1d5aed6f96c3f3f8964b9505855412e
Ryan Mayo <rmmayo@evoforge.org>
2019-03-22 12:15:20 -0400

    #33 - added global exception handling with support for ResponseStatusException and @ResponseStatus

--- backend/src/main/java/skills/service/controller/exceptions/RestExceptionHandler.groovy
+++ backend/src/main/java/skills/service/controller/exceptions/RestExceptionHandler.groovy
@@ -2,22 +2,25 @@ package skills.service.controller.exceptions
 
 import groovy.util.logging.Slf4j
 import org.hibernate.exception.ConstraintViolationException
+import org.springframework.core.annotation.AnnotatedElementUtils
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.context.request.WebRequest
+import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
 @Slf4j
-class RestExceptionHandler extends ResponseEntityExceptionHandler{
+class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     static class BasicErrBody {
         String message
-        String errorCode
+        String errorCode = ErrorCode.InternalError
     }
 
     static class DomainSpecificErrBody extends BasicErrBody {
@@ -26,9 +29,9 @@ class RestExceptionHandler extends ResponseEntityExceptionHandler{
     }
 
     @ExceptionHandler(SkillException)
-    protected ResponseEntity<Object> handleSkillException(SkillException ex, WebRequest webRequest){
+    protected ResponseEntity<Object> handleSkillException(Exception ex, WebRequest webRequest) {
         Object body
-        if(ex instanceof SkillException){
+        if (ex instanceof SkillException) {
             body = new DomainSpecificErrBody(projectId: ex.projectId, skillId: ex.skillId, message: ex.message, errorCode: ex.errorCode.name())
             log.error("Exception for: projectId=[${ex.projectId}], skillId=${ex.skillId}", ex)
         } else {
@@ -43,6 +46,37 @@ class RestExceptionHandler extends ResponseEntityExceptionHandler{
         String msg = "Data Integrity Violation"
         BasicErrBody body = new BasicErrBody(message: msg, errorCode: ErrorCode.ConstraintViolation)
         return handleExceptionInternal(violationException, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest)
+    }
+
+    /**
+     * Supports the ResponseStatusException introduced in Spring 5
+     */
+    @ExceptionHandler(ResponseStatusException)
+    protected ResponseEntity<Object> handleResponseStatusException(ResponseStatusException ex, WebRequest webRequest) {
+        BasicErrBody body = new BasicErrBody(message: ex.message)
+        return handleExceptionInternal(ex, body, new HttpHeaders(), ex.status, webRequest)
+    }
+
+    /**
+     * Fallback handler - this will any Exception that does have a more specific handler method
+     * defined above.  This method will also support Exceptions that have the @ResponseStatus
+     * annotation.
+     */
+    @ExceptionHandler([Throwable, Error])
+    protected ResponseEntity<Object> handleOtherExceptions(Exception ex, WebRequest webRequest) {
+        BasicErrBody body
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR
+        // check if the exception is annotated w/ @ResponseStatus
+        ResponseStatus responseStatus = AnnotatedElementUtils.findMergedAnnotation(ex.getClass(), ResponseStatus.class)
+        if (responseStatus) {
+            httpStatus = responseStatus.code()
+            String reason = responseStatus.reason()
+            String message = "${httpStatus}${(reason ? " \"${reason}\"" : "")}"
+            body = new BasicErrBody(message: message)
+        } else {
+            body = new BasicErrBody(message: 'Unexpected Exception')
+        }
+        return handleExceptionInternal(ex, body, new HttpHeaders(), httpStatus, webRequest)
     }
 
 }
