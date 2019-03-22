4df0f6759ce57e70ba77f49c57cd17bee5c17f3e
EVOFORGE\dimay <dimay@evoforge.org>
2019-03-22 13:59:45 -0400

    #45: display a lock over skills that have unfulfilled dependencies

--- client-display/src/common/progress/VerticalProgress.vue
+++ client-display/src/common/progress/VerticalProgress.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="user-skill-progress-layers">
+    <div v-if="isLocked" class="locked-border"></div>
+    <span v-if="isLocked" class="locked-icon">
+      <i class="fas fa-lock"></i>
+    </span>
     <progress-bar
       :val="totalProgress"
       :size="barSize"
       :bar-color="totalProgressBeforeToday ? totalProgressBarColor : beforeTodayBarColor"
+      :bg-color="isLocked ? lightgrey : 'E6E6E6'"
       class="complete-total"/>
     <progress-bar
       v-if="totalProgress !== 100"
@@ -23,6 +28,7 @@
       ProgressBar,
     },
     props: {
+      isLocked: Boolean,
       totalProgress: Number,
       totalProgressBeforeToday: Number,
       beforeTodayBarColor: {
@@ -51,5 +57,20 @@
 
   .user-skill-progress-layers {
     position: relative;
+    background-color: lightgrey;
   }
+
+  .locked-border {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: #E6E6E6 solid 2px;
+  }
+
+  .locked-icon {
+      position: absolute;
+      top: 2px;
+      color: #5d5d5d;
+  }
+
 </style>
