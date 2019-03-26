ebd5b1a0eb58ac1ca48ab41959636cdafe0d9ecc
Matthew Kirkley <mtkirkl@evoforge.org>
2019-03-26 07:35:05 -0400

    WIP

--- client-display/src/App.vue
+++ client-display/src/App.vue
@@ -1,58 +1,13 @@
 <template>
   <div id="app">
-    <user-skills
-      v-if="token"
-      :service-url="serviceUrl"
-      :project-id="projectId"
-      :token="token"
-      @height-change="onHeightChange"/>
+    <transition name="slide">
+      <router-view />
+    </transition>
   </div>
 </template>
 
 <script>
-  import UserSkills from '@/userSkills/UserSkills.vue';
-
-  import '@fortawesome/fontawesome-free/css/all.css';
-  import 'bootstrap/dist/css/bootstrap.css';
-
-  const getDocumentHeight = () => {
-    const html = document.documentElement;
-    return html.offsetHeight;
-  };
-
   export default {
     name: 'app',
-    components: {
-      UserSkills,
-    },
-    data() {
-      return {
-        serviceUrl: 'http://localhost:8080',
-        projectId: 'MyProject',
-        // eslint-disable-next-line max-len
-        token: null,
-      };
-    },
-    mounted() {
-      this.onHeightChange();
-      window.addEventListener('message', (event) => {
-        const eventData = event.data && event.data.split ? event.data.split('::') : [];
-        if (eventData.length === 3 && eventData[0] === 'skills' && eventData[1] === 'data-init') {
-          const payload = JSON.parse(eventData[2]);
-          this.serviceUrl = payload.serviceUrl;
-          this.projectId = payload.projectId;
-          this.token = payload.authToken;
-        }
-      });
-    },
-    methods: {
-      onHeightChange() {
-        const payload = {
-          contentHeight: getDocumentHeight(),
-        };
-        window.parent.postMessage(`skills::frame-loaded::${JSON.stringify(payload)}`, '*');
-      },
-    },
   };
 </script>
-
---
+++ client-display/src/userSkills/subject/SubjectDetails.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+
+    HELLO!?!?!?
+    <!--<div-->
+      <!--v-if="subject && initialized"-->
+      <!--class="user-skill-subject-body">-->
+      <!--<div class="user-skill-subject-overall">-->
+        <!--<user-skills-header-->
+          <!--:user-skills="subject" />-->
+      <!--</div>-->
+
+      <!--<div>-->
+        <!--<p-->
+          <!--v-if="showDescriptions && subject.description"-->
+          <!--class="user-skill-subject-description"-->
+          <!--v-html="parseMarkdown(subject.description)"/>-->
+      <!--</div>-->
+
+      <!--<skills-progress-list-->
+        <!--:subject="subject"-->
+        <!--:show-descriptions="showDescriptions" />-->
+    <!--</div>-->
+  </div>
+</template>
+
+<script>
+  import UserSkillsHeader from '@/userSkills/UserSkillsHeader.vue';
+  import Ribbon from '@/common/ribbon/Ribbon.vue';
+  import SkillsProgressList from '@/userSkills/modal/SkillsProgressList.vue';
+
+  import marked from 'marked';
+
+  import ToggleButton from 'vue-js-toggle-button/src/Button.vue';
+
+  export default {
+    components: {
+      UserSkillsHeader,
+      ToggleButton,
+      Ribbon,
+      SkillsProgressList,
+    },
+    props: {
+      starArray: Array,
+      ribbonColor: String,
+      subject: Object,
+    },
+    data() {
+      return {
+        initialized: false,
+        showDescriptions: false,
+      };
+    },
+    mounted() {
+      this.showDescriptions = false;
+      this.initialized = true;
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
