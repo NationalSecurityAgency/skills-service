5d7ad8534a3835dd80930c61332b7801f351b1ac
Matthew Kirkley <mtkirkl@evoforge.org>
2019-03-26 10:27:43 -0400

    Further proxy config updates

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,16 +2,28 @@ const path = require('path');
 
 const resolve = dir => path.join(__dirname, dir);
 
+const proxyConf = {
+  target: 'http://localhost:8080',
+  changeOrigin: true,
+};
+
 module.exports = {
   devServer: {
     host: 'localhost',
     port: 8082,
     overlay: true,
     proxy: {
-      '/': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
+      '/admin': proxyConf,
+      '/app': proxyConf,
+      '/api': proxyConf,
+      '/server': proxyConf,
+      '/icons': proxyConf,
+      '/performLogin': proxyConf,
+      '/logout': proxyConf,
+      '/createAccount': proxyConf,
+      '/oauth2': proxyConf,
+      '/login': proxyConf,
+      '/static': proxyConf,
     },
   },
 
