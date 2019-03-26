381f167381a60bfd3e05b649bf12c38dc3a70911
Matthew Kirkley <mtkirkl@evoforge.org>
2019-03-26 08:45:29 -0400

    Fixed inconsistent line spacing.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,27 +3,27 @@ const path = require('path');
 const resolve = dir => path.join(__dirname, dir);
 
 module.exports = {
-    devServer: {
-		host: 'localhost',
-		port: 8082,
-		overlay: true,
-		proxy: 'http://localhost:8080',
-	},
+  devServer: {
+    host: 'localhost',
+    port: 8082,
+    overlay: true,
+    proxy: 'http://localhost:8080',
+  },
 
-    configureWebpack: {
-	resolve: {
-		alias: {
-			'@$': resolve('src'),
-		},
-	},
-	devtool: 'cheap-module-eval-source-map',
-	},
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@$': resolve('src'),
+      },
+    },
+    devtool: 'cheap-module-eval-source-map',
+  },
 
-    publicPath: undefined,
-    outputDir: undefined,
-    assetsDir: undefined,
-    runtimeCompiler: true,
-    productionSourceMap: undefined,
-    parallel: undefined,
-    css: undefined,
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: true,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined,
 };
