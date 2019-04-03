bda14c586d9c5c3e576c9dd92b30a3fbab1ac458
Matthew Kirkley <mtkirkl@evoforge.org>
2019-04-03 12:58:35 -0400

    Revert "#21  rm src/common/modal/ModalHeader.vue src/common/modal/ModalFooter.vue src/common/modal/Modal.vue"
    This reverts commit debdd2f53d7e214c34f103d68e4c1ddf8fba1aa1.

---
+++ client-display/src/common/modal/ModalHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <span
+      type="button"
+      class="modal-close-x"
+      @click="emitButtonClick('cancel')">&times;</span>
+    <i
+      class="modal-header-icon"
+      :class="iconClass"/>
+    <span class="modal-title">{{ title }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      iconClass: String,
+      title: String,
+      hrefList: Array,
+    },
+    methods: {
+      emitButtonClick(type) {
+        this.$emit(type);
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .modal-title {
+    font-family: inherit;
+    font-size: 21px;
+    line-height: 1.1;
+    vertical-align: middle;
+    color: #698dad;
+  }
+
+  .modal-header-icon {
+    font-size: 40px;
+    color: #d0d9e8;
+    vertical-align: middle;
+  }
+
+  .modal-close-x {
+    padding-left: 1rem;
+    margin-top: -2px;
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 1;
+    float: right;
+    opacity: 0.2;
+    color: #000;
+    text-shadow: 0 1px 0 #fff;
+    cursor: pointer;
+  }
+</style>
