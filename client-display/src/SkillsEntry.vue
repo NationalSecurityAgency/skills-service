15b0ccf1c0cda608c8592dd291d94a22b698b2ee
Matthew Kirkley <mtkirkl@evoforge.org>
2019-04-03 10:03:52 -0400

    #21 accidentally commited token.

--- client-display/src/SkillsEntry.vue
+++ client-display/src/SkillsEntry.vue
@@ -52,7 +52,7 @@
         serviceUrl: 'http://localhost:8080',
         projectId: 'movies',
         // eslint-disable-next-line max-len
-        token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsic2tpbGxzLXNlcnZpY2Utb2F1dGgiXSwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sInByb3h5X3VzZXIiOiJtdGtpcmtsQGV2b2ZvcmdlLm9yZyIsImV4cCI6MTU1NDI1MzM0NiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9UUlVTVEVEX0NMSUVOVCJdLCJqdGkiOiJmMDRlNmFhNy1mMzM4LTQzYmUtYTkwMS1mZmNiMzkyMWY3YTciLCJjbGllbnRfaWQiOiJtb3ZpZXMifQ.KalXZeXlogCrHS40MXbcgoraUz8mb57HKiv65Z5uRYBzvmpDSrqJh4BqHXjggiqyxefR6xPeFdcWBVoxBTMsFc6Br3sLWVYq4dH_eM0wzLZId-k6g_PX5GuWnjtdj__26XT7xq2Dc4XlQ3cReS6M_94uq7Q1YL6E-Xkf5wWbwA-mfFCMYA_VNGtjvh9TZmEmn-_pvFWVl7gDpdR6QlCbPYmCo981c7nPypiKIjcVdCWGXH3r0q5ElmdlM4NnHthObFIuqucYhQ0fdVronemM934d2lbj_L89tqkInyIO_qrMXq-ezPIV4Vp991J_XuW8DRJUAbqT1DGDm87a_NBBBw',
+        token: null,
       };
     },
     mounted() {
