dedf8dc8a2dcaa4aff1a715f2b4873890ae84d94
EVOFORGE\dimay <dimay@evoforge.org>
2019-04-05 12:53:24 -0400

    #52: removed tmp bootstrap 4 inclusion; started using border css helper class

--- client-display/src/SkillsEntry.vue
+++ client-display/src/SkillsEntry.vue
@@ -49,7 +49,7 @@
         serviceUrl: 'http://localhost:8080',
         projectId: 'movies',
         // eslint-disable-next-line max-len
-        token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsic2tpbGxzLXNlcnZpY2Utb2F1dGgiXSwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sInByb3h5X3VzZXIiOiJtQG0uY29tIiwiZXhwIjoxNTU0NTEwNjgxLCJhdXRob3JpdGllcyI6WyJST0xFX1RSVVNURURfQ0xJRU5UIl0sImp0aSI6ImM3NmI3ZjNiLTMxYjMtNGU4NS1iMDM0LTZiZTVhNzkwZWQ3NiIsImNsaWVudF9pZCI6Im1vdmllcyJ9.XpfbmfWwhVEP5qXivTW8DeZFsiWHXdz9xzSKPiH6z9AXsKDba3O-jZSiUgkPzn3b58pbOQyKlE4dr_w7qNwzQtjVT_tnuA7QYY3m4GZdMLi-M0pRGET8CO_J-OTHO0pewWXzdxaixhCVEbaQayAHKbOKwA-UFt_YaPtWUlzFXKH4YSAk0TYRRCX_xoq1WtkhQu8wk9db4lBT5te3Or8YJDRFFqXZiRhoyTLonoSE0THZ26QiDAo6GNDIM5Z6wypuFi5st7jXOyAtjkgXEUeXUiR2N7bzkDg0L6iaGapbjmM3fHzpVIEjp6MUlKVOiAZRcEjfqEjthfEYEfwdmfLg9A',
+        token: null,
       };
     },
     mounted() {
