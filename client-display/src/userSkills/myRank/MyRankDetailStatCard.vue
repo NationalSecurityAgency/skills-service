4c972b99b512df6bd4d87ce8e3106312cc5cb68c
Matthew Kirkley <mtkirkl@evoforge.org>
2019-04-15 10:14:29 -0400

    #53 stat card

---
+++ client-display/src/userSkills/myRank/MyRankDetailStatCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class='card position-relative'>
+      <div class='card-body'>
+        <i :class="iconClass" />
+        <div>
+          <h1 class='distribution-icon-text'>{{ value | number }}</h1>
+          <h4>{{ label }}</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      iconClass: String,
+      value: Number,
+      label: String,
+    },
+  };
+</script>
+
+<style scoped>
+  .card .card-body i {
+    color: #0fcc15d1;
+    opacity: 0.18;
+    font-size: 6rem;
+    position: absolute;
+    left: 0;
+    top: 50%;
+    right: 0;
+    margin: auto;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+</style>
