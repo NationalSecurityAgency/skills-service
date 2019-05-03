6de907f94c94cad638b4b79d98e249bf81af6475
Ryan Mayo <rmmayo@evoforge.org>
2019-05-02 20:24:41 -0400

    #81 - moved all methods from UserSkillsAdminController into UserSkillsController except for addSkill, and added an optional userId method.  Methods that take the userId method will be authorized for ROLE_PROJECT_ADMIN role using AOP.  Needed to put addSkills methods into their own separate controllers (UserSkillsAdminController and UserSkillsApiController) to avoid ambiguous naming conflict with both /api and /admin versions.

--- backend/src/main/java/skills/service/controller/UserSkillsAdminController.groovy
+++ backend/src/main/java/skills/service/controller/UserSkillsAdminController.groovy
@@ -5,13 +5,12 @@ import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import skills.service.auth.UserInfoService
+import skills.service.auth.aop.AdminUsersOnlyWhenUserIdSupplied
 import skills.service.controller.exceptions.SkillException
 import skills.service.controller.request.model.AddSkillRequest
 import skills.service.skillLoading.RankingLoader
 import skills.service.skillLoading.SkillsLoader
-import skills.service.skillLoading.model.*
 import skills.service.skillsManagement.SkillsManagementFacade
-import skills.utils.Constants
 
 @RestController
 @RequestMapping("/admin")
@@ -31,100 +30,14 @@ class UserSkillsAdminController {
     @Autowired
     SkillsManagementFacade skillsManagementFacade
 
-
-    @RequestMapping(value = "/projects/{projectId}/level", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    Integer getUserLevel(@PathVariable(name = "projectId") String projectId, @RequestParam(name = "userId") String userId) {
-        return skillsLoader.getUserLevel(projectId, userId)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/summary", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    OverallSkillSummary getSkillsSummary(@PathVariable("projectId") String projectId, @RequestParam(name = "userId") String userId, @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadOverallSummary(projectId, userId, version)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/subjects/{subjectId}/summary", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    SkillSubjectSummary getSubjectsSkillsSummary(@PathVariable("projectId") String projectId, @PathVariable("subjectId") String subjectId, @RequestParam(name = "userId") String userId, @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadSubject(projectId, userId, subjectId, version)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/skills/{skillId}/summary", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    SkillSummary getSkillSummary(@PathVariable("projectId") String projectId, @PathVariable("skillId") String skillId, @RequestParam(name = "userId") String userId, @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadSkillSummary(projectId, userId, null, skillId, version)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/projects/{crossProjectId}/skills/{skillId}/summary", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    SkillSummary getCrossProjectSkillSummary(@PathVariable("projectId") String projectId,
-                                             @PathVariable("crossProjectId") String crossProjectId,
-                                             @PathVariable("skillId") String skillId,
-                                             @RequestParam(name = "userId") String userId,
-                                             @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadSkillSummary(projectId, userId, crossProjectId, skillId, version)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/pointHistory", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    UserPointHistorySummary getProjectsPointHistory(@PathVariable("projectId") String projectId, @RequestParam(name = "userId") String userId) {
-        return skillsLoader.loadPointHistorySummary(projectId, userId, 365)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/subjects/{subjectId}/pointHistory", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    UserPointHistorySummary getSubjectsPointHistory(@PathVariable("projectId") String projectId, @PathVariable("subjectId") String subjectId, @RequestParam(name = "userId") String userId) {
-        return skillsLoader.loadPointHistorySummary(projectId, userId, 365, subjectId)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/skills/{skillId}/dependencies", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    SkillDependencyInfo loadSkillDependencyInfo(@PathVariable("projectId") String projectId, @PathVariable("skillId") String skillId, @RequestParam(name = "userId") String userId) {
-        return skillsLoader.loadSkillDependencyInfo(projectId, userId, skillId)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/rank", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    SkillsRanking getRanking(@PathVariable("projectId") String projectId, @RequestParam(name = "userId") String userId) {
-        return rankingLoader.getUserSkillsRanking(projectId, userId)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/subjects/{subjectId}/rank", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    SkillsRanking getRankingBySubject(@PathVariable("projectId") String projectId, @PathVariable("subjectId") String subjectId, @RequestParam(name = "userId") String userId) {
-        return rankingLoader.getUserSkillsRanking(projectId, userId, subjectId)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/rankDistribution", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    SkillsRankingDistribution getRankingDistribution(@PathVariable("projectId") String projectId, @RequestParam(name = "userId") String userId) {
-        return rankingLoader.getRankingDistribution(projectId, userId)
-    }
-
-    @RequestMapping(value = "/projects/{projectId}/subjects/{subjectId}/rankDistribution", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    SkillsRankingDistribution getRankingDistributionBySubject(@PathVariable("projectId") String projectId, @PathVariable("subjectId") String subjectId, @RequestParam(name = "userId") String userId) {
-        return rankingLoader.getRankingDistribution(projectId, userId, subjectId)
-    }
-
     @RequestMapping(value = "/projects/{projectId}/skills/{skillId}", method = [RequestMethod.PUT, RequestMethod.POST], produces = "application/json")
     @ResponseBody
     @CompileStatic
+    @AdminUsersOnlyWhenUserIdSupplied
     SkillsManagementFacade.AddSkillResult addSkill(
             @PathVariable("projectId") String projectId,
             @PathVariable("skillId") String skillId,
+            @RequestParam(name = "userId", required = false) String userIdss,
             @RequestBody AddSkillRequest skillRequest) {
         try {
             assert skillRequest?.userId
--- backend/src/main/java/skills/service/controller/UserSkillsController.groovy
+++ backend/src/main/java/skills/service/controller/UserSkillsController.groovy
@@ -5,6 +5,7 @@ import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import skills.service.auth.UserInfoService
+import skills.service.auth.aop.AdminUsersOnlyWhenUserIdSupplied
 import skills.service.icons.CustomIconFacade
 import skills.service.skillLoading.RankingLoader
 import skills.service.skillLoading.SkillsLoader
@@ -14,9 +15,10 @@ import skills.utils.Constants
 
 @CrossOrigin(allowCredentials = 'true')
 @RestController
-@RequestMapping("/api")
+@RequestMapping(["/api", "/admin"])
 @Slf4j
 @CompileStatic
+@AdminUsersOnlyWhenUserIdSupplied
 class UserSkillsController {
 
     @Autowired
@@ -36,29 +38,38 @@ class UserSkillsController {
 
     @RequestMapping(value = "/projects/{projectId}/level", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    Integer getUserLevel(@PathVariable(name = "projectId") String projectId) {
-        return skillsLoader.getUserLevel(projectId, userInfoService.currentUser.username)
+    Integer getUserLevel(@PathVariable(name = "projectId") String projectId,
+                         @RequestParam(name = "userId", required = false) String userId) {
+        return skillsLoader.getUserLevel(projectId, userId ?: userInfoService.currentUser.username)
     }
 
     @RequestMapping(value = "/projects/{projectId}/summary", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    OverallSkillSummary getSkillsSummary(@PathVariable("projectId") String projectId) {
-        return skillsLoader.loadOverallSummary(projectId, userInfoService.currentUser.username)
+    OverallSkillSummary getSkillsSummary(@PathVariable("projectId") String projectId,
+                                         @RequestParam(name = "userId", required = false) String userId,
+                                         @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
+        return skillsLoader.loadOverallSummary(projectId, userId ?: userInfoService.currentUser.username, version)
     }
 
     @RequestMapping(value = "/projects/{projectId}/subjects/{subjectId}/summary", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    SkillSubjectSummary getSubjectsSkillsSummary(@PathVariable("projectId") String projectId, @PathVariable("subjectId") String subjectId, @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadSubject(projectId, userInfoService.currentUser.username, subjectId, version)
+    SkillSubjectSummary getSubjectsSkillsSummary(@PathVariable("projectId") String projectId,
+                                                 @PathVariable("subjectId") String subjectId,
+                                                 @RequestParam(name = "userId", required = false) String userId,
+                                                 @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
+        return skillsLoader.loadSubject(projectId, userId ?: userInfoService.currentUser.username, subjectId, version)
     }
 
     @RequestMapping(value = "/projects/{projectId}/skills/{skillId}/summary", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    SkillSummary getSkillSummary(@PathVariable("projectId") String projectId, @PathVariable("skillId") String skillId, @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadSkillSummary(projectId, userInfoService.currentUser.username, null, skillId, version)
+    SkillSummary getSkillSummary(@PathVariable("projectId") String projectId,
+                                 @PathVariable("skillId") String skillId,
+                                 @RequestParam(name = "userId", required = false) String userId,
+                                 @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
+        return skillsLoader.loadSkillSummary(projectId, userId ?: userInfoService.currentUser.username, null, skillId, version)
     }
 
     @RequestMapping(value = "/projects/{projectId}/projects/{crossProjectId}/skills/{skillId}/summary", method = RequestMethod.GET, produces = "application/json")
@@ -67,8 +78,9 @@ class UserSkillsController {
     SkillSummary getCrossProjectSkillSummary(@PathVariable("projectId") String projectId,
                                              @PathVariable("crossProjectId") String crossProjectId,
                                              @PathVariable("skillId") String skillId,
+                                             @RequestParam(name = "userId", required = false) String userId,
                                              @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadSkillSummary(projectId, userInfoService.currentUser.username, crossProjectId, skillId, version)
+        return skillsLoader.loadSkillSummary(projectId, userId ?: userInfoService.currentUser.username, crossProjectId, skillId, version)
     }
 
 
@@ -76,72 +88,108 @@ class UserSkillsController {
     @ResponseBody
     @CompileStatic
     List<SkillBadgeSummary>  getAllBadgesSummary(@PathVariable("projectId") String projectId,
+                                                 @RequestParam(name = "userId", required = false) String userId,
                                                  @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadBadgeSummaries(projectId, userInfoService.currentUser.username, version)
+        return skillsLoader.loadBadgeSummaries(projectId, userId ?: userInfoService.currentUser.username, version)
     }
-
 
     @RequestMapping(value = "/projects/{projectId}/badges/{badgeId}/summary", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    SkillBadgeSummary getBadgeSummary(@PathVariable("projectId") String projectId, @PathVariable("badgeId") String badgeId) {
-        return skillsLoader.loadBadge(projectId, userInfoService.currentUser.username, badgeId)
+    SkillBadgeSummary getBadgeSummary(@PathVariable("projectId") String projectId,
+                                      @PathVariable("badgeId") String badgeId,
+                                      @RequestParam(name = "userId", required = false) String userId) {
+        return skillsLoader.loadBadge(projectId, userId ?: userInfoService.currentUser.username, badgeId)
     }
 
     @RequestMapping(value = "/projects/{projectId}/pointHistory", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    UserPointHistorySummary getProjectsPointHistory(@PathVariable("projectId") String projectId) {
-        return skillsLoader.loadPointHistorySummary(projectId, userInfoService.currentUser.username, 365)
+    UserPointHistorySummary getProjectsPointHistory(@PathVariable("projectId") String projectId,
+                                                    @RequestParam(name = "userId", required = false) String userId) {
+        return skillsLoader.loadPointHistorySummary(projectId, userId ?: userInfoService.currentUser.username, 365)
     }
 
     @RequestMapping(value = "/projects/{projectId}/subjects/{subjectId}/pointHistory", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    UserPointHistorySummary getSubjectsPointHistory(@PathVariable("projectId") String projectId, @PathVariable("subjectId") String subjectId) {
-        return skillsLoader.loadPointHistorySummary(projectId, userInfoService.currentUser.username, 365, subjectId)
+    UserPointHistorySummary getSubjectsPointHistory(@PathVariable("projectId") String projectId,
+                                                    @PathVariable("subjectId") String subjectId,
+                                                    @RequestParam(name = "userId", required = false) String userId) {
+        return skillsLoader.loadPointHistorySummary(projectId, userId ?: userInfoService.currentUser.username, 365, subjectId)
     }
 
     @RequestMapping(value = "/projects/{projectId}/skills/{skillId}/dependencies", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    SkillDependencyInfo loadSkillDependencyInfo(@PathVariable("projectId") String projectId, @PathVariable("skillId") String skillId) {
-        return skillsLoader.loadSkillDependencyInfo(projectId, userInfoService.currentUser.username, skillId)
+    SkillDependencyInfo loadSkillDependencyInfo(@PathVariable("projectId") String projectId,
+                                                @PathVariable("skillId") String skillId,
+                                                @RequestParam(name = "userId", required = false) String userId) {
+        return skillsLoader.loadSkillDependencyInfo(projectId, userId ?: userInfoService.currentUser.username, skillId)
     }
 
-    @RequestMapping(value = "/projects/{projectId}/skills/{skillId}", method = RequestMethod.POST, produces = "application/json")
-    @ResponseBody
-    @CompileStatic
-    SkillsManagementFacade.AddSkillResult addSkill(@PathVariable("projectId") String projectId, @PathVariable("skillId") String skillId) {
-        skillsManagementFacade.addSkill(projectId, skillId, userInfoService.currentUser.username)
-    }
+    // had to move the following addSkill methods into their own separate controllers
+    // to avoid ambiguous naming conflict with both /api and /admin versions
+
+//    @RequestMapping(value = "/projects/{projectId}/skills/{skillId}", method = RequestMethod.POST, produces = "application/json")
+//    @ResponseBody
+//    @CompileStatic
+//    SkillsManagementFacade.AddSkillResult addSkill(@PathVariable("projectId") String projectId,
+//                                                   @PathVariable("skillId") String skillId,
+//                                                   @RequestParam(name = "userId", required = false) String userId) {
+//        skillsManagementFacade.addSkill(projectId, skillId, userInfoService.currentUser.username)
+//    }
+//
+//    @RequestMapping(value = "/projects/{projectId}/skills/{skillId}", method = [RequestMethod.PUT, RequestMethod.POST], produces = "application/json")
+//    @ResponseBody
+//    @CompileStatic
+//    SkillsManagementFacade.AddSkillResult addSkill(
+//            @PathVariable("projectId") String projectId,
+//            @PathVariable("skillId") String skillId,
+//            @RequestBody AddSkillRequest skillRequest) {
+//        try {
+//            assert skillRequest?.userId
+//            Date incomingDate = skillRequest.timestamp != null ? new Date(skillRequest.timestamp) : null
+//
+//            return skillsManagementFacade.addSkill(projectId, skillId, userInfoService.lookupUserId(skillRequest.userId), incomingDate)
+//        } catch (Exception e) {
+//            log.error("Failed for projetId=[$projectId], skillId=[$skillId]", e)
+//            throw new SkillException(e.message, projectId, skillId)
+//        }
+//    }
 
     @RequestMapping(value = "/projects/{projectId}/rank", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    SkillsRanking getRanking(@PathVariable("projectId") String projectId) {
-        return rankingLoader.getUserSkillsRanking(projectId, userInfoService.currentUser.username)
+    SkillsRanking getRanking(@PathVariable("projectId") String projectId,
+                             @RequestParam(name = "userId", required = false) String userId) {
+        return rankingLoader.getUserSkillsRanking(projectId, userId ?: userInfoService.currentUser.username)
     }
 
     @RequestMapping(value = "/projects/{projectId}/subjects/{subjectId}/rank", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    SkillsRanking getRankingBySubject(@PathVariable("projectId") String projectId, @PathVariable("subjectId") String subjectId) {
-        return rankingLoader.getUserSkillsRanking(projectId, userInfoService.currentUser.username, subjectId)
+    SkillsRanking getRankingBySubject(@PathVariable("projectId") String projectId,
+                                      @PathVariable("subjectId") String subjectId,
+                                      @RequestParam(name = "userId", required = false) String userId) {
+        return rankingLoader.getUserSkillsRanking(projectId, userId ?: userInfoService.currentUser.username, subjectId)
     }
 
     @RequestMapping(value = "/projects/{projectId}/rankDistribution", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    SkillsRankingDistribution getRankingDistribution(@PathVariable("projectId") String projectId) {
-        return rankingLoader.getRankingDistribution(projectId, userInfoService.currentUser.username)
+    SkillsRankingDistribution getRankingDistribution(@PathVariable("projectId") String projectId,
+                                                     @RequestParam(name = "userId", required = false) String userId) {
+        return rankingLoader.getRankingDistribution(projectId, userId ?: userInfoService.currentUser.username)
     }
 
     @RequestMapping(value = "/projects/{projectId}/subjects/{subjectId}/rankDistribution", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
-    SkillsRankingDistribution getRankingDistributionBySubject(@PathVariable("projectId") String projectId, @PathVariable("subjectId") String subjectId) {
-        return rankingLoader.getRankingDistribution(projectId, userInfoService.currentUser.username, subjectId)
+    SkillsRankingDistribution getRankingDistributionBySubject(@PathVariable("projectId") String projectId,
+                                                              @PathVariable("subjectId") String subjectId,
+                                                              @RequestParam(name = "userId", required = false) String userId) {
+        return rankingLoader.getRankingDistribution(projectId, userId ?: userInfoService.currentUser.username, subjectId)
     }
 
     @RequestMapping(value = "/projects/{id}/customIconCss", method = RequestMethod.GET, produces = "text/css")
@@ -149,5 +197,4 @@ class UserSkillsController {
     String getCustomIconCss(@PathVariable("id") String projectId) {
         return customIconFacade.generateCss(projectId)
     }
-
 }
