ac998446543974bb5a9ec5753e6e846a5ecb0297
EVOFORGE\dimay <dimay@evoforge.org>
2019-05-08 16:15:22 -0400

    #83: boostrap transition - wip - cross-projects

--- frontend/src/components/skills/crossProjects/SharedSkillsFromOtherProjects.vue
+++ frontend/src/components/skills/crossProjects/SharedSkillsFromOtherProjects.vue
@@ -1,18 +1,21 @@
 <template>
-  <simple-card id="shared-skills-from-others-panel">
-    <h6>Skills Shared <strong>From</strong> Other Projects</h6>
-
-    <loading-container :is-loading="loading">
-      <div v-if="sharedSkills && sharedSkills.length > 0" class="my-4">
-        <shared-skills-table :shared-skills="sharedSkills" :disable-delete="true"></shared-skills-table>
-      </div>
-      <div v-else class="my-5">
+  <div id="shared-skills-from-others-panel" class="card">
+    <div class="card-header">
+      Skills Shared <strong>From</strong> Other Projects
+    </div>
+    <div class="card-body">
+      <loading-container :is-loading="loading">
+        <div v-if="sharedSkills && sharedSkills.length > 0" class="my-4">
+          <shared-skills-table :shared-skills="sharedSkills" :disable-delete="true"></shared-skills-table>
+        </div>
+        <div v-else class="my-5">
           <no-content2 title="No Shared Skills Yet..." icon="far fa-handshake"
-            message="Coordinate with other projects to share skills with this project."></no-content2>
-      </div>
+                       message="Coordinate with other projects to share skills with this project."></no-content2>
+        </div>
 
-    </loading-container>
-  </simple-card>
+      </loading-container>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -20,12 +23,10 @@
   import SkillsShareService from './SkillsShareService';
   import LoadingContainer from '../../utils/LoadingContainer';
   import SharedSkillsTable from './SharedSkillsTable';
-  import SimpleCard from '../../utils/cards/SimpleCard';
 
   export default {
     name: 'SharedSkillsFromOtherProjects',
     components: {
-      SimpleCard,
       SharedSkillsTable,
       LoadingContainer,
       NoContent2,
