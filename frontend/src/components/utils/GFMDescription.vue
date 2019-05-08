e0f5d2b71107bb9123a18497087f63befd8e70fe
Ryan Mayo <rmmayo@evoforge.org>
2019-05-08 17:37:53 -0400

    #83 - converted styling to use bootstrap

--- frontend/src/components/utils/GFMDescription.vue
+++ frontend/src/components/utils/GFMDescription.vue
@@ -1,77 +1,82 @@
 <template>
-    <div id="gfmDescription" class="modal-card" style="width: 1110px;">
-      <modal-header title="Markdown Syntax" @cancel-clicked="$parent.close()"></modal-header>
+  <div class="container">
+    <div id="gfmDescription" class="card">
+      <div class="card-header bg-info" @cancel-clicked="$parent.close()">
+        <h4 class="text-white">Markdown Syntax</h4>
+      </div>
 
 
-      <section class="modal-card-body content">
-        <p>Descriptions support GFM (Git Flavored Markdown). Some limited examples of the supported formatting are:</p>
+      <div class="card-body">
+        <div class="card-text">
+          <p class="card-title">Descriptions support GFM (Git Flavored Markdown). Some limited examples of the supported
+            formatting are:</p>
 
-        <h3>Headers</h3>
-        <pre>
+          <h3>Headers</h3>
+          <pre class="bg-light"><code>
         # H1
         ## H2
         ### H3
         #### H4
         ##### H5
         ###### H6
-        </pre>
+        </code></pre>
 
-        <h3>Emphasis</h3>
-        <pre>
+          <h3>Emphasis</h3>
+          <pre class="bg-light"><code>
         italics: *italicized* or _italicized_
         bold: **bolded** or __bolded__
         combination **_bolde&italicized_**
         strikethrough: ~~struck~~
-        </pre>
+        </code></pre>
 
-        <h3>Inline</h3>
-        <pre>
-          Inline `code` hs `back-ticks around` it.
-        </pre>
+          <h3>Inline</h3>
+          <pre class="bg-light"><code>
+        Inline `code` hs `back-ticks around` it.
+        </code></pre>
 
-        <h3>Lists</h3>
-        <p>Ordered Lists:</p>
-        <pre>
+          <h3>Lists</h3>
+          <p>Ordered Lists:</p>
+          <pre class="bg-light"><code>
         1. Item one
         1. Item two
         1. Item three (actual number does not matter)
-        </pre>
-        <p>
-        If  List item has multiple lines of text, subsequent lines must be idented four spaces,
-        otherwise list item numbers will reset, e.g.,
-        </p>
-        <pre>
+        </code></pre>
+          <p>
+            If List item has multiple lines of text, subsequent lines must be idented four spaces,
+            otherwise list item numbers will reset, e.g.,
+          </p>
+          <pre class="bg-light"><code>
         1. item one
             paragrah one
         1. item two
         1. item three
-        </pre>
-        <p>Unordered Lists</p>
-        <pre>
+        </code></pre>
+          <p>Unordered Lists</p>
+          <pre class="bg-light"><code>
         * Item
         * Item
         * Item
-        </pre>
+        </code></pre>
 
-        <h3>
-        Links
-        </h3>
-        <pre>
+          <h3>
+            Links
+          </h3>
+          <pre class="bg-light"><code>
         [in line link](https://www.somewebsite.com)
-        </pre>
+        </code></pre>
 
 
-        <h3>
-        Blockquotes
-        </h3>
-        <pre>
+          <h3>
+            Blockquotes
+          </h3>
+          <pre class="bg-light"><code>
         > This is a block quote
-        </pre>
+        </code></pre>
 
-        <h3>
-        Horizontal rule
-        </h3>
-        <pre>
+          <h3>
+            Horizontal rule
+          </h3>
+          <pre class="bg-light"><code>
         Use three or more dashes, asterisks, or underscores to generate a horizontal rule line
 
         Separate me
@@ -80,23 +85,18 @@
         ---
         Separate me
         ***
-        </pre>
-
-      </section>
+        </code></pre>
+        </div>
+      </div>
     </div>
-</template>
+  </div></template>
 
 <script>
-  import ModalHeader from './modal/ModalHeader';
-
   export default {
     name: 'GFMDescription',
-    components: { ModalHeader },
     props: [],
     data() {
-      return {
-
-      };
+      return {};
     },
   };
 </script>
