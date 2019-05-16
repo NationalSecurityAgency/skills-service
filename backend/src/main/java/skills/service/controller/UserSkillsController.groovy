b2fd665f904bf6f9b8746c31877ca76f2834d251
EVOFORGE\dimay <dimay@evoforge.org>
2019-05-16 10:04:00 -0400

    #80: Fixed user points calculation for cross project skill summary, than you integration tests!

--- backend/src/main/java/skills/service/controller/UserSkillsController.groovy
+++ backend/src/main/java/skills/service/controller/UserSkillsController.groovy
@@ -63,25 +63,29 @@ class UserSkillsController {
         return skillsLoader.loadSubject(projectId, getUserId(userIdParam), subjectId, version)
     }
 
+    /**
+     * Note: skill versioning is not applicable to a single skill
+     */
     @RequestMapping(value = "/projects/{projectId}/skills/{skillId}/summary", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
     SkillSummary getSkillSummary(@PathVariable("projectId") String projectId,
                                  @PathVariable("skillId") String skillId,
-                                 @RequestParam(name = "userId", required = false) String userIdParam,
-                                 @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadSkillSummary(projectId, getUserId(userIdParam), null, skillId, version)
+                                 @RequestParam(name = "userId", required = false) String userIdParam) {
+        return skillsLoader.loadSkillSummary(projectId, getUserId(userIdParam), null, skillId)
     }
 
+    /**
+     * Note: skill versioning is not applicable to a single skill
+     */
     @RequestMapping(value = "/projects/{projectId}/projects/{crossProjectId}/skills/{skillId}/summary", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CompileStatic
     SkillSummary getCrossProjectSkillSummary(@PathVariable("projectId") String projectId,
                                              @PathVariable("crossProjectId") String crossProjectId,
                                              @PathVariable("skillId") String skillId,
-                                             @RequestParam(name = "userId", required = false) String userIdParam,
-                                             @RequestParam(name = 'version', required = false, defaultValue = Constants.MAX_VERSION_STRING) Integer version) {
-        return skillsLoader.loadSkillSummary(projectId, getUserId(userIdParam), crossProjectId, skillId, version)
+                                             @RequestParam(name = "userId", required = false) String userIdParam) {
+        return skillsLoader.loadSkillSummary(projectId, getUserId(userIdParam), crossProjectId, skillId)
     }
 
 
