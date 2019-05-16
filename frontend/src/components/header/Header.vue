5950152f59938c01f51d670c4db7b8ef70f7f274
EVOFORGE\dimay <dimay@evoforge.org>
2019-05-16 14:02:24 -0400

    #115: settings and log-out within drop-down

--- frontend/src/components/header/Header.vue
+++ frontend/src/components/header/Header.vue
@@ -7,10 +7,6 @@
 
       <div class="col text-center text-sm-right mt-3 mt-sm-0">
         <settings-button class="mr-2"/>
-        <button v-if="isJwtAuthenticatedUser" class="btn btn-outline-info" v-on:click="signOut"
-                v-b-tooltip.hover title="Sign Out">
-          <i class="fas fa-sign-out-alt"/>
-        </button>
       </div>
     </div>
     <breadcrumb></breadcrumb>
@@ -27,35 +23,9 @@
       Breadcrumb,
       SettingsButton,
     },
-    methods: {
-      signOut() {
-        this.$store.dispatch('logout');
-      },
-    },
-    computed: {
-      isAuthenticated() {
-        return this.$store.getters.isAuthenticated;
-      },
-      isJwtAuthenticatedUser() {
-        return this.$store.getters.isAuthenticated && !this.$store.getters.isPkiAuthenticated;
-      },
-    },
   };
 </script>
 
 <style scoped>
-  .header {
-    padding-top: 1rem;
-  }
-
-  .title {
-    font-size: 2rem;
-    /*font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;*/
-    font-family: 'Trocchi', serif;
-    /*color: #7c795d;*/
-    font-weight: normal;
-    line-height: 1.8rem;
-    /*margin: 0 0 1em;*/
-  }
 
 </style>
--- frontend/src/components/header/SettingsButton.vue
+++ frontend/src/components/header/SettingsButton.vue
@@ -1,9 +1,21 @@
 <template>
-    <button class="btn btn-outline-info" v-b-tooltip.hover.bottom="toolTipText" @click="gotoSettings">
-      <i class="fas fa-user-astronaut pr-1"/>
-      <span v-if="isAuthenticatedUser">{{ userInfo.first }} {{ userInfo.last }}</span>
-      <span v-else>Hello, Sign In</span>
-    </button>
+  <b-dropdown right variant="outline-info">
+    <template slot="button-content">
+      <i class="fas fa-user-astronaut pr-1"/> {{ userInfo.first }} {{ userInfo.last }}
+    </template>
+    <b-dropdown-item href="#"  @click="gotoSettings">
+      <span class="text-info"> <i class="fas fa-cog" style="width: 1.5rem;"/>Settings</span>
+    </b-dropdown-item>
+    <b-dropdown-item v-if="isJwtAuthenticatedUser" href="#" @click="signOut">
+      <span class="text-info"> <i class="fas fa-sign-out-alt" style="width: 1.5rem;"/>Log Out</span>
+    </b-dropdown-item>
+  </b-dropdown>
+
+<!--    <button class="btn btn-outline-info" v-b-tooltip.hover.bottom="toolTipText" @click="gotoSettings">-->
+<!--      <i class="fas fa-user-astronaut pr-1"/>-->
+<!--      <span v-if="isAuthenticatedUser">{{ userInfo.first }} {{ userInfo.last }}</span>-->
+<!--      <span v-else>Hello, Sign In</span>-->
+<!--    </button>-->
 </template>
 
 <script>
@@ -15,23 +27,22 @@
       };
     },
     computed: {
-      toolTipText() {
-        let text = 'Hello, Please Sign In Below';
-        if (this.userInfo) {
-          text = `Logged in as '${this.userInfo.first} ${this.userInfo.last}'`;
-        }
-        return text;
-      },
       userInfo() {
         return this.$store.getters.userInfo;
       },
       isAuthenticatedUser() {
         return this.$store.getters.isAuthenticated;
       },
+      isJwtAuthenticatedUser() {
+        return this.isAuthenticatedUser && !this.$store.getters.isPkiAuthenticated;
+      },
     },
     methods: {
       gotoSettings() {
         this.$router.push({ name: 'Settings', params: { settingsCategory: 'general' } });
+      },
+      signOut() {
+        this.$store.dispatch('logout');
       },
     },
   };
