b7e9aedc7f5f4d5a68487ee2b5d921ee0cf0ce1a
Ryan Mayo <rmmayo@evoforge.org>
2019-05-17 11:11:54 -0400

    #99 - converted skills-bootstrap to cli 3, reduced dependencies and converted to bootstrap 4

--- skills-bootstrap/src/components/RootPki.vue
+++ skills-bootstrap/src/components/RootPki.vue
@@ -1,49 +1,41 @@
 <template>
-    <div class="container is-fluid">
-        <div class="columns section">
-            <div class="column is-half is-offset-one-quarter">
-                <div class="card">
-                    <div class="card-header has-background-grey-lighter">
-                        <span class="icon is-large">
-                          <i class="card-header-icon fas fa-award"/>
-                        </span>
-                        <p class="card-header-title">User Skills Management Dashboard</p>
-                    </div>
-                    <div class="card-content">
-                        <p>
-                            Welcome to the User Skills Management Dashboard.  It looks like you're using PKI authentication, but you don't currently have a user with root permissions. At least one user must have root privileges.
-                        </p>
-                        <p style="margin-top:2em">
-                            Click Continue to grant root privileges to your account. </p>
-                    </div>
-                    <form @submit="$emit('grantRoot')">
-                            <div class="field">
-                                <div id="button-control" class="control">
-                                    <button id="continue-button" class="button is-primary"
-                                            :disabled="errors.any()">
-                                      <span class="icon is-small">
-                                        <i class="fas fa-arrow-circle-right"/>
-                                      </span>
-                                        <span>Continue</span>
-                                    </button>
-                                </div>
-                            </div>
-                    </form>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8 col-lg-6 mt-5">
+        <div class="card">
+          <div class="card-header">
+            <strong>
+              <i class="fas fa-award mr-3"/>User Skills Management Dashboard
+            </strong>
+          </div>
+          <div class="card-body">
+            <p>Welcome to the User Skills Management Dashboard. It looks like you're using PKI authentication, but you don't currently have a user with root permissions. At least one user must have root privileges.</p>
+            <p class="mt-4">Click Continue to grant root privileges to your account.</p>
+            <form @submit="$emit('grantRoot')">
+              <div class="field">
+                <div class="text-center">
+                  <button class="btn btn-primary" :disabled="errors.any()">
+                      <i class="fas fa-arrow-circle-right"/> Continue
+                  </button>
                 </div>
-            </div>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'RootPki',
-  };
+export default {
+  name: "RootPki"
+};
 </script>
 
 <style scoped>
-    #button-control {
-        text-align: center;
-        padding-bottom: 1.5rem;
-    }
+#button-control {
+  text-align: center;
+  padding-bottom: 1.5rem;
+}
 </style>
--- skills-bootstrap/src/components/RootRegistration.vue
+++ skills-bootstrap/src/components/RootRegistration.vue
@@ -1,135 +1,163 @@
 <template>
-    <div class="container is-fluid">
-        <div class="columns section">
-            <div class="column is-half is-offset-one-quarter">
-                <div class="card">
-                    <div class="card-header has-background-grey-lighter">
-                        <span class="icon is-large">
-                          <i class="card-header-icon fas fa-award"/>
-                        </span>
-                        <p class="card-header-title">User Skills Management Dashboard</p>
-                    </div>
-                    <div class="card-content">
-                        <p>
-                            Welcome to the User Skills Management Dashboard.  It looks like the application isn't fully setup. Before you can start using the Dashboard, you'll need a root user configured to update settings, grant permissions, etc.
-                        </p>
-                        <p style="margin-top:2em">
-                            Please create the root account to get started. </p>
-                    </div>
-                    <form @submit.prevent="validateAndLogin">
-                        <div class="columns">
-
-                            <div class="column is-three-fifths is-offset-one-fifth">
-                                <div class="field">
-                                    <label class="label">First Name</label>
-                                    <input class="input" type="text" v-model="loginFields.firstName" name="firstName"
-                                           v-validate="'required'" data-vv-delay="500"/>
-                                    <p class="help is-danger" v-show="errors.has('firstName')">{{
-                                        errors.first('firstName')}}</p>
-                                </div>
-                                <div class="field">
-                                    <label class="label">Last Name</label>
-                                    <input class="input" type="text" v-model="loginFields.lastName" name="lastName"
-                                           v-validate="'required'" data-vv-delay="500"/>
-                                    <p class="help is-danger" v-show="errors.has('lastName')">{{
-                                        errors.first('lastName')}}</p>
-                                </div>
-                                <div class="field">
-                                    <label class="label">Email</label>
-                                    <input class="input" type="text" v-model="loginFields.email" name="email"
-                                           v-validate="'required|email|uniqueEmail'" data-vv-delay="500"/>
-                                    <p class="help is-danger" v-show="errors.has('email')">{{
-                                        errors.first('email')}}</p>
-                                </div>
-                                <div class="field">
-                                    <label class="label">Password</label>
-                                    <input class="input" type="password" v-model="loginFields.password" name="password" ref="password"
-                                           placeholder="At least 8 characters"
-                                           v-validate="'required|min:8|max:15'" data-vv-delay="500"/>
-                                    <p class="help is-danger" v-show="errors.has('password')">{{
-                                        errors.first('password')}}</p>
-                                </div>
-                                <div class="field">
-                                    <label class="label">Re-enter Password</label>
-                                    <input class="input" type="password" v-model="loginFields.password_confirmation" name="password_confirmation"
-                                           v-validate="'required|confirmed:password'" data-vv-delay="500" data-vv-as="password"/>
-                                    <p class="help is-danger" v-show="errors.has('password_confirmation')">{{
-                                        errors.first('password_confirmation')}}</p>
-                                </div>
-                                <div class="field">
-                                    <div class="control">
-                                        <button class="button is-primary is-outlined"
-                                                :disabled="errors.any() || missingRequiredValues()">
-                                            <span class="icon is-small">
-                                              <i class="fas fa-arrow-circle-right"/>
-                                            </span>
-                                            <span>Create Account</span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8 col-lg-6 mt-5">
+        <div class="card">
+          <div class="card-header">
+            <strong>
+              <i class="fas fa-award mr-3"/>User Skills Management Dashboard
+            </strong>
+          </div>
+          <div class="card-body">
+            <div>
+              <p>Welcome to the User Skills Management Dashboard. It looks like the application isn't fully setup. Before you can start using the Dashboard, you'll need a root user configured to update settings, grant permissions, etc.</p>
+              <p style="margin-top:2em">Please create the root account to get started.</p>
             </div>
+            <form @submit.prevent="validateAndLogin">
+              <div class="columns">
+                <div class="column is-three-fifths is-offset-one-fifth">
+                  <div class="form-group">
+                    <label class="label">First Name</label>
+                    <input
+                      class="form-control"
+                      type="text"
+                      v-model="loginFields.firstName"
+                      name="firstName"
+                      v-validate="'required'"
+                      data-vv-delay="500"
+                    >
+                    <p class="help is-danger" v-show="errors.has('firstName')">{{errors.first('firstName')}}</p>
+                  </div>
+                  <div class="form-group">
+                    <label class="label">Last Name</label>
+                    <input
+                      class="form-control"
+                      type="text"
+                      v-model="loginFields.lastName"
+                      name="lastName"
+                      v-validate="'required'"
+                      data-vv-delay="500"
+                    >
+                    <p class="help is-danger" v-show="errors.has('lastName')">{{errors.first('lastName')}}</p>
+                  </div>
+                  <div class="form-group">
+                    <label class="label">Email</label>
+                    <input
+                      class="form-control"
+                      type="text"
+                      v-model="loginFields.email"
+                      name="email"
+                      v-validate="'required|email|uniqueEmail'"
+                      data-vv-delay="500"
+                    >
+                    <p class="help is-danger" v-show="errors.has('email')">{{errors.first('email')}}</p>
+                  </div>
+                  <div class="form-group">
+                    <label class="label">Password</label>
+                    <input
+                      class="form-control"
+                      type="password"
+                      v-model="loginFields.password"
+                      name="password"
+                      ref="password"
+                      placeholder="At least 8 characters"
+                      v-validate="'required|min:8|max:15'"
+                      data-vv-delay="500"
+                    >
+                    <p class="help is-danger" v-show="errors.has('password')">{{errors.first('password')}}</p>
+                  </div>
+                  <div class="form-group">
+                    <label class="label">Re-enter Password</label>
+                    <input
+                      class="form-control"
+                      type="password"
+                      v-model="loginFields.password_confirmation"
+                      name="password_confirmation"
+                      v-validate="'required|confirmed:password'"
+                      data-vv-delay="500"
+                      data-vv-as="password"
+                    >
+                    <p class="help is-danger" v-show="errors.has('password_confirmation')">{{errors.first('password_confirmation')}}</p>
+                  </div>
+                  <div>
+                    <button class="btn btn-outline-primary" :disabled="errors.any() || missingRequiredValues()">
+                      <i class="fas fa-arrow-circle-right"/> Create Account
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-  import { Validator } from 'vee-validate';
-  import BootstrapService from './BootstrapService';
+import { Validator } from "vee-validate";
+import BootstrapService from "./BootstrapService";
 
-  const dictionary = {
-    en: {
-      attributes: {
-        password: 'Password',
-        email: 'Email',
-        firstName: 'First Name',
-        lastName: 'Last Name',
-      },
-    },
-  };
-  Validator.localize(dictionary);
-  Validator.extend('uniqueEmail', {
-    getMessage: 'The email address is already used for another account.',
+const dictionary = {
+  en: {
+    attributes: {
+      password: "Password",
+      email: "Email",
+      firstName: "First Name",
+      lastName: "Last Name"
+    }
+  }
+};
+Validator.localize(dictionary);
+Validator.extend(
+  "uniqueEmail",
+  {
+    getMessage: "The email address is already used for another account.",
     validate(value) {
-      return BootstrapService.userWithEmailExists(value)
-        .catch((e) => { throw e; });
-    },
-  }, {
-    immediate: false,
-  });
+      return BootstrapService.userWithEmailExists(value).catch(e => {
+        throw e;
+      });
+    }
+  },
+  {
+    immediate: false
+  }
+);
 
-  export default {
-    name: 'RootRegistration',
-    components: {},
-    data() {
-      return {
-        loginFields: {
-          firstName: '',
-          lastName: '',
-          email: '',
-          password: '',
-          password_confirmation: '',
-        },
-      };
+export default {
+  name: "RootRegistration",
+  components: {},
+  data() {
+    return {
+      loginFields: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        password_confirmation: ""
+      }
+    };
+  },
+  methods: {
+    validateAndLogin() {
+      this.$validator.validate().then(valid => {
+        if (valid) {
+          this.$emit("registerUser", this.loginFields);
+        }
+      });
     },
-    methods: {
-      validateAndLogin() {
-        this.$validator.validate().then((valid) => {
-          if (valid) {
-            this.$emit('registerUser', this.loginFields);
-          }
-        });
-      },
-      missingRequiredValues() {
-        return !this.loginFields.firstName || !this.loginFields.lastName || !this.loginFields.email || !this.loginFields.password || !this.loginFields.password_confirmation;
-      },
-    },
-  };
+    missingRequiredValues() {
+      return (
+        !this.loginFields.firstName ||
+        !this.loginFields.lastName ||
+        !this.loginFields.email ||
+        !this.loginFields.password ||
+        !this.loginFields.password_confirmation
+      );
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
