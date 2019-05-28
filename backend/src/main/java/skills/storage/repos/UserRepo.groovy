301ea1d86fec148d3ccef5502619ebec5e89384f
Matthew Kirkley <mtkirkl@evoforge.org>
2019-05-28 11:29:33 -0400

    #116 @Nullable

--- backend/src/main/java/skills/storage/repos/UserRepo.groovy
+++ backend/src/main/java/skills/storage/repos/UserRepo.groovy
@@ -1,6 +1,5 @@
 package skills.storage.repos
 
-
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
