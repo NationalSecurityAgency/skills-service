10c5cf1b50a39a540d0133c5a009765a767dc953
EVOFORGE\dimay <dimay@evoforge.org>
2019-05-31 13:36:06 -0400

    #136: Integration of Inception project - WIP

--- skills-bootstrap/src/components/RootPki.vue
+++ skills-bootstrap/src/components/RootPki.vue
@@ -1,35 +1,24 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8 col-lg-6 mt-5">
-        <div class="card">
-          <div class="card-header">
-            <strong>
-              <i class="fas fa-award mr-3"/>User Skills Management Dashboard
-            </strong>
-          </div>
-          <div class="card-body">
-            <p>Welcome to the User Skills Management Dashboard. It looks like you're using PKI authentication, but you don't currently have a user with root permissions. At least one user must have root privileges.</p>
-            <p class="mt-4">Click Continue to grant root privileges to your account.</p>
-            <form @submit="$emit('grantRoot')">
-              <div class="field">
-                <div class="text-center">
-                  <button class="btn btn-primary" :disabled="errors.any()">
-                      <i class="fas fa-arrow-circle-right"/> Continue
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+  <bootstrap-container title="User Skills Dashboard">
+    <p>Welcome to the User Skills Management Dashboard. It looks like you're using PKI authentication, but you don't currently have a user with root permissions. At least one user must have root privileges.</p>
+    <p class="mt-4">Click Continue to grant root privileges to your account.</p>
+    <form @submit="$emit('grantRoot')">
+      <div class="field">
+        <div class="text-center">
+          <button class="btn btn-primary" :disabled="errors.any()">
+            <i class="fas fa-arrow-circle-right"/> Continue
+          </button>
         </div>
       </div>
-    </div>
-  </div>
+    </form>
+  </bootstrap-container>
 </template>
 
 <script>
+import BootstrapContainer from './BootstrapContainer';
 export default {
-  name: "RootPki"
+  name: "RootPki",
+  components: { BootstrapContainer }
 };
 </script>
 
--- skills-bootstrap/src/components/Success.vue
+++ skills-bootstrap/src/components/Success.vue
@@ -1,7 +1,8 @@
 <template>
     <bootstrap-container title="Initialization is Done!">
-        <p><i class="far fa-check-square"></i> The root account has been successfully created!</p>
-        <p style="margin-top:2em">Click Continue to proceed the User Skills Management Dashboard.</p>
+        <p><i class="far fa-check-square text-success"></i> The root account has been successfully created!</p>
+        <p><i class="far fa-check-square text-success"></i> Inception self-training project created!</p>
+        <p style="margin-top:2em">Please proceed the User Skills Dashboard.</p>
         <div class="text-center">
             <button class="btn btn-outline-primary" @click="$emit('proceed')">
                 <i class="fas fa-arrow-circle-right"/> Continue
