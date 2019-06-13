ead1c851b50fdfb17df34b5e3bf7adea0d54c27f
Ryan Mayo <rmmayo@evoforge.org>
2019-06-13 07:16:48 -0400

    #147 - added https support to dev proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
+const fs = require('fs');
 
 const resolve = dir => path.join(__dirname, dir);
 
+const getTarget = () => {
+  let target = 'http://localhost:8080';
+  const proxySslKeyLocation = process.env.WEBPACK_DEV_SERVER_PROXY_TABLE_SSL_KEY_PATH;
+  const proxyCertKeyLocation = process.env.WEBPACK_DEV_SERVER_PROXY_TABLE_SSL_CERT_PATH;
+  if (fs.existsSync(proxySslKeyLocation) && fs.existsSync(proxyCertKeyLocation)) {
+    target = {
+      host: 'localhost',
+      port: 8443,
+      key: fs.readFileSync(proxySslKeyLocation),
+      cert: fs.readFileSync(proxyCertKeyLocation),
+      protocol: 'https:'
+    };
+  }
+  return target;
+};
+
 const proxyConf = {
-  target: 'http://localhost:8080',
+  target: getTarget(),
+  secure: false,
   changeOrigin: true,
   logLevel: 'debug',
 };
