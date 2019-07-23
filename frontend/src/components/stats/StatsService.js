990a1d0602ec5299fd4ad7ca2b0c67d5dbb21c92
Ryan Mayo <rmmayo@evoforge.org>
2019-07-23 12:46:20 -0400

    #189 - removing dead code

--- frontend/src/components/stats/StatsService.js
+++ frontend/src/components/stats/StatsService.js
@@ -2,19 +2,6 @@ import axios from 'axios';
 import { SECTION } from './SectionHelper';
 
 export default {
-  getUsage(projectId, numDays) {
-    return axios.get(`/admin/projects/${projectId}/usage?numDays=${numDays}`)
-      .then(response => response.data);
-  },
-  numAchievedSkillsPivotedBySubject(projectId) {
-    return axios.get(`/admin/projects/${projectId}/stats?type=NUM_ACHIEVED_SKILLS_PIVOTED_BY_SUBJECT`)
-      .then(response => response.data);
-  },
-  numUsersPerSkillLevel(projectId) {
-    return axios.get(`/admin/projects/${projectId}/stats?type=NUM_USERS_PER_OVERALL_SKILL_LEVEL`)
-      .then(response => response.data);
-  },
-
   getChartsForSection(sectionParams) {
     let url = `/admin/projects/${sectionParams.projectId}/metrics?numDays=${sectionParams.numDays}&numMonths=${sectionParams.numMonths}&loadDataForFirst=${sectionParams.loadDataForFirst}`;
     if (sectionParams.section !== SECTION.PROJECTS) {
