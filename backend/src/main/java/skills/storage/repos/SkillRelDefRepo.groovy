99bb57cab3d0574700ba9cb314bfef9bddf372e0
EVOFORGE\dimay <dimay@evoforge.org>
2019-08-02 14:57:22 -0400

    #185: performance improvements - dep. graph loading improved

--- backend/src/main/java/skills/storage/repos/SkillRelDefRepo.groovy
+++ backend/src/main/java/skills/storage/repos/SkillRelDefRepo.groovy
@@ -38,7 +38,36 @@ interface SkillRelDefRepo extends CrudRepository<SkillRelDef, Integer> {
               and sd1.projectId=?1 and sd1.skillId=?2''')
     List<SkillDef> getChildren(String projectId, String parentSkillId, SkillRelDef.RelationshipType type)
 
-    @Query('''select sd1, sd2 from SkillDef sd1, SkillDef sd2, SkillRelDef srd 
+    static interface SkillDefSkinny {
+        Integer getId()
+        String getProjectId()
+        String getName()
+        String getSkillId()
+        Integer getVersion()
+        Integer getDisplayOrder()
+        Date getCreated()
+    }
+
+    /**
+     * mapping directly to entity is slow, we can save over a second in latency by mapping attributes explicitly
+     */
+    @Query('''select 
+        sd1.id as id,
+        sd1.name as name, 
+        sd1.skillId as skillId, 
+        sd1.projectId as projectId, 
+        sd1.pointIncrement as pointIncrement,
+        sd1.totalPoints as totalPoints,
+        sd1.type as skillType,
+         
+        sd2.id as id,
+        sd2.name as name, 
+        sd2.skillId as skillId, 
+        sd2.projectId as projectId, 
+        sd2.pointIncrement as pointIncrement,
+        sd2.totalPoints as totalPoints,
+        sd2.type as skillType
+        from SkillDef sd1, SkillDef sd2, SkillRelDef srd 
         where sd1 = srd.parent and sd2 = srd.child and srd.type=?2 
               and sd1.projectId=?1''')
     List<Object[]> getGraph(String projectId, SkillRelDef.RelationshipType type)
