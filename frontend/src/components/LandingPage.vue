54c6c8c7aa29a8df6b3f26dbe192827a2280657d
Ryan Mayo <rmmayo@evoforge.org>
2019-08-05 13:44:51 -0400

    #201 - adding nav bar to home page

---
+++ frontend/src/components/LandingPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <navigation :nav-items="[
+          {name: 'Projects', iconClass: 'fa-project-diagram', page: 'HomePage'},
+          {name: 'Badges', iconClass: 'fa-award', page: 'CrossProjectBadges'},
+          // {name: 'Settings', iconClass: 'fa-cogs', page: 'GeneralSettings'}
+        ]">
+    </navigation>
+
+  </div>
+</template>
+
+<script>
+  import Navigation from './utils/Navigation';
+
+  export default {
+    name: 'LandingPage',
+    components: {
+      Navigation,
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
