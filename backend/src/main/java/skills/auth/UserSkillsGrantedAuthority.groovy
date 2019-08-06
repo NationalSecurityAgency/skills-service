1826bdf9ddc8c5cd3caa6c06262d40c6a5ae60a2
Ryan Mayo <rmmayo@evoforge.org>
2019-08-06 16:02:14 -0400

    #205 - getting 'org.codehaus.groovy.runtime.typehandling.GroovyCastException: Cannot cast object '127' with class 'java.lang.Integer' to class 'java.lang.StringBuilder'' on integration

--- backend/src/main/java/skills/auth/UserSkillsGrantedAuthority.groovy
+++ backend/src/main/java/skills/auth/UserSkillsGrantedAuthority.groovy
@@ -2,10 +2,14 @@ package skills.auth
 
 import groovy.transform.Canonical
 import groovy.transform.CompileStatic
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
 import org.springframework.security.core.GrantedAuthority
 import skills.storage.model.auth.UserRole
 
-@Canonical
+//@Canonical
+@EqualsAndHashCode
+@ToString
 @CompileStatic
 class UserSkillsGrantedAuthority implements GrantedAuthority {
 
