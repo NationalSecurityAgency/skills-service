d5b4413c9e6952305d6a5284e5ff65c6544653a1
Ryan Mayo <rmmayo@evoforge.org>
2019-08-08 12:16:32 -0400

    #214 - only display global badges link in the nav bar to root users

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,24 +1,45 @@
 <template>
   <div>
-    <navigation :nav-items="[
-          {name: 'Projects', iconClass: 'fa-project-diagram', page: 'HomePage'},
-          {name: 'Badges', iconClass: 'fa-award', page: 'GlobalBadges'},
-          {name: 'Metrics', iconClass: 'fa-cogs', page: 'GlobalMetrics'}
-        ]">
+    <navigation :nav-items="navItems">
     </navigation>
   </div>
 </template>
 
 <script>
   import Navigation from './utils/Navigation';
+  import SettingsService from './settings/SettingsService';
 
   export default {
     name: 'HomePage',
     components: {
       Navigation,
     },
+    data() {
+      return {
+        isRoot: false,
+        navItems: [
+          { name: 'Projects', iconClass: 'fa-project-diagram', page: 'HomePage' },
+          { name: 'Metrics', iconClass: 'fa-cogs', page: 'GlobalMetrics' },
+        ],
+      };
+    },
+    mounted() {
+      this.loadNavItems();
+    },
+    methods: {
+      loadNavItems() {
+        SettingsService.hasRoot()
+          .then((response) => {
+            this.isRoot = response;
+            if (this.isRoot) {
+              this.navItems.splice(1, 0, { name: 'Badges', iconClass: 'fa-award', page: 'GlobalBadges' });
+            }
+          });
+      },
+    },
   };
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
