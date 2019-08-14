1320d86a806061aea8eb2e7e4a47a5015906a9a3
Christopher Moses <cjmoses@evoforge.org>
2019-08-14 13:39:33 -0400

    #213 made maxDescriptionLength validator global, removed duplicated declaration across editXYZ components

--- frontend/src/components/badges/EditBadge.vue
+++ frontend/src/components/badges/EditBadge.vue
@@ -281,17 +281,6 @@
         }, {
           immediate: false,
         });
-        Validator.extend('maxDescriptionLength', {
-          getMessage: field => `${field} cannot exceed ${self.$store.getters.config.descriptionMaxLength} characters.`,
-          validate(value) {
-            if (value.length > self.$store.getters.config.descriptionMaxLength) {
-              return false;
-            }
-            return true;
-          },
-        }, {
-          immediate: false,
-        });
       },
     },
   };
