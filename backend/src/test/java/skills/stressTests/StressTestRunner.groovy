fbd8eac6cbcf690b71876f121b61324edb37c66a
EVOFORGE\dimay <dimay@evoforge.org>
2019-08-27 14:22:30 -0400

    #253: subject description was not being returned

--- backend/src/test/java/skills/stressTests/StressTestRunner.groovy
+++ backend/src/test/java/skills/stressTests/StressTestRunner.groovy
@@ -1,9 +1,11 @@
 package skills.stressTests
 
+import spock.lang.Ignore
 import spock.lang.Specification
 
 class StressTestRunner extends Specification{
 
+    @Ignore
     def "run stress test"(){
         HitSkillsHard hitSkillsHard = new HitSkillsHard()
         hitSkillsHard.execute()
