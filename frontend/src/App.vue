6740602ab8373b3990f8d6dfeaa735c46dc1c46c
Ryan Mayo <rmmayo@evoforge.org>
2019-09-11 16:55:40 -0400

    #214 - only attempt to load global custom icons if the user has ROLE_SUPERVISOR

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -56,6 +56,9 @@
       activeProjectId() {
         this.addCustomIconCSS();
       },
+      isSupervisor() {
+        this.addCustomIconCSS();
+      },
       userInfo(newUserInfo) {
         if (newUserInfo) {
           InceptionConfigurer.configure();
