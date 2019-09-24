6039a9d991ab5a809a59f7db8d2ee7f0702e5521
Ryan Mayo <rmmayo@evoforge.org>
2019-09-24 14:11:11 -0400

    #272 - clicking on the earned badge needs to take into account whether the badge is global or not and link to the appropriate page

--- client-display/src/userSkills/badge/MyBadgesDetails.vue
+++ client-display/src/userSkills/badge/MyBadgesDetails.vue
@@ -14,7 +14,7 @@
             <div v-if="badges && badges.length > 0" class="row justify-content-md-center">
                 <div v-for="(badge, index) in badges" v-bind:key="badge.badgeId" class="col-lg-3 col-sm-6 my-2">
                     <div class="card h-100">
-                        <router-link  :to="{ name: 'badgeDetails', params: { badgeId: badge.badgeId }}" tag="div" class="card-body">
+                        <router-link  :to="{ name: badge.global ? 'globalBadgeDetails' : 'badgeDetails', params: { badgeId: badge.badgeId }}" tag="div" class="card-body">
                             <i class="fa fa-check-circle position-absolute text-success" style="right: 10px; top: 10px;"/>
                             <i v-if="badge.gem" class="fas fa-gem position-absolute" style="top: 10px; left: 10px; color: purple"></i>
                             <i v-if="badge.global" class="fas fa-globe position-absolute" style="top: 10px; left: 10px; color: blue"></i>
