9c5cbe7766c833c501b98a32b0a62d296b0186e9
EVOFORGE\dimay <dimay@evoforge.org>
2019-10-09 15:29:54 -0400

    #351: Add ability to auto load setting from properties by using the following naming convention

---
+++ backend/src/main/java/skills/services/settings/SettingsInitializingBean.groovy
@@ -0,0 +1,49 @@
+package skills.services.settings
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.event.ApplicationStartedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import skills.controller.request.model.GlobalSettingsRequest
+import skills.controller.request.model.SettingsRequest
+
+/**
+ * Auto load setting from properties by using the following naming convention:
+ * skills.store.settings.<group_name>.<setting>=<value>
+ *
+ *  for example:
+ *      skills.store.settings.public_groupName1.settingId1=valuea
+ *  would equate to setting:
+ *      group=public_groupName1, setting=settingId1, value=valuea
+ */
+@Component
+class SettingsInitializingBean {
+
+    @Autowired
+    SettingsService settingsService
+
+    @Autowired
+    DefaultSettingsToInit settingsToInit
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void onApplicationEvent() {
+        loadSettings()
+    }
+
+    private void loadSettings() {
+        List<SettingsRequest> toSave = []
+        settingsToInit.settings?.each {
+            String groupName = it?.key
+            if (groupName) {
+                it?.value?.each { def setting ->
+                    String prop = setting.key
+                    String value = setting.value
+                    toSave.add(new GlobalSettingsRequest(settingGroup: groupName, setting: prop, value: value))
+                }
+            }
+        }
+        if (toSave) {
+            settingsService.saveSettings(toSave)
+        }
+    }
+}
