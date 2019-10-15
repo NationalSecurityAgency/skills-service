4016d26ee0f4f2929a46a96e2cc3bc654027f9dc
EVOFORGE\dimay <dimay@evoforge.org>
2019-10-15 15:18:47 -0400

    #346: improve user representation in pki mode

---
+++ backend/src/test/java/skills/intTests/UserInfoSpecs.groovy
@@ -0,0 +1,30 @@
+package skills.intTests
+
+import skills.intTests.utils.DefaultIntSpec
+import skills.intTests.utils.SkillsFactory
+
+class UserInfoSpecs extends DefaultIntSpec {
+
+    def "get user info"() {
+        String user = "UserInfoSpecsUserA"
+        createService(user)
+
+        def proj1 = SkillsFactory.createProject(1)
+        def proj1_subj = SkillsFactory.createSubject(1, 1)
+        List<Map> proj1_skills = SkillsFactory.createSkills(3, 1, 1)
+
+        skillsService.createProject(proj1)
+        skillsService.createSubject(proj1_subj)
+        skillsService.createSkills(proj1_skills)
+
+        skillsService.addSkill([projectId: proj1.projectId, skillId: proj1_skills.get(0).skillId], user, new Date())
+
+        when:
+        def userInfo = skillsService.getUserInfoForProject(proj1.projectId, user)
+        then:
+        userInfo
+        userInfo.userId == user.toLowerCase()
+        userInfo.userIdForDisplay == user
+    }
+
+}
