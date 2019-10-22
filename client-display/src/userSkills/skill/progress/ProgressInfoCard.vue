2a3890c0b54e994e2fe1d3bdff25d963766c9d5c
Matthew Kirkley <mtkirkl@evoforge.org>
2019-10-22 18:00:46 -0400

    #358 truncate text in card and a little compromise on text size so small and large cards look ok

--- client-display/src/userSkills/skill/progress/ProgressInfoCard.vue
+++ client-display/src/userSkills/skill/progress/ProgressInfoCard.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="card h-100">
-        <div class="card-body container bg-light">
-            <div class="media">
-                <div class="d-inline-block mt-2 mr-3">
-                    <i class="fa-3x" :class="icon"/>
+            <div class="card-body px-2 bg-light">
+                <div class="media">
+                    <div class="d-inline-block mt-2 mr-3">
+                        <i class="fa-3x" :class="icon"/>
+                    </div>
+                    <div class="media-body text-truncate text-primary">
+                        <h4 class="mb-0 text-truncate">
+                            <span :title="title | number">{{title | number}}</span>
+                        </h4>
+                        <div class="mt-0 pt-0">{{subTitle}}</div>
+                    </div>
                 </div>
-                <div class="media-body text-primary">
-                    <h3 class="mb-0">
-                        <span>{{title | number}}</span>
-                    </h3>
-                    <div class="mt-0 pt-0">{{subTitle}}</div>
-                </div>
+                <div class="mt-2">{{label}}</div>
             </div>
-            <div class="mt-2">{{label}}</div>
-        </div>
     </div>
 </template>
 
@@ -31,7 +31,3 @@
         },
     };
 </script>
-
-<style scoped>
-
-</style>
