7af19b0f96f0c62083e1449c917cc892e82c81f7
EVOFORGE\dimay <dimay@evoforge.org>
2020-01-03 13:41:52 -0500

    #447: initial take on some basic cypress.io setup

--- backend/src/test/java/skills/intTests/cypress/CypressSpecs.groovy
+++ backend/src/test/java/skills/intTests/cypress/CypressSpecs.groovy
@@ -2,9 +2,11 @@ package skills.intTests.cypress
 
 
 import skills.intTests.utils.SkillsService
+import spock.lang.Ignore
 
 class CypressSpecs extends CypressIntSpec {
 
+    @Ignore
     def "CYPRESS TEST: login_spec.js" () {
         SkillsService myService = createService('dimay@evoforge.org', 'password')
         myService.createProject([projectId: "ProjectA", name: "ProjectA"])
@@ -14,6 +16,7 @@ class CypressSpecs extends CypressIntSpec {
         true
     }
 
+    @Ignore
     def "CYPRESS TEST: project_management_spec.js"() {
         when:
         runCypress()
