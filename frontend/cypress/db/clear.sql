e6021afa61eacc2844f629ac8868d4017bb7e073
EVOFORGE\dimay <dimay@evoforge.org>
2020-01-06 09:22:55 -0500

    #447: configuring CI

---
+++ frontend/cypress/db/clear.sql
@@ -0,0 +1,2 @@
+delete from PROJECT_DEFINITION;
+delete from USER_ATTRS;
--- frontend/cypress/integration/login_spec.js
+++ frontend/cypress/integration/login_spec.js
@@ -1,6 +1,17 @@
 describe('Login Tests', () => {
 
   beforeEach(() => {
+    cy.exec('npm version', {failOnNonZeroExit: false})
+    cy.exec('npm run backend:clearDb')
+    cy.request('PUT', '/createAccount', {
+      firstName: 'Person',
+      lastName : 'OneTwo',
+      email    : 'root@skills.org',
+      password : 'password',
+    })
+    cy.request('POST', '/grantFirstRoot');
+    cy.request('POST', '/logout');
+
     cy.server()
       .route('GET', '/app/projects').as('getProjects')
       .route('GET', '/api/icons/customIconCss').as('getProjectsCustomIcons')
@@ -11,7 +22,7 @@ describe('Login Tests', () => {
   it('form: successful dashboard login', () => {
     cy.visit('/');
 
-    cy.get('#username').type('dimay@evoforge.org');
+    cy.get('#username').type('root@skills.org');
     cy.get('#inputPassword').type('password');
     cy.contains('Login').click();
 
@@ -20,13 +31,13 @@ describe('Login Tests', () => {
 
     cy.contains('Project');
     cy.contains('My Projects');
-    cy.contains('ProjectA');
+    cy.contains('Inception');
   });
 
   it('form: bad password', () => {
     cy.visit('/');
 
-    cy.get('#username').type('dimay@evoforge.org');
+    cy.get('#username').type('root@skills.org');
     cy.get('#inputPassword').type('password1');
     cy.contains('Login').click();
     cy.wait('@postPerformLogin');
@@ -37,7 +48,7 @@ describe('Login Tests', () => {
   it('form: bad user', () => {
     cy.visit('/');
 
-    cy.get('#username').type('dimay1@evoforge.org');
+    cy.get('#username').type('root1@skills.org');
     cy.get('#inputPassword').type('password');
     cy.contains('Login').click();
     cy.wait('@postPerformLogin');
