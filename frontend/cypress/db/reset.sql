840f5a6cf69cb0c6d9a93be672bf75992355166e
EVOFORGE\dimay <dimay@evoforge.org>
2020-01-06 15:11:38 -0500

    #447: cleaning up

--- frontend/cypress/db/reset.sql
+++
@@ -1,3 +0,0 @@
--- creating Inception project is expensive so lets not delete it
-delete from PROJECT_DEFINITION where project_id <> 'Inception';
-delete from USER_ATTRS where user_id <> 'root@skills.org';
