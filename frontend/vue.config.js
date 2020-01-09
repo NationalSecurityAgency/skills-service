430ffdbd8e2cffb9188e84aefd5ef1bad459ce72
Ryan Mayo <rmmayo@evoforge.org>
2020-01-09 13:27:18 -0500

    #48 - added workaround to get frontend app to resolve modules that have been added using 'npm link'.  See https://github.com/webpack/webpack/issues/811 and https://gist.github.com/diachedelic/6ded48f5c6442482fa69e91ec7ab1742

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -26,6 +26,31 @@ const proxyConf = {
   logLevel: 'debug',
 };
 
+// absolute paths to all symlinked modules inside `nodeModulesPath`
+// adapted from https://github.com/webpack/webpack/issues/811#issuecomment-405199263
+const findLinkedModules = (nodeModulesPath) => {
+  const modules = []
+
+  fs.readdirSync(nodeModulesPath).forEach(dirname => {
+    const modulePath = path.resolve(nodeModulesPath, dirname)
+    const stat = fs.lstatSync(modulePath)
+
+    if (dirname.startsWith('.')) {
+      // not a module or scope, ignore
+    } else if (dirname.startsWith('@')) {
+      // scoped modules
+      modules.push(...findLinkedModules(modulePath))
+    } else if (stat.isSymbolicLink()) {
+      const realPath = fs.realpathSync(modulePath)
+      const realModulePath = path.resolve(realPath, 'node_modules')
+
+      modules.push(realModulePath)
+    }
+  })
+
+  return modules
+};
+
 module.exports = {
   devServer: {
     host: 'localhost',
@@ -57,6 +82,19 @@ module.exports = {
       alias: {
         '@$': resolve('src'),
       },
+      // work with npm link
+      // see https://github.com/webpack/webpack/issues/985
+      // see https://github.com/vuejs-templates/webpack/pull/688
+      symlinks: false,
+      modules: [
+        // provide absolute path to the main node_modules,
+        // to avoid webpack searching around and getting confused
+        // see https://webpack.js.org/configuration/resolve/#resolve-modules
+        path.resolve('node_modules'),
+        // include linked node_modules as fallback, in case the deps haven't
+        // yet propagated to the main node_modules
+        ...findLinkedModules(path.resolve('node_modules')),
+      ],
     },
     devtool: 'cheap-module-eval-source-map',
     // optimization: { minimize: false },
