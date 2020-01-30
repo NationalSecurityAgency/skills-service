9fa65ee2776f9a74121854f0a838dbd90ff91055
EVOFORGE\dimay <dimay@evoforge.org>
2020-01-30 13:38:33 -0500

    #458: added back button to client display

--- client-display/src/common/utilities/SkillsTitle.vue
+++ client-display/src/common/utilities/SkillsTitle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="py-2 mt-2 mb-3 card-body skills-page-title-text-color card rounded bg-white text-info text-uppercase">
-        <div v-if="backButton" class="position-fixed">
+        <div v-if="backButton" class="position-absolute">
             <h2><button @click="navigateBack" type="button" class="btn btn-outline-info skills-page-title-text-color" data-cy="back">
                     <i class="fas fa-arrow-left"></i>
                 </button>
