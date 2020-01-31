70730cfa6b468e20ecaf3096bd1f2fd36d49fd67
EVOFORGE\dimay <dimay@evoforge.org>
2020-01-31 11:59:34 -0500

    #459: improve navigable components look-and-feel - WIP - working out theming

--- client-display/src/common/utilities/SkillsTitle.vue
+++ client-display/src/common/utilities/SkillsTitle.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="py-2 mt-2 mb-3 card-body skills-page-title-text-color card rounded bg-white text-info text-uppercase">
         <div v-if="backButton" class="position-absolute">
-            <h2><button @click="navigateBack" type="button" class="btn btn-outline-info skills-page-title-text-color" data-cy="back">
+            <h2><button @click="navigateBack" type="button" class="btn btn-outline-info" data-cy="back">
                     <i class="fas fa-arrow-left"></i>
                 </button>
             </h2>
@@ -26,3 +26,15 @@
         },
     };
 </script>
+
+<style>
+    .skills-page-title-text-color button {
+        color: white;
+        border-color: white;
+    }
+    .skills-page-title-text-color button:hover {
+        color: #626d7d;
+        background-color: white;
+        border-color: white;
+    }
+</style>
