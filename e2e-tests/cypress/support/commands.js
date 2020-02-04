0942cb67cd3dbe4bec4cdfcbc598f4c5f0c904a0
EVOFORGE\dimay <dimay@evoforge.org>
2020-02-04 10:57:53 -0500

    #459: improve navigable components look-and-feel - ci

--- e2e-tests/cypress/support/commands.js
+++ e2e-tests/cypress/support/commands.js
@@ -103,4 +103,12 @@ Cypress.Commands.add("getIdField", () => {
 });
 
 
+Cypress.Commands.add("setResolution", (size) => {
+    if (Cypress._.isArray(size)) {
+        cy.viewport(size[0], size[1]);
+    } else {
+        cy.viewport(size);
+    }
+});
+
 
