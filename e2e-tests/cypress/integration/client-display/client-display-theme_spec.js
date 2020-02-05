74bd8c03bfc475ff567ccc35beeec06468e53e88
EVOFORGE\dimay <dimay@evoforge.org>
2020-02-05 09:00:09 -0500

    #459: improve navigable components look-and-feel - ci

--- e2e-tests/cypress/integration/client-display/client-display-theme_spec.js
+++ e2e-tests/cypress/integration/client-display/client-display-theme_spec.js
@@ -2,16 +2,12 @@ describe('Client Display Tests', () => {
 
     const snapshotOptions = {
         blackout: ['[data-cy=pointHistoryChart]'],
-        // failureThreshold: 0.03, // threshold for entire image
-        // customDiffConfig: { threshold: 0.5 }, // threshold for each pixel
     };
-    const resolution = 'ipad-2'; // [1000, 2000];
     const sizes = [
-        ['iphone-6', 'landscape'],
         'iphone-6',
         'ipad-2',
-        ['ipad-2', 'landscape'],
-        [1200, 1080],
+        'default',
+        // [1200, 1080],
     ];
 
     before(() => {
@@ -147,79 +143,94 @@ describe('Client Display Tests', () => {
             projectId: 'proj1',
             name: 'proj1'
         });
-        // cy.setResolution(resolution);
-        // cy.viewport(resolution);
-    });
-
-    it('test theming', () => {
-        cy.cdInitProjWithSkills();
-
-        cy.cdVisit('/?enableTheme=true')
-        // hex #626d7d = rgb(98, 109, 125)
-        cy.get("#app").should('have.css', 'background-color')
-            .and('equal', 'rgb(98, 109, 125)');
-
-        cy.get('[data-cy=pointHistoryChart]')
-
-        cy.wait(1000);
-        cy.matchImageSnapshot('Project-Overview', snapshotOptions);
-
-        // back button - border color
-        cy.cdClickRank();
-        // THEME: "pageTitleTextColor": "#fdfbfb",
-        cy.get('[data-cy=back]').should('have.css', 'border-color')
-            .and('equal', 'rgb(253, 251, 251)');
-        cy.get('[data-cy=back]').should('have.css', 'color')
-            .and('equal', 'rgb(253, 251, 251)');
-
-        // wait for the bar (on the bar chart) to render
-        cy.wait(1000);
-        cy.contains('You are Level 2!');
-        cy.matchImageSnapshot('Project-Rank', snapshotOptions);
-
-        cy.cdBack();
-        cy.cdClickBadges();
-        cy.wait(1000);
-        cy.matchImageSnapshot('Badges', snapshotOptions);
-
-        cy.contains('View Details').click()
-        cy.wait(1000);
-        cy.matchImageSnapshot('Badge-Details', snapshotOptions);
-
-        cy.cdBack('Badges');
-        cy.cdBack();
-        cy.cdClickSubj(0);
-        cy.contains('Subject 1')
-        cy.wait(1000);
-        cy.matchImageSnapshot('Subject0-Overview', snapshotOptions);
-
-        cy.get('[data-cy=toggleSkillDetails]').click()
-        cy.contains('Lorem ipsum dolor sit amet')
-        cy.wait(1000);
-        cy.matchImageSnapshot('Subject0-Overview-WithSkillDetails', snapshotOptions);
-
-        cy.cdClickSkill(0);
-        cy.contains('Skill Overview')
-        cy.wait(1000);
-        cy.matchImageSnapshot('Subject0-Skill0-Details', snapshotOptions);
-
-        cy.cdBack('Subject 1');
-        cy.cdClickSkill(3);
-        cy.contains('Skill Overview')
-        cy.wait(1000);
-        cy.matchImageSnapshot('Subject0-Skill3-Details', snapshotOptions);
-
     });
 
     sizes.forEach((size) => {
-        it(`test theming - No Subjects - ${size}`, () => {
+        it(`test theming - ${size}`, () => {
             cy.setResolution(size);
 
+            cy.cdInitProjWithSkills();
+
             cy.cdVisit('/?enableTheme=true')
-            cy.contains('User Skills');
+            // hex #626d7d = rgb(98, 109, 125)
+            cy.get("#app").should('have.css', 'background-color')
+                .and('equal', 'rgb(98, 109, 125)');
+
+            cy.get('[data-cy=pointHistoryChart]')
+
+            cy.contains('Subject 3');
+            cy.get('.user-skill-subject-tile:nth-child(1)').contains('Subject 1');
+            cy.get('[data-cy=myRank]').contains('1');
+            cy.get('[data-cy=myBadges]').contains('1 Badge')
+            cy.matchImageSnapshot(`Project-Overview_${size}`, snapshotOptions);
+
+            // back button - border color
+            cy.cdClickRank();
+            // THEME: "pageTitleTextColor": "#fdfbfb",
+            cy.get('[data-cy=back]').should('have.css', 'border-color')
+                .and('equal', 'rgb(253, 251, 251)');
+            cy.get('[data-cy=back]').should('have.css', 'color')
+                .and('equal', 'rgb(253, 251, 251)');
+
+            // wait for the bar (on the bar chart) to render
             cy.wait(1000);
-            cy.matchImageSnapshot(`Project-Overview-No_Subjects_${size}`, snapshotOptions);
+            cy.contains('You are Level 2!');
+            cy.matchImageSnapshot(`Project-Rank_${size}`, snapshotOptions);
+
+            cy.cdBack();
+            cy.cdClickBadges();
+            cy.contains('Badge 3')
+            cy.contains('a few seconds ago')
+            cy.matchImageSnapshot(`Badges_${size}`, snapshotOptions);
+
+            cy.contains('View Details').click()
+            cy.contains('Badge 1');
+            cy.contains('This is 3');
+            cy.matchImageSnapshot(`Badge-Details_${size}`, snapshotOptions);
+
+            cy.cdBack('Badges');
+            cy.cdBack();
+            cy.cdClickSubj(0);
+            cy.contains('Subject 1')
+            cy.get('[data-cy=myRank]').contains('1');
+            cy.contains('This is 4');
+            cy.contains('200 Points earned Today');
+            cy.matchImageSnapshot(`Subject0-Overview_${size}`, snapshotOptions);
+
+            cy.get('[data-cy=toggleSkillDetails]').click();
+            cy.get('[data-cy=myRank]').contains('1');
+            cy.contains('Lorem ipsum dolor sit amet');
+            cy.contains('Skill has 1 direct dependent(s).');
+            cy.contains('200 Points earned Today');
+            cy.contains('Description');
+            cy.matchImageSnapshot(`Subject0-Overview-WithSkillDetails_${size}`, snapshotOptions);
+
+            cy.cdClickSkill(0);
+            cy.contains('Skill Overview')
+            cy.contains('This is 1');
+            cy.contains('Lorem ipsum dolor sit amet');
+            cy.matchImageSnapshot(`Subject0-Skill0-Details_${size}`, snapshotOptions);
+
+            cy.cdBack('Subject 1');
+            cy.cdClickSkill(3);
+            cy.contains('Skill Overview')
+            cy.contains('This is 4');
+            cy.contains('Lorem ipsum dolor sit amet');
+            cy.contains('Achieved Dependencies');
+            // wait for graph to finish animating
+            cy.wait(4000);
+            cy.matchImageSnapshot(`Subject0-Skill3-Details_${size}`, snapshotOptions);
         });
+
+    });
+
+    it(`test theming - No Subjects`, () => {
+        cy.cdVisit('/?enableTheme=true')
+        cy.contains('User Skills');
+        cy.get('[data-cy=myRank]').contains('1');
+        cy.contains('0 Points earned Today');
+        cy.contains('Subjects have not been added yet.');
+        cy.matchImageSnapshot(`Project-Overview-No_Subjects`, snapshotOptions);
     });
 
 
@@ -240,7 +251,9 @@ describe('Client Display Tests', () => {
         cy.cdClickSubj(0);
         cy.contains('Subject 1');
         cy.contains('Skills have not been added yet.')
-        cy.wait(1000);
+        cy.get('[data-cy=myRank]').contains('1');
+        cy.contains('0 Points earned Today');
+        cy.contains('Description');
         cy.matchImageSnapshot('Project-Overview-Empty_Subject', snapshotOptions);
     });
 
