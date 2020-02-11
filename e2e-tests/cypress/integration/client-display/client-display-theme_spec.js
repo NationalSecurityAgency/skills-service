ee75f80386d314a8d80b1d4dcee2a47d0fef1717
EVOFORGE\dimay <dimay@evoforge.org>
2020-02-11 12:19:58 -0500

    #479: upgrade cypress

--- e2e-tests/cypress/integration/client-display/client-display-theme_spec.js
+++ e2e-tests/cypress/integration/client-display/client-display-theme_spec.js
@@ -2,6 +2,10 @@ describe('Client Display Tests', () => {
 
     const snapshotOptions = {
         blackout: ['[data-cy=pointHistoryChart]'],
+        failureThreshold: 0.03, // threshold for entire image
+        failureThresholdType: 'percent', // percent of image or number of pixels
+        customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
+        capture: 'viewport', // capture viewport in screenshot
     };
     const sizes = [
         'iphone-6',
