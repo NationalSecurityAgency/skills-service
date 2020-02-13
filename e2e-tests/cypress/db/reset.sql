d767faee2604fb59d3503e5fd6b8db1552a1460c
Christopher Moses <cjmoses@evoforge.org>
2020-02-13 12:56:54 -0500

    #483 changing ids used to determine if root user should be added to table

--- e2e-tests/cypress/db/reset.sql
+++ e2e-tests/cypress/db/reset.sql
@@ -1,5 +1,6 @@
 -- creating Inception project is expensive so lets not delete it
 delete from PROJECT_DEFINITION where PROJECT_ID <> 'Inception';
 delete from USER_ATTRS where USER_ID <> 'root@skills.org' and USER_ID <> 'skills@skills.org';
+delete from USER_ROLES where USER_ID = 'skills@skills.org' and ROLE_NAME = 'ROLE_SUPER_DUPER_USER';
 delete from GLOBAL_BADGE_LEVEL_DEFINITION;
 delete from SKILL_DEFINITION where PROJECT_ID is null;
