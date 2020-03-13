5dc0d363658f5b692c1b8821e770a6f2e0130393
Christopher Moses <cjmoses@evoforge.org>
2020-03-13 16:31:06 -0400

    #469 add tests for forward slash to other user suggest endpoints

--- e2e-tests/cypress/integration/settings_spec.js
+++ e2e-tests/cypress/integration/settings_spec.js
@@ -26,10 +26,17 @@ describe('Settings Tests', () => {
         cy.server();
         cy.route('POST', '/root/users/without/role/ROLE_SUPER_DUPER_USER?userSuggestOption=ONE').as('getEligibleForRoot');
         cy.route('PUT', '/root/users/skills@skills.org/roles/ROLE_SUPER_DUPER_USER').as('addRoot');
+        cy.route({
+            method: 'GET',
+            url: '/app/projects'
+        }).as('loadProjects');
+        cy.route({method: 'GET', url: '/root/isRoot'}).as('checkRoot');
+
         cy.visit('/');
         cy.contains('My Projects');
         cy.get('button.dropdown-toggle').first().click({force: true});
         cy.contains('Settings').click();
+        cy.wait('@checkRoot');
         cy.contains('Security').click();
         cy.contains('Enter user id').first().type('sk{enter}');
         cy.wait('@getEligibleForRoot');
@@ -39,13 +46,40 @@ describe('Settings Tests', () => {
         cy.get('div.table-responsive').contains('Firstname LastName (skills@skills.org)');
     });
 
+    it('Add Root User - forward slash character does not cause error', () => {
+        cy.server();
+        cy.route('POST', '/root/users/without/role/ROLE_SUPER_DUPER_USER?userSuggestOption=ONE').as('getEligibleForRoot');
+        cy.route('PUT', '/root/users/skills@skills.org/roles/ROLE_SUPER_DUPER_USER').as('addRoot');
+        cy.route({
+            method: 'GET',
+            url: '/app/projects'
+        }).as('loadProjects');
+        cy.route({method: 'GET', url: '/root/isRoot'}).as('checkRoot');
+
+        cy.visit('/');
+        cy.contains('My Projects');
+        cy.get('button.dropdown-toggle').first().click({force: true});
+        cy.contains('Settings').click();
+        cy.wait('@checkRoot');
+        cy.contains('Security').click();
+        cy.contains('Enter user id').first().type('sk/foo{enter}');
+        cy.wait('@getEligibleForRoot');
+    });
+
     it('Add Root User With No Query', () => {
         cy.server();
         cy.route('POST', '/root/users/without/role/ROLE_SUPER_DUPER_USER?userSuggestOption=ONE').as('getEligibleForRoot');
         cy.route('PUT', '/root/users/skills@skills.org/roles/ROLE_SUPER_DUPER_USER').as('addRoot');
+        cy.route({
+            method: 'GET',
+            url: '/app/projects'
+        }).as('loadProjects');
+        cy.route({method: 'GET', url: '/root/isRoot'}).as('checkRoot');
+
         cy.visit('/');
         cy.get('button.dropdown-toggle').first().click({force: true});
         cy.contains('Settings').click();
+        cy.wait('@checkRoot');
         cy.contains('Security').click();
         cy.contains('Enter user id').first().type('{enter}');
         cy.wait('@getEligibleForRoot');
@@ -56,16 +90,23 @@ describe('Settings Tests', () => {
     });
 
     it('Add Supervisor User', () => {
-        cy.visit('/');
-        cy.contains('My Projects');
         cy.server();
         cy.route('PUT', '/root/users/root@skills.org/roles/ROLE_SUPERVISOR').as('addSupervisor');
         cy.route('POST', 'root/users/without/role/ROLE_SUPERVISOR?userSuggestOption=ONE').as('getEligibleForSupervisor');
+        cy.route({
+            method: 'GET',
+            url: '/app/projects'
+        }).as('loadProjects');
+        cy.route({method: 'GET', url: '/root/isRoot'}).as('checkRoot');
+
+        cy.visit('/');
+        cy.contains('My Projects');
 
         cy.get('li').contains('Badges').should('not.exist');
         cy.vuex().its('state.access.isSupervisor').should('equal', false);
         cy.get('button.dropdown-toggle').first().click({force: true});
         cy.contains('Settings').click();
+        cy.wait('@checkRoot');
         cy.contains('Security').click();
         cy.get('[data-cy=supervisorrm]  div.multiselect__tags').type('root');
         cy.wait('@getEligibleForSupervisor');
@@ -79,24 +120,26 @@ describe('Settings Tests', () => {
     });
 
     it('Add Supervisor User No Query', () => {
-        cy.visit('/');
+
         cy.server();
         cy.route('PUT', '/root/users/root@skills.org/roles/ROLE_SUPERVISOR').as('addSupervisor');
         cy.route('POST', 'root/users/without/role/ROLE_SUPERVISOR?userSuggestOption=ONE').as('getEligibleForSupervisor');
+        cy.route({
+            method: 'GET',
+            url: '/app/projects'
+        }).as('loadProjects');
+
+        cy.route({method: 'GET', url: '/root/isRoot'}).as('checkRoot');
+
+        cy.visit('/');
 
         cy.get('li').contains('Badges').should('not.exist');
         cy.vuex().its('state.access.isSupervisor').should('equal', false);
         cy.get('button.dropdown-toggle').first().click({force: true});
         cy.contains('Settings').click();
+        cy.wait('@checkRoot');
         cy.contains('Security').click();
-        cy.get('[data-cy=supervisorrm]  div.multiselect__tags').type('{enter}');
+        cy.get('[data-cy=supervisorrm]  div.multiselect__tags').type('foo/bar{enter}');
         cy.wait('@getEligibleForSupervisor');
-        cy.get('[data-cy=supervisorrm]').contains('root@skills.org').click();
-        cy.get('[data-cy=supervisorrm]').contains('Add').click();
-        cy.wait('@addSupervisor');
-        cy.get('div.table-responsive').contains('Firstname LastName (root@skills.org)');
-        cy.vuex().its('state.access.isSupervisor').should('equal', true);
-        cy.contains('Home').click();
-        cy.get('[data-cy=navigationmenu]').contains('Badges', {timeout: 5000}).should('be.visible');
     });
 });
