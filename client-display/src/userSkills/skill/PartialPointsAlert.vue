655081c2d142d02d380eabf86e94a2505cad8e16
EVOFORGE\dimay <dimay@evoforge.org>
2020-03-31 11:33:15 -0400

    #455: improved display of locked skills that were already fully achieved

--- client-display/src/userSkills/skill/PartialPointsAlert.vue
+++ client-display/src/userSkills/skill/PartialPointsAlert.vue
@@ -14,12 +14,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 <template>
-    <div v-if="isLocked && skill.points > 0" class="alert alert-info mt-2 text-center">
-        <p>
-            <i class="fas fa-info-circle mr-1"></i> You were able to earn partial points before dependencies were added. Don't worry you get to keep those points!!!
-        </p>
-        <hr>
-        <div class="text-center">Accomplish all of the dependencies to unlock the rest of this skill's points!</div>
+    <div v-if="isPartiallyAchievedAndLocked || isFullyAchievedAndLocked">
+        <div v-if="isPartiallyAchievedAndLocked" class="alert alert-info mt-2 text-center">
+            <p>
+                <i class="fas fa-info-circle mr-1"></i> You were able to earn partial points before the dependencies were added. Don't worry you get to keep the points!!!
+            </p>
+            <hr>
+            <div class="text-center">Accomplish all of the dependencies to unlock the rest of the skill's points!</div>
+        </div>
+
+        <div v-if="isFullyAchievedAndLocked" class="alert alert-success mt-2 text-center">
+            <p>
+                <i class="fas fa-info-circle mr-1"></i> Congrats! You completed this skill before the dependencies were added. Don't worry you get to keep the points!!!
+            </p>
+        </div>
     </div>
 </template>
 
@@ -29,6 +37,14 @@ limitations under the License.
         props: {
             skill: Object,
             isLocked: Boolean,
+        },
+        computed: {
+            isPartiallyAchievedAndLocked() {
+                return this.isLocked && this.skill.points > 0 && this.skill.points !== this.skill.totalPoints;
+            },
+            isFullyAchievedAndLocked() {
+                return this.isLocked && this.skill.points === this.skill.totalPoints;
+            },
         },
     };
 </script>
