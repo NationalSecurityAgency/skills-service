d6bf927e680f41ac4847abb6df16aae0a2ee4b91
EVOFORGE\dimay <dimay@evoforge.org>
2020-04-01 07:02:48 -0400

    #455: improving accuracy of snapshots - WIP

--- e2e-tests/cypress/integration/client-display/client-display-features_spec.js
+++ e2e-tests/cypress/integration/client-display/client-display-features_spec.js
@@ -22,7 +22,6 @@ describe('Client Display Features Tests', () => {
         // failureThreshold: 0.03, // threshold for entire image
         // failureThresholdType: 'percent', // percent of image or number of pixels
         // customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
-        // capture: 'viewport', // capture viewport in screenshot
     };
 
     beforeEach(() => {
