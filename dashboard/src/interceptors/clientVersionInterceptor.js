77de569fbd1622c2fb1fbb188e49b46b508e2faa
rmmayo <rmmayo@evoforge.org>
2020-07-06 14:07:20 -0400

    #526 - guard against null config which can happen on HTTP 401

--- dashboard/src/interceptors/clientVersionInterceptor.js
+++ dashboard/src/interceptors/clientVersionInterceptor.js
@@ -17,8 +17,10 @@ import axios from 'axios';
 import store from '../store/store';
 
 function handleFunction(config) {
-  const incomingVersion = config.headers['skills-client-lib-version'];
-  store.dispatch('updateLibVersionIfDifferent', incomingVersion);
+  if (config) {
+    const incomingVersion = config.headers['skills-client-lib-version'];
+    store.dispatch('updateLibVersionIfDifferent', incomingVersion);
+  }
 }
 
 // apply interceptor on response
