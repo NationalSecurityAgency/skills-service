252258a3eccf438157fcf63ebd8790af8f6edd94
EVOFORGE\dimay <dimay@evoforge.org>
2020-07-30 13:08:16 -0400

    #28: Create and publish skills-service docker image

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -4,16 +4,16 @@ set -e
 
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 VERSION=$(cd ../ && mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
-JAR_LOC=$(ls ../backend/target/*.jar)
+JAR_LOC=$(ls ../service/target/*.jar)
 if [ "$JAR_LOC" = "" ]
 then
   echo "Failed to find jar"
   exit
 fi
-rm skills-service.jar
+rm -f skills-service.jar
 cp $JAR_LOC skills-service.jar
 
-VCS_REF=$(git rev-parse $VERSION)
+#VCS_REF=$(git rev-parse $VERSION)
 
 echo "-------------------"
 echo "BUILD_DATE=[$BUILD_DATE]"
