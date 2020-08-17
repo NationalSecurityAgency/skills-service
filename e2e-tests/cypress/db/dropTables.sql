2428348def26cea95c046080cc7eb79970330cc7
EVOFORGE\dimay <dimay@evoforge.org>
2020-08-17 10:19:44 -0400

    #53: PostgreSQL Cypress CI were not running against PostgreSQL

---
+++ e2e-tests/cypress/db/dropTables.sql
@@ -0,0 +1,49 @@
+-- Copyright 2020 SkillTree
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- creating Inception project is expensive so lets not delete it
+drop table databasechangeloglock;
+
+drop table databasechangelog;
+
+drop table skills_db_locks;
+
+drop table skill_relationship_definition;
+
+drop table skill_share_definition;
+
+drop table user_roles;
+
+drop table user_performed_skill;
+
+drop table user_points;
+
+drop table user_achievement;
+
+drop table settings;
+
+drop table global_badge_level_definition;
+
+drop table level_definition;
+
+drop table skill_definition;
+
+drop table custom_icons;
+
+drop table project_definition;
+
+drop table password_reset_token;
+
+drop table users;
+
+drop table user_attrs;
