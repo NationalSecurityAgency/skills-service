2616ab749d06e545b7e7d40cb68c92e5c548665b
mosefer <cjmoses@evoforge.org>
2020-08-17 08:35:21 -0400

    #56 remove colons from datestamp in tag

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -3,6 +3,7 @@
 set -e
 
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+BUILD_DATE_TAG=$(date -u +'%Y%m%dT%H%M%SZ')
 VERSION=$(cd ../ && mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
 JAR_LOC=$(ls ../service/target/*.jar)
 IMG_NAME=${1:-"skilltree/skills-service"}
@@ -22,4 +23,4 @@ echo "VCS_REF=[$VCS_REF]"
 echo "IMG_NAME=[$IMG_NAME]"
 echo "-------------------"
 
-docker build --no-cache=true --build-arg BUILD_DATE=$BUILD_DATE --build-arg VERSION=$VERSION --build-arg VCS_REF=$GITHUB_SHA -t $IMG_NAME -t "${IMG_NAME}:${VERSION}_${BUILD_DATE}" .
+docker build --no-cache=true --build-arg BUILD_DATE=$BUILD_DATE --build-arg VERSION=$VERSION --build-arg VCS_REF=$GITHUB_SHA -t $IMG_NAME -t "${IMG_NAME}:${VERSION}_${BUILD_DATE_TAG}" .
