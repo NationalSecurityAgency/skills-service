c18280ecc45e3a23b8ade59e32ac90c0e12f647a
EVOFORGE\dimay <dimay@evoforge.org>
2020-09-22 10:16:34 -0400

    #106: integration tests

--- service/src/test/java/skills/intTests/reportSkills/ReportSkills_AchievementsSpecs.groovy
+++ service/src/test/java/skills/intTests/reportSkills/ReportSkills_AchievementsSpecs.groovy
@@ -15,13 +15,14 @@
  */
 package skills.intTests.reportSkills
 
-
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import skills.intTests.utils.DefaultIntSpec
 import skills.intTests.utils.SkillsFactory
 import skills.storage.model.UserAchievement
 import skills.storage.repos.UserAchievedLevelRepo
 
+@Slf4j
 class ReportSkills_AchievementsSpecs extends DefaultIntSpec {
 
     String projId = SkillsFactory.defaultProjId
@@ -210,5 +211,191 @@ class ReportSkills_AchievementsSpecs extends DefaultIntSpec {
         }
     }
 
+    def "user the latest event when the badge is achieved"() {
+        def proj = SkillsFactory.createProject()
+        def subj = SkillsFactory.createSubject()
+        def badge = SkillsFactory.createBadge()
+        def skills = SkillsFactory.createSkills(2,)
+        skills[0].pointIncrement = 200
+        skills[0].numPerformToCompletion = 2
+        skills[1].pointIncrement = 2000
+
+        skillsService.createProject(proj)
+        skillsService.createSubject(subj)
+        skillsService.createSkills(skills)
+        skillsService.createBadge(badge)
+        skillsService.assignSkillToBadge(proj.projectId, badge.badgeId, skills[0].skillId)
+        skillsService.assignSkillToBadge(proj.projectId, badge.badgeId, skills[1].skillId)
+
+        Date date1 = new Date() - 60
+        Date date2 = new Date() - 30
+        when:
+        String user1 = "user1"
+        skillsService.addSkill([projectId: projId, skillId: skills[0].skillId], user1, date2)
+        skillsService.addSkill([projectId: projId, skillId: skills[0].skillId], user1, date1)
+        def res = skillsService.addSkill([projectId: projId, skillId: skills[1].skillId], user1, date1)
+
+        String user2 = "user2"
+        skillsService.addSkill([projectId: projId, skillId: skills[0].skillId], user2, date2)
+        skillsService.addSkill([projectId: projId, skillId: skills[0].skillId], user2, date1)
+        def resuser2 = skillsService.addSkill([projectId: projId, skillId: skills[1].skillId], user2, date1)
+
+        String user3 = "user3"
+        skillsService.addSkill([projectId: projId, skillId: skills[0].skillId], user3, date1)
+        skillsService.addSkill([projectId: projId, skillId: skills[0].skillId], user3, date2)
+        def resuser3 = skillsService.addSkill([projectId: projId, skillId: skills[1].skillId], user3, date2)
+
+        List<UserAchievement> achievements = userAchievementRepo.findAll()
+        then:
+        res.body.completed.findAll { it.type == "Badge" }
+        resuser2.body.completed.findAll { it.type == "Badge" }
+        resuser3.body.completed.findAll { it.type == "Badge" }
+
+        achievements.find { it.userId == user1 && it.skillId == badge.skillId }.achievedOn == date2
+        achievements.find { it.userId == user2 && it.skillId == badge.skillId }.achievedOn == date2
+        achievements.find { it.userId == user3 && it.skillId == badge.skillId }.achievedOn == date2
+    }
+
+    def "achieved date for levels should come from the latest skill event - multiple skills and users"() {
+        def proj = SkillsFactory.createProject()
+        def subj = SkillsFactory.createSubject()
+        def subj2 = SkillsFactory.createSubject(1, 2)
+        def skills = SkillsFactory.createSkills(3,)
+        skills.each {
+            it.pointIncrement = 200
+            it.numPerformToCompletion = 10
+        }
+
+        def skills_subj2 = SkillsFactory.createSkills(3, 1, 2)
+        skills_subj2.each {
+            it.pointIncrement = 200
+            it.numPerformToCompletion = 10
+        }
+
+        skillsService.createProject(proj)
+        skillsService.createSubject(subj)
+        skillsService.createSubject(subj2)
+        skillsService.createSkills(skills)
+        skillsService.createSkills(skills_subj2)
+
+        List<Date> dates = (1..10).collect({ new Date() - it}).reverse()
+        log.info("Created dates: ${dates}")
+
+        when:
+        // user 1 achieves skill in order
+        String user1 = "user1"
+        List user1Res = addSkills(skills + skills_subj2, dates, user1)
+
+        String user2 = "user2"
+        List user2Res = addSkills(skills + skills_subj2, dates.reverse(), user2)
+
+        List<UserAchievement> achievements = userAchievementRepo.findAll()
+        then:
+        user1Res[2].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:1"]
+        achievements.find { it.userId == user1 && it.skillId == subj.subjectId && it.level == 1 }.achievedOn == dates[2]
+
+        user1Res[5].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:1"]
+        achievements.find { it.userId == user1 && !it.skillId && it.level == 1 }.achievedOn == dates[5]
+
+        user1Res[7].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:2"]
+        achievements.find { it.userId == user1 && it.skillId == subj.subjectId && it.level == 2 }.achievedOn == dates[7]
+
+        user1Res[13].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:3"]
+        achievements.find { it.userId == user1 && it.skillId == subj.subjectId && it.level == 3 }.achievedOn == dates[9]
+
+        user1Res[14].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:2"]
+        achievements.find { it.userId == user1 && !it.skillId && it.level == 2 }.achievedOn == dates[9]
+
+        user1Res[20].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:4"]
+        achievements.find { it.userId == user1 && it.skillId == subj.subjectId && it.level == 4 }.achievedOn == dates[9]
+
+        user1Res[26].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:3"]
+        achievements.find { it.userId == user1 && !it.skillId && it.level == 3 }.achievedOn == dates[9]
+
+        user1Res[27].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:5"]
+        achievements.find { it.userId == user1 && it.skillId == subj.subjectId && it.level == 5 }.achievedOn == dates[9]
+
+        user1Res[32].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:1"]
+        achievements.find { it.userId == user1 && it.skillId == subj2.subjectId && it.level == 1 }.achievedOn == dates[2]
+
+        user1Res[37].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:2"]
+        achievements.find { it.userId == user1 && it.skillId == subj2.subjectId && it.level == 2 }.achievedOn == dates[7]
+
+        user1Res[40].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:4"]
+        achievements.find { it.userId == user1 && !it.skillId && it.level == 4 }.achievedOn == dates[9]
+
+        user1Res[43].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:3"]
+        achievements.find { it.userId == user1 && it.skillId == subj2.subjectId && it.level == 3 }.achievedOn == dates[9]
+
+        user1Res[50].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:4"]
+        achievements.find { it.userId == user1 && it.skillId == subj2.subjectId && it.level == 4 }.achievedOn == dates[9]
+
+        user1Res[55].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:5"]
+        achievements.find { it.userId == user1 && !it.skillId && it.level == 5 }.achievedOn == dates[9]
+
+        user1Res[57].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:5"]
+        achievements.find { it.userId == user1 && it.skillId == subj2.subjectId && it.level == 5 }.achievedOn == dates[9]
+
+        // ------------------
+        // user 2
+        user2Res[2].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:1"]
+        achievements.find { it.userId == user2 && it.skillId == subj.subjectId && it.level == 1 }.achievedOn == dates[9]
+
+        user2Res[5].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:1"]
+        achievements.find { it.userId == user2 && !it.skillId && it.level == 1 }.achievedOn == dates[9]
+
+        user2Res[7].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:2"]
+        achievements.find { it.userId == user2 && it.skillId == subj.subjectId && it.level == 2 }.achievedOn == dates[9]
+
+        user2Res[13].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:3"]
+        achievements.find { it.userId == user2 && it.skillId == subj.subjectId && it.level == 3 }.achievedOn == dates[9]
+
+        user2Res[14].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:2"]
+        achievements.find { it.userId == user2 && !it.skillId && it.level == 2 }.achievedOn == dates[9]
+
+        user2Res[20].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:4"]
+        achievements.find { it.userId == user2 && it.skillId == subj.subjectId && it.level == 4 }.achievedOn == dates[9]
+
+        user2Res[26].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:3"]
+        achievements.find { it.userId == user2 && !it.skillId && it.level == 3 }.achievedOn == dates[9]
+
+        user2Res[27].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject1:5"]
+        achievements.find { it.userId == user2 && it.skillId == subj.subjectId && it.level == 5 }.achievedOn == dates[9]
+
+        user2Res[32].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:1"]
+        achievements.find { it.userId == user2 && it.skillId == subj2.subjectId && it.level == 1 }.achievedOn == dates[9]
+
+        user2Res[37].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:2"]
+        achievements.find { it.userId == user2 && it.skillId == subj2.subjectId && it.level == 2 }.achievedOn == dates[9]
+
+        user2Res[40].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:4"]
+        achievements.find { it.userId == user2 && !it.skillId && it.level == 4 }.achievedOn == dates[9]
+
+        user2Res[43].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:3"]
+        achievements.find { it.userId == user2 && it.skillId == subj2.subjectId && it.level == 3 }.achievedOn == dates[9]
+
+        user2Res[50].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:4"]
+        achievements.find { it.userId == user2 && it.skillId == subj2.subjectId && it.level == 4 }.achievedOn == dates[9]
+
+        user2Res[55].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["OVERALL:5"]
+        achievements.find { it.userId == user2 && !it.skillId && it.level == 5 }.achievedOn == dates[9]
+
+        user2Res[57].completed.findAll { it.level }.collect { "${it.id}:${it.level}" } ==  ["TestSubject2:5"]
+        achievements.find { it.userId == user2 && it.skillId == subj2.subjectId && it.level == 5 }.achievedOn == dates[9]
+    }
+
+    private def addSkills(List skills, List<Date> dates, String userId) {
+        int index = 0
+        skills.collect { def skill ->
+            int dateIndex = 0;
+            dates.collect { Date date ->
+                def body = skillsService.addSkill([projectId: projId, skillId: skill.skillId], userId, date).body
+//                println "[$index]: User=[$userId], skill=[${skill.skillId}], date[${dateIndex}]=[$date], completed=${body.completed}"
+                index++
+                dateIndex++
+                return body
+            }
+        }.flatten()
+    }
 
 }
