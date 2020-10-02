a4c9e35d5d365568a16a59bc867127b3b44feee9
mosefer <cjmoses@evoforge.org>
2020-10-02 14:11:26 -0400

    #110 vue-virtual-scroll-list update introduced new api, refactor IconManager to use new api

--- dashboard/src/components/utils/iconPicker/IconManager.vue
+++ dashboard/src/components/utils/iconPicker/IconManager.vue
@@ -23,21 +23,14 @@ limitations under the License.
             <i class="fab fa-font-awesome-flag"></i> {{ fontAwesomeIcons.iconPack }}
           </template>
             <span v-if="fontAwesomeIcons.icons.length === 0 && this.activePack === fontAwesomeIcons.iconPack">No icons matched your search</span>
-            <virtual-list :size="60" :remain="5" :bench="10" wclass="scroll-container">
-              <div class="icon-row" v-for="(row, index) in fontAwesomeIcons.icons" :key="`${row[0].cssClass}-${index}`">
-                  <div class="icon-item" v-for="item in row" :key="item.cssClass">
-                    <a
-                      href="#"
-                      @click.stop.prevent="getIcon(item.name, item.cssClass, fontAwesomeIcons.iconPack)"
-                      :class="`item ${selectedCss === item.cssClass ? 'selected' : ''}`"
-                    >
-                      <span class="icon is-large">
-                        <i :class="item.cssClass"></i>
-                      </span>
-                    </a><br/>
-                    <span class="iconName">{{ item.name }}</span>
-                  </div>
-              </div>
+              <virtual-list style="height: 360px; overflow-y: auto;"
+                            :keeps="5"
+                            :data-key="uniqueIdGenerator"
+                            :data-sources="fontAwesomeIcons.icons"
+                            :data-component="rowItemComponent"
+                            wrap-class="scroll-container"
+                            @icon-selected="getIcon($event, fontAwesomeIcons.iconPack)"
+                            ref="fontAwesomeVirtualList">
             </virtual-list>
         </b-tab>
         <b-tab>
@@ -45,21 +38,14 @@ limitations under the License.
             <i class="fas fa-file-alt"></i> {{ materialIcons.iconPack }}
           </template>
           <span v-if="materialIcons.icons.length === 0 && this.activePack === materialIcons.iconPack">No icons matched your search</span>
-            <virtual-list :size="60" :remain="5" :bench="10" wclass="scroll-container">
-              <div class="icon-row" v-for="(row, index) in materialIcons.icons" :key="index">
-                <div class="icon-item" v-for="item in row" :key="item.cssClass">
-                  <a
-                    href="#"
-                    @click.stop.prevent="getIcon(item.name, item.cssClass, materialIcons.iconPack)"
-                    :class="`item ${selectedCss === item.cssClass ? 'selected' : ''}`"
-                  >
-                      <span class="icon is-large">
-                        <i :class="item.cssClass"></i>
-                      </span>
-                  </a><br/>
-                  <span class="iconName">{{ item.name }}</span>
-                </div>
-              </div>
+            <virtual-list style="height: 360px; overflow-y: auto;"
+                          :keeps="5"
+                          :data-key="uniqueIdGenerator"
+                          :data-sources="materialIcons.icons"
+                          :data-component="rowItemComponent"
+                          wrap-class="scroll-container"
+                          @icon-selected="getIcon($event, materialIcons.iconPack)"
+                          ref="materialVirtualList">
             </virtual-list>
         </b-tab>
         <b-tab>
@@ -81,7 +67,7 @@ limitations under the License.
               <div class="icon-item">
                 <a
                   href="#"
-                  @click.stop.prevent="getIcon(name, cssClassname, 'Custom Icons')"
+                  @click.stop.prevent="getIcon({name, cssClassname}, 'Custom Icons')"
                   :class="`item ${selectedCss === cssClassname ? 'selected' : ''}`">
                   <span class="icon is-large text-info">
                     <i :class="cssClassname"></i>
@@ -114,6 +100,8 @@ limitations under the License.
   import materialIconsCanonical from './material-index';
   import IconManagerService from './IconManagerService';
   import ToastSupport from '../ToastSupport';
+  import IconRow from './IconRow';
+  import GroupedIcons from './GroupedIcons';
 
   const faIconList = fontAwesomeIconsCanonical.icons.slice();
   const matIconList = materialIconsCanonical.icons.slice();
@@ -131,20 +119,20 @@ limitations under the License.
   let self = null;
 
   function groupIntoRows(array, rl) {
-    let subArr = [];
+    let grouped = new GroupedIcons([]);
     const result = [];
     for (let i = 0; i < array.length; i += 1) {
       if (i > 0 && i % rl === 0) {
-        result.push(subArr);
-        subArr = [];
+        result.push(grouped);
+        grouped = new GroupedIcons([]);
       }
 
       const item = array[i];
-      subArr.push(item);
+      grouped.row.push(item);
     }
 
-    if (subArr.length > 0) {
-      result.push(subArr);
+    if (grouped.row.length > 0) {
+      result.push(grouped);
     }
 
     return result;
@@ -288,6 +276,7 @@ limitations under the License.
         materialIcons: materialIconsCanonical,
         customIconList,
         disableCustomUpload: false,
+        rowItemComponent: IconRow,
       };
     },
     computed: {
@@ -344,19 +333,24 @@ limitations under the License.
       this.resetIcons();
     },
     methods: {
-      getIcon(icon, iconCss, iconPack) {
-        this.selected = icon;
-        this.selectedCss = iconCss;
+      uniqueIdGenerator(groupedIcons) {
+        return groupedIcons.id;
+      },
+      getIcon(event, iconPack) {
+        this.selected = event.icon;
+        this.selectedCss = event.cssClass;
         this.selectedIconPack = iconPack;
 
-        this.selectIcon(icon, iconCss, iconPack);
+        this.selectIcon(event.icon, event.cssClass, iconPack);
       },
       onChange(tabIndex) {
         const { value } = this.$refs.iconFilterInput;
         if (tabIndex === 0) {
+          this.$refs.materialVirtualList.reset();
           this.activePack = fontAwesomeIconsCanonical.iconPack;
           this.filter(value);
         } else if (tabIndex === 1) {
+          this.$refs.materialVirtualList.reset();
           this.activePack = materialIconsCanonical.iconPack;
           this.filter(value);
         } else if (tabIndex === 2) {
@@ -428,7 +422,9 @@ limitations under the License.
             this.fontAwesomeIcons.icons = groupIntoRows(faIconList, rowLength);
             this.materialIcons.icons = groupIntoRows(matIconList, rowLength);
             this.customIconList = definitiveCustomIconList;
-            this.$refs.iconFilterInput.value = '';
+            if (this.$refs.iconFilterInput) {
+              this.$refs.iconFilterInput.value = '';
+            }
           }, 100);
         }
       },
@@ -489,5 +485,10 @@ limitations under the License.
     z-index: 1000;
     display: inline-block;
     visibility: visible;
+  }
+
+  .virtual-container {
+    height: 320px;
+    overflow-y: auto;
   }
 </style>
---
+++ dashboard/src/components/utils/iconPicker/IconRow.vue
@@ -0,0 +1,104 @@
+/*
+Copyright 2020 SkillTree
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+<template>
+    <div class="icon-row" :key="source.id">
+      <div class="icon-item" v-for="item in iconRows" :key="item.cssClass">
+        <a
+          href="#"
+          @click.stop.prevent="handleClick(item.name, item.cssClass)"
+          :class="item">
+          <span class="icon is-large">
+            <i :class="item.cssClass"></i>
+          </span>
+        </a><br/>
+        <span class="iconName">{{ item.name }}</span>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'IconRow',
+    props: {
+      source: {
+        type: Object,
+        default() {
+          return [];
+        },
+      },
+    },
+    data() {
+      return {
+        iconRows: this.source.row,
+      };
+    },
+    methods: {
+      handleClick(name, cssClass) {
+        // hacky but can't pass an event from grand-child to grand-parent otherwise
+        this.$parent.$parent.$emit('icon-selected', { name, cssClass });
+      },
+    },
+  };
+</script>
+
+<style scoped>
+.icon-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  padding-top: 12px;
+  padding-bottom: 12px;
+}
+
+.icon-item {
+  border-radius: 3px;
+  color: inherit;
+  flex: auto;
+  text-align: center;
+}
+
+.icon-item i {
+  box-sizing: content-box;
+  text-align: center;
+  border-radius: 3px;
+  font-size: 3rem;
+  width: 48px;
+  height: 48px;
+  display: inline-block;
+}
+
+.tab-content div {
+  width: 100%;
+}
+
+.delete-icon {
+  left: 0;
+  visibility: hidden;
+}
+
+.is-tiny {
+  height: .5rem;
+  width: .5rem;
+  font-size: .5rem;
+  padding-right: .5rem;
+}
+
+.icon-item:hover .delete-icon {
+  z-index: 1000;
+  display: inline-block;
+  visibility: visible;
+}
+</style>
