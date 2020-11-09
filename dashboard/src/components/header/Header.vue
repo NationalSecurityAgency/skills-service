9956bdd0d771aa2f7f33b470e8ba9f19577d2f55
EVOFORGE\dimay <dimay@evoforge.org>
2020-11-09 16:54:15 -0500

    #155: logo update

--- dashboard/src/components/header/Header.vue
+++ dashboard/src/components/header/Header.vue
@@ -19,7 +19,7 @@ limitations under the License.
       <div class="row">
         <div class="col-sm text-center text-sm-left">
           <router-link class="h2 text-primary ml-2" to="/">
-            <img src="/static/img/skilltree_logo.svg" />
+            <img src="/static/img/skilltree_logo_v1.png" />
           </router-link>
         </div>
 
@@ -54,18 +54,18 @@ limitations under the License.
 </script>
 
 <style scoped>
-.header img {
-  height: 70px;
-  position: absolute;
-  top: -15px;
-  left: 15px;
-}
-@media (max-width: 576px) {
-  .header img {
-    position: unset !important;
-    top: unset !important;
-    left: unset !important;
-    height: 100px !important;
-  }
-}
+/*.header img {*/
+/*  height: 70px;*/
+/*  position: absolute;*/
+/*  top: -15px;*/
+/*  left: 15px;*/
+/*}*/
+/*@media (max-width: 576px) {*/
+/*  .header img {*/
+/*    position: unset !important;*/
+/*    top: unset !important;*/
+/*    left: unset !important;*/
+/*    height: 100px !important;*/
+/*  }*/
+/*}*/
 </style>
