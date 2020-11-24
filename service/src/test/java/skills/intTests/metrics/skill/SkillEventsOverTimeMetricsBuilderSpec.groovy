6cad9c914e0da8c7dfe1b738a4f663a47ea057f0
EVOFORGE\dimay <dimay@evoforge.org>
2020-11-24 12:49:39 -0500

    #95: service integration tests

--- service/src/test/java/skills/intTests/metrics/skill/SkillEventsOverTimeMetricsBuilderSpec.groovy
+++ service/src/test/java/skills/intTests/metrics/skill/SkillEventsOverTimeMetricsBuilderSpec.groovy
@@ -15,7 +15,7 @@
  */
 package skills.intTests.metrics.skill
 
-
+import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 import groovy.time.TimeCategory
 import skills.intTests.utils.DefaultIntSpec
@@ -23,9 +23,9 @@ import skills.intTests.utils.SkillsClientException
 import skills.intTests.utils.SkillsFactory
 import skills.metrics.builders.MetricsParams
 
-class NumUserAchievedOverTimeMetricsBuilderSpec extends DefaultIntSpec {
+class SkillEventsOverTimeMetricsBuilderSpec  extends DefaultIntSpec {
 
-    String metricsId = "numUserAchievedOverTimeChartBuilder"
+    String metricsId = "skillEventsOverTimeChartBuilder"
 
     def "must supply skillId param"() {
         def proj = SkillsFactory.createProject()
@@ -45,12 +45,11 @@ class NumUserAchievedOverTimeMetricsBuilderSpec extends DefaultIntSpec {
         body.explanation == "Metrics[${metricsId}]: Must supply skillId param"
     }
 
-    def "number achievements over time"() {
+    def "number events over time"() {
         List<String> users = (1..10).collect { "user$it" }
         def proj = SkillsFactory.createProject()
         List<Map> skills = SkillsFactory.createSkills(10)
-        skills.each { it.pointIncrement = 100; it.numPerformToCompletion = 1 }
-
+        skills.each { it.pointIncrement = 100; it.numPerformToCompletion = 5 }
 
         skillsService.createProject(proj)
         skillsService.createSubject(SkillsFactory.createSubject())
@@ -77,26 +76,28 @@ class NumUserAchievedOverTimeMetricsBuilderSpec extends DefaultIntSpec {
             props[MetricsParams.P_SKILL_ID] = it.skillId
             return skillsService.getMetricsData(proj.projectId, metricsId, props)
         }
+
         then:
-        res[0].achievementCounts.collect { it.num } == [1, 2, 3, 4, 5]
-        res[0].achievementCounts.collect { new Date(it.timestamp) } == [days[1], days[2], days[3], days[4], days[5]]
+        res[0].countsByDay.collect { it.num } == [1, 2, 3, 4, 5]
+        res[0].countsByDay.collect { new Date(it.timestamp) } == [days[1], days[2], days[3], days[4], days[5]]
 
-        res[1].achievementCounts.collect { it.num } == [2, 3, 4, 5]
-        res[1].achievementCounts.collect { new Date(it.timestamp) } == [days[2], days[3], days[4], days[5]]
+        res[1].countsByDay.collect { it.num } == [2, 3, 4, 5]
+        res[1].countsByDay.collect { new Date(it.timestamp) } == [days[2], days[3], days[4], days[5]]
 
-        res[2].achievementCounts.collect { it.num } == [3, 4, 5]
-        res[2].achievementCounts.collect { new Date(it.timestamp) } == [days[3], days[4], days[5]]
+        res[2].countsByDay.collect { it.num } == [3, 4, 5]
+        res[2].countsByDay.collect { new Date(it.timestamp) } == [days[3], days[4], days[5]]
 
-        res[3].achievementCounts.collect { it.num } == [4, 5]
-        res[3].achievementCounts.collect { new Date(it.timestamp) } == [days[4], days[5]]
+        res[3].countsByDay.collect { it.num } == [4, 5]
+        res[3].countsByDay.collect { new Date(it.timestamp) } == [days[4], days[5]]
 
-        res[4].achievementCounts.collect { it.num } == [5]
-        res[4].achievementCounts.collect { new Date(it.timestamp) } == [days[5]]
+        res[4].countsByDay.collect { it.num } == [5]
+        res[4].countsByDay.collect { new Date(it.timestamp) } == [days[5]]
 
-        res[5].achievementCounts.collect { it.num } == []
-        res[6].achievementCounts.collect { it.num } == []
-        res[7].achievementCounts.collect { it.num } == []
-        res[8].achievementCounts.collect { it.num } == []
-        res[9].achievementCounts.collect { it.num } == []
+        res[5].countsByDay.collect { it.num } == []
+        res[6].countsByDay.collect { it.num } == []
+        res[7].countsByDay.collect { it.num } == []
+        res[8].countsByDay.collect { it.num } == []
+        res[9].countsByDay.collect { it.num } == []
     }
+
 }
