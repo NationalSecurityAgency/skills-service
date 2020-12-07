d47f4ae10a00bf1a5811bb663e7b464662499c66
mosefer <cjmoses@evoforge.org>
2020-12-07 10:32:30 -0500

    #182 include common bundle to reduce duplication across split bundles

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -62,11 +62,21 @@ let optimization = {
     minSize: 30,
     cacheGroups: {
       vendor: {
+        chunks: 'all',
         test: /[\\/]node_modules[\\/]/,
+        priority: 20,
         name(module) {
           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
           return `npm.${packageName.replace('@', '')}`;
         },
+      },
+      common: {
+        name: 'common',
+        minChunks: 2,
+        chunks: 'async',
+        priority: 10,
+        reuseExistingChunk: true,
+        enforce: true
       },
     },
   },
