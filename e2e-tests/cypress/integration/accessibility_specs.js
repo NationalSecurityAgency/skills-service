b46dc4186d3a52d69cd959fc263be93f01c26b0a
mosefer <cjmoses@evoforge.org>
2021-01-04 09:39:09 -0500

    #261 remove .only, add license header

--- e2e-tests/cypress/integration/accessibility_specs.js
+++ e2e-tests/cypress/integration/accessibility_specs.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 SkillTree
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const moment = require('moment-timezone');
 
 describe('Accessibility Tests', () => {
@@ -71,7 +86,7 @@ describe('Accessibility Tests', () => {
     cy.request('POST', `/api/projects/MyNewtestProject/skills/skill1`, {userId: 'u5', timestamp: m.subtract(1, 'day').format('x')})
   });
 
-  it.only('home page', () => {
+  it('home page', () => {
     cy.visit('/');
     cy.customLighthouse();
     cy.injectAxe()
