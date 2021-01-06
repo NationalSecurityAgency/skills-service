9f26030d7c1c86b9ae50bd419db574f38463fe84
rmmayo <rmmayo@evoforge.org>
2021-01-06 10:38:11 -0500

    #267 - add link to SettingsButton.vue for switching between Admin and My Skills

--- dashboard/src/components/header/SettingsButton.vue
+++ dashboard/src/components/header/SettingsButton.vue
@@ -21,10 +21,10 @@ limitations under the License.
       <span class="sr-only">settings menu</span>
     </template>
     <b-dropdown-item href="#"  @click="gotoMySkills">
-      <span class="text-gray-700"> <i class="fas fa-cog" aria-hidden="true"/><span class="link-name">My Skills</span></span>
+      <span class="text-gray-700"> <i class="fas fa-trophy" aria-hidden="true"/><span class="link-name">My Skills</span></span>
     </b-dropdown-item>
     <b-dropdown-item href="#"  @click="gotoAdmin">
-      <span class="text-gray-700"> <i class="fas fa-cog" aria-hidden="true"/><span class="link-name">Admin</span></span>
+      <span class="text-gray-700"> <i class="fas fa-tasks" aria-hidden="true"/><span class="link-name">Admin</span></span>
     </b-dropdown-item>
     <b-dropdown-item href="#"  @click="gotoSettings">
       <span class="text-gray-700"> <i class="fas fa-cog" aria-hidden="true"/><span class="link-name">Settings</span></span>
