0867cb7b9b52028d39acc70bf094bad64ffeaf0e
rmmayo <rmmayo@evoforge.org>
2021-01-11 09:50:09 -0500

    #267 - added skilltree license

--- dashboard/src/components/mySkills/MySkillsPage.vue
+++ dashboard/src/components/mySkills/MySkillsPage.vue
@@ -34,12 +34,33 @@ limitations under the License.
       </b-col>
     </b-row>
 
-    <metrics-card title="Chart" class="mt-3">
-      <div class="text-center">
-        <span class="font-weight-bold"><i class="fas fa-tasks mr-2 text-secondary"></i>Project Participation</span>
-        <apexchart type="radialBar" height="350" :options="chartOptions" :series="series"></apexchart>
-        <span>Participating in 3 or 5 Projects</span>
-      </div>
+    <metrics-card title="SkillTree Progress" class="mt-3">
+      <b-row class="my-4">
+        <b-col>
+          <b-card>
+            <div class="text-center">
+              <span class="font-weight-bold"><i class="fas fa-tasks mr-2 text-secondary"></i>Project Participation</span>
+              <apexchart type="radialBar" height="350"
+                         :options="chartOptions1"
+                         :series="series1">
+              </apexchart>
+              <span>Participating in 3 or 5 Projects</span>
+            </div>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card>
+            <div class="text-center">
+              <span class="font-weight-bold"><i class="fas fa-tasks mr-2 text-secondary"></i>Project Point History</span>
+<!--              <apexchart type="line" height="350"-->
+<!--                         :options="chartOptions2"-->
+<!--                         :series="series2">-->
+<!--              </apexchart>-->
+<!--              <span>Participating in 3 or 5 Projects</span>-->
+            </div>
+          </b-card>
+        </b-col>
+      </b-row>
     </metrics-card>
 
     <metrics-card title="Chart" class="mt-3">
@@ -113,8 +134,8 @@ limitations under the License.
           totalUsers: 379,
           rank: 78,
         }],
-        series: [66],
-        chartOptions: {
+        series1: [66],
+        chartOptions1: {
           chart: {
             height: 350,
             type: 'radialBar',
@@ -193,6 +214,83 @@ limitations under the License.
           },
           labels: ['Percent'],
         },
+        series2: [{
+                    name: 'Session Duration',
+                    data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10],
+                  },
+                  {
+                    name: 'Page Views',
+                    data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35],
+                  },
+                  {
+                    name: 'Total Visits',
+                    data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47],
+                  },
+        ],
+        chartOptions2: {
+          chart: {
+            height: 350,
+            type: 'line',
+            zoom: {
+              enabled: false,
+            },
+          },
+          dataLabels: {
+            enabled: false,
+          },
+          stroke: {
+            width: [5, 7, 5],
+            curve: 'straight',
+            dashArray: [0, 8, 5],
+          },
+          // title: {
+          //   text: 'Page Statistics',
+          //   align: 'left',
+          // },
+          legend: {
+            tooltipHoverFormatter(val, opts) {
+              return `${val} - ${opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex]}`;
+            },
+          },
+          markers: {
+            size: 0,
+            hover: {
+              sizeOffset: 6,
+            },
+          },
+          xaxis: {
+            categories: ['01 Jan', '03 Jan', '05 Jan', '07 Jan', '09 Jan', '11 Jan',
+            ],
+          },
+          tooltip: {
+            y: [
+              {
+                title: {
+                  formatter(val) {
+                    return `${val} (mins)`;
+                  },
+                },
+              },
+              {
+                title: {
+                  formatter(val) {
+                    return `${val} per session`;
+                  },
+                },
+              },
+              {
+                title: {
+                  formatter(val) {
+                    return val;
+                  },
+                },
+              },
+            ],
+          },
+          grid: {
+            borderColor: '#f1f1f1',
+          },
+        },
       };
     },
     mounted() {
@@ -208,5 +306,10 @@ limitations under the License.
 <style scoped>
 .project-link :hover {
   cursor: pointer;
+}
+.charts-content {
+  /* this little hack is required to prevent apexcharts from wrapping onto a new line;
+  the gist is that they calculate width dynamically and do not work properly with the width of 0*/
+  min-width: 1rem !important;
 }
 </style>
