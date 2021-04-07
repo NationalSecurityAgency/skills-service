7702e27c638ef51defbd9b8087780cebac558432
EVOFORGE\dimay <dimay@evoforge.org>
2021-04-07 08:55:19 -0400

    #497: Fixed post achievement counts to include a single usage after the achievement

--- service/src/test/java/skills/intTests/metrics/skill/UsagePostAchievementMetricsBuilderSpec.groovy
+++ service/src/test/java/skills/intTests/metrics/skill/UsagePostAchievementMetricsBuilderSpec.groovy
@@ -39,26 +39,37 @@ class UsagePostAchievementMetricsBuilderSpec extends DefaultIntSpec {
     def "produces accurate post achievement usage counts"() {
         //simple case, not taking into account event compaction boundaries
         def proj = SkillsFactory.createProject()
-        def skill = SkillsFactory.createSkill(1, 1, 1, 0, 10,  )
+        def skill = SkillsFactory.createSkill(1, 1, 1, 0, 2,  )
+        skill.pointIncrement = 100
 
         skillsService.createProject(proj)
         skillsService.createSubject(SkillsFactory.createSubject())
         skillsService.createSkill(skill)
 
-        def users = getRandomUsers(3)
+        def users = getRandomUsers(6)
 
-        use(TimeCategory){
-            (20..0).each {
-                Date date = it.days.ago
-                if (it >= 10) {
-                    users.each {
-                        skillsService.addSkill(skill, it, date)
-                    }
-                } else {
-                    skillsService.addSkill(skill, users[0], date)
-                }
-            }
-        }
+        // user 1 - achieved and used after
+        assert skillsService.addSkill(skill, users[0], new Date() - 4).body.skillApplied
+        assert skillsService.addSkill(skill, users[0], new Date() - 3).body.skillApplied
+        assert !skillsService.addSkill(skill, users[0], new Date()).body.skillApplied
+
+        // user 2 - did not achieve
+        assert skillsService.addSkill(skill, users[1], new Date()).body.skillApplied
+
+        // user 3 - achieved but did not use after
+        assert skillsService.addSkill(skill, users[2], new Date() - 2).body.skillApplied
+        assert skillsService.addSkill(skill, users[2], new Date() - 1).body.skillApplied
+
+        // user 4 - achieved and used after
+        assert skillsService.addSkill(skill, users[3], new Date() - 9).body.skillApplied
+        assert skillsService.addSkill(skill, users[3], new Date() - 8).body.skillApplied
+        assert !skillsService.addSkill(skill, users[3], new Date() - 7).body.skillApplied
+        assert !skillsService.addSkill(skill, users[3], new Date() - 6).body.skillApplied
+        assert !skillsService.addSkill(skill, users[3], new Date() - 5).body.skillApplied
+
+        // user 5 and 6 - did not achieve
+        assert skillsService.addSkill(skill, users[5], new Date()).body.skillApplied
+        assert skillsService.addSkill(skill, users[6], new Date()).body.skillApplied
 
         when:
         def props = ["skillId": skill.skillId]
@@ -67,7 +78,7 @@ class UsagePostAchievementMetricsBuilderSpec extends DefaultIntSpec {
         then:
         result
         result.totalUsersAchieved == 3
-        result.usersPostAchievement == 1
+        result.usersPostAchievement == 2
     }
 
     def "skill usage post achievement crossing event compaction boundaries"() {
