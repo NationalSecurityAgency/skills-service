5520b04ab90f20840ca2fdcc8ccb38b4a80ec4a1
rmmayo <rmmayo@evoforge.org>
2021-05-07 10:21:56 -0400

    #598 - fixing cypress tests

--- e2e-tests/cypress/integration/client-display/client-display-features_spec.js
+++ e2e-tests/cypress/integration/client-display/client-display-features_spec.js
@@ -272,7 +272,7 @@ describe('Client Display Features Tests', () => {
     cy.createSkill(2);
     cy.request('POST', `/admin/projects/proj1/skills/skill1/dependency/skill2`)
 
-    cy.cdVisit('/');
+    cy.cdVisit('/?internalBackButton=true');
     cy.cdClickSubj(0, 'Subject 1');
 
     cy.matchSnapshotImage(`Subject-WithLockedSkills-ThatWereFullyAchieved`, snapshotOptions);
