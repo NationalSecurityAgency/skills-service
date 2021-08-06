6d70e2a1b4109ae32efaa3557be0d94fe8359115
mosefer <cjmoses@evoforge.org>
2021-08-06 14:53:10 -0400

    #777 use correct data attribute in spec

--- e2e-tests/cypress/integration/contact_admins_spec.js
+++ e2e-tests/cypress/integration/contact_admins_spec.js
@@ -128,7 +128,6 @@ describe('Contact Project Admins Specs', () => {
     })
 
     it('preview email button', () => {
-
         cy.intercept('GET', '/public/isFeatureSupported?feature=emailservice').as('emailSupported');
         cy.intercept('GET', '/app/userInfo/hasRole/ROLE_SUPER_DUPER_USER').as('isRoot');
 
@@ -141,6 +140,6 @@ describe('Contact Project Admins Specs', () => {
         cy.get('[data-cy=emailUsers_subject]').type('Test Subject');
         cy.get('[data-cy=previewUsersEmail]').should('be.disabled');
         cy.get('[data-cy="markdownEditorInput"]').type('Test Body');
-        cy.get('[data-cy=previewUsersEmail]').should('be.enabled');
+        cy.get('[data-cy=previewAdminEmail]').should('be.enabled');
     });
 });
