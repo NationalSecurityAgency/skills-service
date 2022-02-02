b42ed7e73842566aa1d5ffd02c2dec2026e1a4c2
mosefer <cjmoses@evoforge.org>
2022-02-02 08:53:12 -0500

    #1117 add support for enabling metrics and the option to expose prometheus metrics without authentication

--- service/src/main/java/skills/auth/PortalWebSecurityHelper.groovy
+++ service/src/main/java/skills/auth/PortalWebSecurityHelper.groovy
@@ -15,17 +15,51 @@
  */
 package skills.auth
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.util.matcher.AndRequestMatcher
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
 import org.springframework.stereotype.Component
 import skills.storage.model.auth.RoleName
 
+import javax.servlet.http.HttpServletRequest
+
 @Component
 class PortalWebSecurityHelper {
+
+    @Value('#{"${management.server.port:8808}"}')
+    Integer managementPort
+
+    @Value('#{"${server.port:8080}"}')
+    Integer serverPort
+
+    @Value('#{"${skills.config.publiclyExposePrometheusMetrics:false}"}')
+    Boolean publiclyExposePrometheusMetrics
+
+    @Value('#{"${management.endpoints.web.base-path:/actuator}"}')
+    String managementPath
+
+    @Value('#{"${management.endpoints.web.path-mapping.prometheus:prometheus}"}')
+    String prometheusPath
+
     HttpSecurity configureHttpSecurity(HttpSecurity http) {
+
         http
             .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/", "/favicon.ico",
+
+        if (publiclyExposePrometheusMetrics) {
+            Integer port = serverPort
+            if (managementPort) {
+                port = managementPort
+            }
+
+            http.authorizeRequests()
+                    .requestMatchers(forPortAndPath(port, HttpMethod.GET, "${managementPath}/${prometheusPath}")).permitAll()
+        }
+
+        http.authorizeRequests().antMatchers("/", "/favicon.ico",
                     "/icons/**", "/static/**",
                     "/skilltree.ico",
                     "/error", "/oauth/**",
@@ -42,10 +76,23 @@ class PortalWebSecurityHelper {
             .antMatchers('/supervisor/**').hasAnyAuthority(RoleName.ROLE_SUPERVISOR.name(), RoleName.ROLE_SUPER_DUPER_USER.name())
             .antMatchers('/root/isRoot').hasAnyAuthority(RoleName.values().collect {it.name()}.toArray(new String[0]))
             .antMatchers('/root/**').hasRole('SUPER_DUPER_USER')
-            .antMatchers('/actuator/**').hasRole('SUPER_DUPER_USER')
+            .antMatchers("/${managementPath}/**").hasRole('SUPER_DUPER_USER')
             .anyRequest().authenticated()
         http.headers().frameOptions().disable()
 
         return http
+    }
+
+    private RequestMatcher forPortAndPath(final int port, final String pathPattern) {
+        return new AndRequestMatcher(forPort(port), new AntPathRequestMatcher(pathPattern));
+    }
+
+    private RequestMatcher forPortAndPath(final int port, final HttpMethod method,
+                                          final String pathPattern) {
+        return new AndRequestMatcher(forPort(port), new AntPathRequestMatcher(pathPattern, method.name()));
+    }
+
+    private RequestMatcher forPort(final int port) {
+        return {HttpServletRequest request -> port == request.getLocalPort()};
     }
 }
