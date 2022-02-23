3a09712d9ff3f4ab8eb4481f845fd61f6705a65f
EVOFORGE\dimay <dimay@evoforge.org>
2022-02-22 20:12:26 -0500

    #1098: populate number of disabled skills any time ProjSummaryResult is returned

--- service/src/main/java/skills/storage/repos/ProjDefRepo.groovy
+++ service/src/main/java/skills/storage/repos/ProjDefRepo.groovy
@@ -65,6 +65,7 @@ interface ProjDefRepo extends CrudRepository<ProjDef, Long> {
                     pd.total_points AS totalPoints,
                     COALESCE(errors.errorCount, 0) AS numErrors,
                     COALESCE(skills.skillCount, 0) AS numSkills,
+                    COALESCE(disabledSkills.skillCount, 0) AS numSkillsDisabled,
                     COALESCE(badges.badgeCount, 0) AS numBadges,
                     COALESCE(subjects.subjectCount, 0) AS numSubjects,
                     COALESCE(groups.groupCount, 0) AS numGroups,
@@ -77,6 +78,7 @@ interface ProjDefRepo extends CrudRepository<ProjDef, Long> {
                 LEFT JOIN (SELECT project_id, MAX(event_time) AS latest FROM user_events GROUP BY project_id) events ON events.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS errorCount FROM project_error GROUP BY project_id) errors ON errors.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS skillCount, MAX(updated) AS skillUpdated FROM skill_definition WHERE type = 'Skill' and enabled = 'true' GROUP BY project_id) skills ON skills.project_id = pd.project_id
+                LEFT JOIN (SELECT project_id, COUNT(id) AS skillCount, MAX(updated) AS skillUpdated FROM skill_definition WHERE type = 'Skill' and enabled = 'false' GROUP BY project_id) disabledSkills ON skills.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS badgeCount, MAX(updated) AS badgeUpdated FROM skill_definition WHERE type = 'Badge' GROUP BY project_id) badges ON badges.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS subjectCount, MAX(updated) AS subjectUpdated FROM skill_definition WHERE type = 'Subject' GROUP BY project_id) subjects ON subjects.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS groupCount FROM skill_definition WHERE type = 'SkillsGroup' and enabled = 'true' GROUP BY project_id) groups ON groups.project_id = pd.project_id
@@ -96,6 +98,7 @@ interface ProjDefRepo extends CrudRepository<ProjDef, Long> {
                     pd.total_points AS totalPoints,
                     COALESCE(errors.errorCount, 0) AS numErrors,
                     COALESCE(skills.skillCount, 0) AS numSkills,
+                    COALESCE(disabledSkills.skillCount, 0) AS numSkillsDisabled,
                     COALESCE(badges.badgeCount, 0) AS numBadges,
                     COALESCE(subjects.subjectCount, 0) AS numSubjects,
                     COALESCE(groups.groupCount, 0) AS numGroups,
@@ -108,6 +111,7 @@ interface ProjDefRepo extends CrudRepository<ProjDef, Long> {
                 LEFT JOIN (SELECT project_id, MAX(event_time) AS latest FROM user_events GROUP BY project_id) events ON events.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS errorCount FROM project_error GROUP BY project_id) errors ON errors.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS skillCount, MAX(updated) AS skillUpdated FROM skill_definition WHERE type = 'Skill' and enabled = 'true' GROUP BY project_id) skills ON skills.project_id = pd.project_id
+                LEFT JOIN (SELECT project_id, COUNT(id) AS skillCount, MAX(updated) AS skillUpdated FROM skill_definition WHERE type = 'Skill' and enabled = 'false' GROUP BY project_id) disabledSkills ON skills.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS badgeCount, MAX(updated) AS badgeUpdated FROM skill_definition WHERE type = 'Badge' GROUP BY project_id) badges ON badges.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS subjectCount, MAX(updated) AS subjectUpdated FROM skill_definition WHERE type = 'Subject' GROUP BY project_id) subjects ON subjects.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS groupCount FROM skill_definition WHERE type = 'SkillsGroup' and enabled = 'true' GROUP BY project_id) groups ON groups.project_id = pd.project_id
@@ -191,6 +195,7 @@ interface ProjDefRepo extends CrudRepository<ProjDef, Long> {
                     pd.total_points AS totalPoints,
                     COALESCE(errors.errorCount, 0) AS numErrors,
                     COALESCE(skills.skillCount, 0) AS numSkills,
+                    COALESCE(disabledSkills.skillCount, 0) AS numSkillsDisabled,
                     COALESCE(badges.badgeCount, 0) AS numBadges,
                     COALESCE(subjects.subjectCount, 0) AS numSubjects,
                     COALESCE(groups.groupCount, 0) AS numGroups,
@@ -202,6 +207,7 @@ interface ProjDefRepo extends CrudRepository<ProjDef, Long> {
                 LEFT JOIN (SELECT project_id, MAX(event_time) AS latest FROM user_events GROUP BY project_id) events ON events.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS errorCount FROM project_error GROUP BY project_id) errors ON errors.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS skillCount FROM skill_definition WHERE type = 'Skill' and enabled = 'true' GROUP BY project_id) skills ON skills.project_id = pd.project_id
+                LEFT JOIN (SELECT project_id, COUNT(id) AS skillCount FROM skill_definition WHERE type = 'Skill' and enabled = 'false' GROUP BY project_id) disabledSkills ON skills.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS badgeCount FROM skill_definition WHERE type = 'Badge' GROUP BY project_id) badges ON badges.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS subjectCount FROM skill_definition WHERE type = 'Subject' GROUP BY project_id) subjects ON subjects.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS groupCount FROM skill_definition WHERE type = 'SkillsGroup' and enabled = 'true' GROUP BY project_id) groups ON groups.project_id = pd.project_id
@@ -222,6 +228,7 @@ interface ProjDefRepo extends CrudRepository<ProjDef, Long> {
                     pd.total_points AS totalPoints,
                     COALESCE(errors.errorCount, 0) AS numErrors,
                     COALESCE(skills.skillCount, 0) AS numSkills,
+                    COALESCE(disabledSkills.skillCount, 0) AS numSkillsDisabled,
                     COALESCE(badges.badgeCount, 0) AS numBadges,
                     COALESCE(subjects.subjectCount, 0) AS numSubjects,
                     COALESCE(groups.groupCount, 0) AS numGroups,
@@ -233,6 +240,7 @@ interface ProjDefRepo extends CrudRepository<ProjDef, Long> {
                 LEFT JOIN (SELECT project_id, MAX(event_time) AS latest FROM user_events GROUP BY project_id) events ON events.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS errorCount FROM project_error GROUP BY project_id) errors ON errors.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS skillCount FROM skill_definition WHERE type = 'Skill' and enabled = 'true' GROUP BY project_id) skills ON skills.project_id = pd.project_id
+                LEFT JOIN (SELECT project_id, COUNT(id) AS skillCount FROM skill_definition WHERE type = 'Skill' and enabled = 'false' GROUP BY project_id) disabledSkills ON skills.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS badgeCount FROM skill_definition WHERE type = 'Badge' GROUP BY project_id) badges ON badges.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS subjectCount FROM skill_definition WHERE type = 'Subject' GROUP BY project_id) subjects ON subjects.project_id = pd.project_id
                 LEFT JOIN (SELECT project_id, COUNT(id) AS groupCount FROM skill_definition WHERE type = 'SkillsGroup' and enabled = 'true' GROUP BY project_id) groups ON groups.project_id = pd.project_id
