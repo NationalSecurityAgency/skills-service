db48a0211ddb1c39d57865280f05b34febd0934e
EVOFORGE\dimay <dimay@evoforge.org>
2022-05-09 13:49:56 -0400

    #1171: fixing tests

--- dashboard/src/components/utils/cards/NavCardWithStatsAndControls.vue
+++ dashboard/src/components/utils/cards/NavCardWithStatsAndControls.vue
@@ -61,9 +61,8 @@ limitations under the License.
                  :aria-label="`Warning: ${stat.warnMsg}`"
                  data-cy="warning"/>
               <div v-if="stat.disabledCount && stat.disabledCount > 0" style="font-size: 0.9rem">
-                <b-badge variant="warning" :data-cy="`pagePreviewCardStat_${stat.label}_disabled`">{{
-                    stat.disabledCount
-                  }}
+                <b-badge variant="warning">
+                  <span :data-cy="`pagePreviewCardStat_${stat.label}_disabled`">{{ stat.disabledCount }}</span>
                 </b-badge>
                 <span class="text-left text-secondary text-uppercase ml-1" style="font-size: 0.8rem">disabled</span>
               </div>
