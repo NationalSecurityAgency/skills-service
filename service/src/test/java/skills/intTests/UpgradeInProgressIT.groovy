3eb9d07adba668b49fabd5541a09e15295a499de
Moses <EVOFORGE\cjmoses@a-1s2we25hoszmm.evoforge.org>
2022-05-18 07:46:18 -0400

    #1308 headers

--- service/src/test/java/skills/intTests/UpgradeInProgressIT.groovy
+++ service/src/test/java/skills/intTests/UpgradeInProgressIT.groovy
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020 SkillTree
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package skills.intTests
 
 import groovy.util.logging.Slf4j
@@ -5,16 +20,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.io.ClassPathResource
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.RowCallbackHandler
 import skills.SpringBootApp
 import skills.auth.UserAuthService
 import skills.intTests.utils.DefaultIntSpec
 import skills.intTests.utils.SkillsClientException
 import skills.intTests.utils.SkillsFactory
-import skills.storage.repos.UserRoleRepo
-
-import java.sql.ResultSet
-import java.sql.SQLException
 
 @Slf4j
 @SpringBootTest(properties = ['skills.h2.port=9093',
