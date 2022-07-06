565ba7c22ac69ae50ae59601e07c5d34ede3b729
rmmayo <rmmayo@evoforge.org>
2022-07-06 07:40:32 -0400

    #1441 - configured v-select dropdown that support multiple selections (`multiple=true`) to remain open after selection, retaining focus with so the user can continue selecting without having to tab back to the list

--- dashboard/src/components/metrics/multipleProjects/TrainingProfileComparator.vue
+++ dashboard/src/components/metrics/multipleProjects/TrainingProfileComparator.vue
@@ -21,6 +21,7 @@ limitations under the License.
                 v-model="projects.selected"
                 :loading="loading"
                 :multiple="true"
+                :close-on-select="false"
                 label="name"
                 placeholder="Select option"
                 :selectable="() => projects.selected.length < 5"
