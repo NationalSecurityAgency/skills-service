fdecfa27a3b274bd37835c5ca8d77c021ae7e6aa
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2022-08-18 14:26:26 -0400

    #1523 Add a Cypress test to test moving between skills

---
+++ e2e-tests/cypress/e2e/client-display/client-display-skill-paging_spec.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 SkillTree
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+describe('Client Display Skills Navigation', () => {
+
+    it('navigate between skills', () => {
+        cy.createProject(1);
+        cy.createSubject(1, 1);
+        cy.createSkill(1, 1, 1);
+        cy.createSkill(1, 1, 2);
+        cy.createSkill(1, 1, 3);
+
+        cy.cdVisit('/');
+        cy.cdClickSubj(0);
+
+        cy.get('[data-cy="skillProgress_index-0"]').should('exist');
+        cy.get('[data-cy="skillProgress_index-0"]').click();
+        cy.get('[data-cy="skillProgressTitle"').contains('Very Great Skill 1');
+        cy.get('[data-cy="prevSkill"]').should('not.exist');
+        cy.get('[data-cy="nextSkill"]').should('exist');
+        cy.get('[data-cy="nextSkill"]').click();
+
+        cy.get('[data-cy="skillProgressTitle"').contains('Very Great Skill 2');
+        cy.get('[data-cy="prevSkill"]').should('exist');
+        cy.get('[data-cy="nextSkill"]').should('exist');
+        cy.get('[data-cy="nextSkill"]').click();
+
+        cy.get('[data-cy="skillProgressTitle"').contains('Very Great Skill 3');
+        cy.get('[data-cy="prevSkill"]').should('exist');
+        cy.get('[data-cy="nextSkill"]').should('not.exist');
+        cy.get('[data-cy="prevSkill"]').click();
+
+        cy.get('[data-cy="skillProgressTitle"').contains('Very Great Skill 2');
+        cy.get('[data-cy="prevSkill"]').should('exist');
+        cy.get('[data-cy="nextSkill"]').should('exist');
+        cy.get('[data-cy="prevSkill"]').click();
+
+        cy.get('[data-cy="skillProgressTitle"').contains('Very Great Skill 1');
+        cy.get('[data-cy="prevSkill"]').should('not.exist');
+        cy.get('[data-cy="nextSkill"]').should('exist');
+    });
+});
