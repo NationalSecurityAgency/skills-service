bdd74e3b68d38902e6891b62bf4c4d45c0ee0899
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2022-09-22 14:52:54 -0400

    #524 Testing for table sort persist - WIP

---
+++ e2e-tests/cypress/e2e/tableSortPersist_spec.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2020 SkillTree
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+describe('Test persistence of table sorting', () => {
+    beforeEach(() => {
+        cy.request('POST', '/app/projects/proj1', {
+            projectId: 'proj1',
+            name: 'proj1'
+        });
+        cy.request('POST', '/admin/projects/proj1/subjects/subj1', {
+            projectId: 'proj1',
+            subjectId: 'subj1',
+            name: 'Subject 1'
+        });
+    });
+
+    it('Verify skills table sort remains sorted after navigating away', () => {
+
+        const numSkills = 13;
+        const expected = [];
+        for (let skillsCounter = 1; skillsCounter <= numSkills; skillsCounter += 1) {
+            const skillName = `Skill # ${skillsCounter}`;
+            expected.push([{ colIndex: 0,  value: skillName }, { colIndex: 1,  value: skillsCounter }])
+            cy.request('POST', `/admin/projects/proj1/subjects/subj1/skills/skill${skillsCounter}`, {
+                projectId: 'proj1',
+                subjectId: 'subj1',
+                skillId: `skill${skillsCounter}`,
+                name: skillName,
+                pointIncrement: '150',
+                numPerformToCompletion: skillsCounter < 3 ? '1' : '200',
+            });
+        };
+
+        cy.visit('/administrator/projects/proj1/subjects/subj1');
+
+        // test skill name sorting
+        cy.get('[data-cy="skillsTable"]').contains('Skill').click();
+        cy.validateTable('[data-cy="skillsTable"]', expected, 10);
+
+        cy.visit('/administrator/projects/proj1/subjects/');
+        cy.visit('/administrator/projects/proj1/subjects/subj1');
+
+        cy.validateTable('[data-cy="skillsTable"]', expected, 10);
+        cy.get('[data-cy="skillsTable"]').contains('Skill').click();
+        cy.validateTable('[data-cy="skillsTable"]', expected.map((item) => item).reverse(), 10);
+
+        cy.visit('/administrator/projects/proj1/subjects/');
+        cy.visit('/administrator/projects/proj1/subjects/subj1');
+        cy.validateTable('[data-cy="skillsTable"]', expected.map((item) => item).reverse(), 10);
+    });
+});
