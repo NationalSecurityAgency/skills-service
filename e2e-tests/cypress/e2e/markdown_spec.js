b3bc83284fcaa68d74788ccf07d84a90f12510f4
rmmayo <rmmayo@evoforge.org>
2022-10-14 11:24:09 -0400

    #1645 - adding basic emoji support

--- e2e-tests/cypress/e2e/markdown_spec.js
+++ e2e-tests/cypress/e2e/markdown_spec.js
@@ -166,8 +166,8 @@ describe('Markdown Tests', () => {
             '---\n\n' +
             'Separate me\n\n' +
             '***\n\n' +
-            // '# Emojis\n' +
-            // ':+1: :+1: :+1: :+1:\n' +
+            '# Emojis\n' +
+            ':+1: :+1: :+1: :+1:\n' +
             '';
         cy.request('POST', '/admin/projects/proj1/subjects/subj1/skills/skill1', {
             projectId: 'proj1',
@@ -185,8 +185,8 @@ describe('Markdown Tests', () => {
         cy.contains('Description');
         cy.wait('@inceptionLevel');
         cy.contains('Level');
-        // cy.contains('Emojis');
-        // cy.contains('👍 👍 👍 👍');
+        cy.contains('Emojis');
+        cy.contains('👍 👍 👍 👍');
         cy.matchSnapshotImageForElement('[data-cy="childRowDisplay_skill1"]', 'Markdown-SkillsPage-Overview', snapshotOptions);
 
         cy.visit('/administrator/projects/proj1/subjects/subj1');
@@ -196,8 +196,8 @@ describe('Markdown Tests', () => {
         cy.get(selectorSkillsRowToggle)
             .click();
         cy.contains('Description');
-        // cy.contains('Emojis');
-        // cy.contains('👍 👍 👍 👍');
+        cy.contains('Emojis');
+        cy.contains('👍 👍 👍 👍');
         cy.matchSnapshotImageForElement('[data-cy="childRowDisplay_skill1"]', 'Markdown-SubjectPage-SkillPreview', snapshotOptions);
     });
 
