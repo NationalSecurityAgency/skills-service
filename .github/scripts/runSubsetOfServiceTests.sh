df62a0ad7438c5857aba2d4ed5bec20752cb6660
EVOFORGE\dimay <dimay@evoforge.org>
2022-10-27 15:15:08 -0400

    #1754: run service tests concurrently

---
+++ .github/scripts/runSubsetOfServiceTests.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo "Running subset of service tests"
+
+usage() { echo "Usage runSubsetOfServiceTests.sh -c <current_run> -t <total_concurrent_runs>" 1>&2; exit 1; }
+
+while getopts ":c:t:" flag;
+do
+    case "${flag}" in
+        c)
+          currentRun=${OPTARG}
+          ;;
+        t)
+          totalConcurrent=${OPTARG}
+          ;;
+        *)
+          usage
+          ;;
+    esac
+done
+echo "Current Run: $currentRun";
+echo "Total Concurrent: $totalConcurrent";
+if [ -z "${currentRun}" ] || [ -z "${totalConcurrent}" ]; then
+    usage
+fi
+
+
+cd ./service/src/test/java/
+IFS=$'\n'
+allTests=($(find . | grep -E '(Test[s]?|Spec[s]?)\.(groovy|java)' | sed "s/\.\///g" | sed "s/\//\./g" | sed "s/\.groovy//g" | sed "s/\.java//g" | sort))
+unset IFS
+cd ../../../../
+
+totalNum=${#allTests[@]}
+numPerRun=$((totalNum/totalConcurrent))
+start=$(((currentRun-1)*numPerRun))
+if  [ "$currentRun" -eq "$totalConcurrent" ]; then
+    numPerRun=$totalNum
+fi
+smallArray=${allTests[@]:$start:$numPerRun}
+echo "There are [$totalNum] total tests. Run [$currentRun]: Start Number=[$start], running up to [$numPerRun] tests"
+echo "Running these tests:"
+count=1;
+for ELEMENT in ${smallArray[@]}
+do
+  echo "$count: $ELEMENT"
+  count=$((count+1))
+done
+
+batchStr=${smallArray[*]// /,}
+commandToRun="mvn -Dtest="$batchStr" test"
+echo $commandToRun
+exec $commandToRun
+
