1ba6d576916dacdb40df9acb7ee41337d9924bb3
EVOFORGE\dimay <dimay@evoforge.org>
2022-10-26 21:49:27 -0400

    #1744: #1747: fixed the selected filter overlapping Last Viewed button; added 2 new filters

--- common-components/src/common/utilities/ListFilterMenu.vue
+++ common-components/src/common/utilities/ListFilterMenu.vue
@@ -14,31 +14,42 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 <template>
-    <div class="row skills-theme-filter-menu" data-cy="filterMenu">
-        <div class="col-sm-auto pl-0 pl-md-3 pr-sm-0">
-            <b-dropdown variant="outline-info" toggle-class="skills-theme-btn" class="ml-1" dropup>
+    <div class="skills-theme-filter-menu d-inline-block" data-cy="filterMenu">
+      <b-dropdown variant="outline-info" toggle-class="skills-theme-btn" class="ml-1" dropup>
                 <template slot="button-content">
                     <i class="fas fa-filter" style="font-size: 1.1rem" aria-hidden="true" data-cy="filterBtn"/>
                     <span class="sr-only">clear filter</span>
                 </template>
-                <b-dropdown-item href="#" v-for="filter in filters" :key="filter.id" @click="filterSelected(filter.id)" :disabled="filter.count === 0"
-                                 :data-cy="`filter_${filter.id}`" class="text-primary"
-                                 :class="{ 'skills-theme-menu-primary-color' : filter.count > 0, 'skills-theme-menu-secondary-color' : filter.count === 0  }">
-                    <div style="min-width: 16rem;">
-                        <i class="text-center" :class="filter.icon" style="min-width: 1.2rem;"></i> <span v-html="filter.html"></span> <span class="badge badge-info float-right" data-cy="filterCount">{{ filter.count }}</span>
+              <b-dropdown-group v-for="filterGroup in filters" :key="filterGroup.groupId" :id="filterGroup.groupId">
+                <template slot="header">
+                  <span class="skills-theme-menu-secondary-color" style="font-size: 0.9rem">{{ filterGroup.groupLabel }}</span>
+                </template>
+                <b-dropdown-item href="#" v-for="(filter, index) in filterGroup.filterItems" :key="filter.id" @click="filterSelected(filter.id)" :disabled="filter.count === 0"
+                                 :data-cy="`filter_${filter.id}`" class="text-primary m-0"
+                                 :class="{ 'skills-theme-menu-primary-color' : filter.count > 0, 'skills-theme-menu-secondary-color' : filter.count === 0, '' : index > 0  }">
+                  <div class="row no-gutters skills-dropdown-item">
+                    <div class="col-auto">
+                      <i class="text-center border rounded p-1" :class="filter.icon" />
                     </div>
+                    <div class="col">
+                      <span class="ml-2 align-middle" v-html="filter.html"></span>
+                    </div>
+                    <div class="col-auto">
+                      <span class="badge badge-info float-right" data-cy="filterCount">{{ filter.count }}</span>
+                    </div>
+                  </div>
                 </b-dropdown-item>
+              </b-dropdown-group>
             </b-dropdown>
-        </div>
-        <div class="col-sm px-sm-0">
-      <span v-if="selectedFilter" class="border skills-card-theme-border rounded py-1 px-2 border-info ml-2" data-cy="selectedFilter">
-        <i class="text-center" :class="selectedFilter.icon"></i> <span style="font-size: 0.8rem;" v-html="selectedFilter.html"></span>
-        <button type="button" class="btn btn-link px-0 text-info" @click="clearSelection" data-cy="clearSelectedFilter">
-          <i class="fas fa-times-circle ml-1"></i>
+      <b-badge v-if="selectedFilter" variant="light" class="mx-1 py-1 border-info border selected-filter"
+               data-cy="selectedFilter"
+               pill>
+        <i :class="selectedFilter.icon" class="ml-1"></i> <span v-html="selectedFilter.html"></span>
+        <button type="button" class="btn btn-link p-0" @click="clearSelection" data-cy="clearSelectedFilter">
+          <i class="fas fa-times-circle ml-1" style="font-size: 0.9rem;"></i>
           <span class="sr-only">clear filter</span>
         </button>
-      </span>
-        </div>
+      </b-badge>
     </div>
 </template>
 
@@ -49,6 +60,7 @@ limitations under the License.
     data() {
       return {
         selectedFilter: null,
+        flattenedFilters: [],
       };
     },
     mounted() {
@@ -65,14 +77,20 @@ limitations under the License.
     methods: {
       updateFilters() {
         const keys = Object.keys(this.counts);
+        console.log(JSON.stringify(this.filters));
+        this.flattenedFilters = this.filters.map((group) => group.filterItems).flat();
+        console.log(this.flattenedFilters);
         keys.map((key) => {
-          const filter = this.filters.find((item) => item.id === key);
-          filter.count = this.counts[key];
+          const filter = this.flattenedFilters.find((item) => item.id === key);
+          // some of the filters are optionally configured based on the page
+          if (filter) {
+            filter.count = this.counts[key];
+          }
           return key;
         });
       },
       filterSelected(filterId) {
-        const filter = this.filters.find((item) => item.id === filterId);
+        const filter = this.flattenedFilters.find((item) => item.id === filterId);
         this.selectedFilter = filter;
         this.$emit('filter-selected', filter);
       },
@@ -84,12 +102,19 @@ limitations under the License.
   };
 </script>
 
-<style>
-    /* by default dropdown button will keep outline when :focus is applied */
-    .dropdown-no-outline button {
-        outline: none 0 !important;
-        box-shadow: none !important;
-        -moz-box-shadow: none !important;
-        -webkit-box-shadow: none !important;
+<style scoped>
+    .selected-filter {
+      font-size: 0.82rem;
+    }
+    .skills-dropdown-item {
+      min-width: 18rem !important;
+      font-size: 0.95rem !important;
+    }
+    .skills-dropdown-item i {
+      min-width: 2rem;
+      font-size: 1.1rem;
+    }
+    .skills-dropdown-item .badge {
+      font-size: 0.85rem;
     }
 </style>
