6ead480b36f048902216fdacaa038bfab63e9ff5
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2023-02-08 15:14:10 -0500

    #1814 Add another test for user tags

---
+++ service/src/test/java/skills/intTests/metrics/skill/AchievementsByTagMetricsBuilderSpec.groovy
@@ -0,0 +1,64 @@
+package skills.intTests.metrics.skill
+
+import org.springframework.beans.factory.annotation.Autowired
+import skills.intTests.utils.DefaultIntSpec
+import skills.intTests.utils.SkillsFactory
+import skills.intTests.utils.SkillsService
+import skills.metrics.builders.skill.AchievementsByTagMetricsBuilder
+
+class AchievementsByTagMetricsBuilderSpec  extends DefaultIntSpec {
+    @Autowired
+    AchievementsByTagMetricsBuilder builder
+
+    def "produces accurate achievement user list"() {
+        def proj = SkillsFactory.createProject()
+        def skill = SkillsFactory.createSkill(1, 1, 1, 0, 5,  )
+        skill.pointIncrement = 150
+        def subj = SkillsFactory.createSubject()
+        skillsService.createProject(proj)
+        skillsService.createSubject(subj)
+        skillsService.createSkill(skill)
+
+        def users = getRandomUsers(7)
+
+        SkillsService rootUser = createRootSkillService()
+
+        users[0..2].forEach(user -> {
+            createService(user)
+            rootUser.saveUserTag(user, 'someTag', ['ABCDE'])
+        })
+
+        users[3..6].forEach(user -> {
+            createService(user)
+            rootUser.saveUserTag(user, 'someTag', ['DEFGH'])
+        })
+
+        assert skillsService.addSkill(skill, users[0], new Date() - 4).body.skillApplied
+        assert skillsService.addSkill(skill, users[0], new Date() - 3).body.skillApplied
+        assert skillsService.addSkill(skill, users[0], new Date() - 2).body.skillApplied
+        assert skillsService.addSkill(skill, users[0], new Date() - 1).body.skillApplied
+        assert skillsService.addSkill(skill, users[1], new Date()).body.skillApplied
+        assert skillsService.addSkill(skill, users[2], new Date() - 2).body.skillApplied
+        assert skillsService.addSkill(skill, users[3], new Date() - 9).body.skillApplied
+        assert skillsService.addSkill(skill, users[4], new Date() - 8).body.skillApplied
+        assert skillsService.addSkill(skill, users[4], new Date() - 7).body.skillApplied
+        assert skillsService.addSkill(skill, users[4], new Date() - 6).body.skillApplied
+        assert skillsService.addSkill(skill, users[5], new Date() - 7).body.skillApplied
+        assert skillsService.addSkill(skill, users[5], new Date() - 6).body.skillApplied
+        assert skillsService.addSkill(skill, users[5], new Date() - 5).body.skillApplied
+        assert skillsService.addSkill(skill, users[5], new Date() - 4).body.skillApplied
+        assert skillsService.addSkill(skill, users[5], new Date() - 3).body.skillApplied
+        assert skillsService.addSkill(skill, users[6], new Date() - 3).body.skillApplied
+        assert skillsService.addSkill(skill, users[6], new Date()).body.skillApplied
+
+        when:
+        def props = ["skillId": skill.skillId, "userTagKey": "someTag"]
+        def result = builder.build(proj.projectId, builder.id, props)
+
+        then:
+        result['ABCDE'].numberAchieved == 0
+        result['ABCDE'].numberInProgress == 3
+        result['DEFGH'].numberAchieved == 1
+        result['DEFGH'].numberInProgress == 3
+    }
+}
