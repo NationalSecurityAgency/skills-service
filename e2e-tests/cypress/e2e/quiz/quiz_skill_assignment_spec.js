5d491895d5fb5d743326e457d58a0397385b0d96
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2023-04-06 09:28:51 -0400

    #2029 Add a test for filtering

--- e2e-tests/cypress/e2e/quiz/quiz_skill_assignment_spec.js
+++ e2e-tests/cypress/e2e/quiz/quiz_skill_assignment_spec.js
@@ -319,14 +319,74 @@ describe('Quiz Skill Assignment Tests', () => {
                 value: 'proj1'
             }, {
                 colIndex: 1,
-                value: ' Very Great Skill 1 ID: skill1'
+                value: 'Very Great Skill 1ID: skill1'
             }],
             [{
                 colIndex: 0,
                 value: 'proj1'
             }, {
                 colIndex: 1,
-                value: ' Very Great Skill 2 ID: skill2'
+                value: 'Very Great Skill 2ID: skill2'
+            }],
+        ], 5);
+    });
+
+    it('filter skill table for a quiz', function() {
+        cy.createProject(1)
+        cy.createSubject(1,1)
+
+        cy.createQuizDef(1, {name: 'Test Your Trivia Knowledge'});
+        cy.createSkill(1, 1, 1, { selfReportingType: 'Quiz', quizId: 'quiz1',  pointIncrement: '150', numPerformToCompletion: 1 });
+        cy.createSkill(1, 1, 2, { selfReportingType: 'Quiz', quizId: 'quiz1',  pointIncrement: '150', numPerformToCompletion: 1 });
+
+        cy.visit('/administrator/quizzes/quiz1/skills')
+        const tableSelector = '[data-cy="quizSkills"]';
+
+        cy.validateTable(tableSelector, [
+            [{
+                colIndex: 0,
+                value: 'proj1'
+            }, {
+                colIndex: 1,
+                value: 'Very Great Skill 1ID: skill1'
+            }],
+            [{
+                colIndex: 0,
+                value: 'proj1'
+            }, {
+                colIndex: 1,
+                value: 'Very Great Skill 2ID: skill2'
+            }],
+        ], 5);
+
+        cy.get('[data-cy="quiz-skillNameFilter"]').type('2');
+        cy.get('[data-cy="quiz-filterBtn"]').click();
+        cy.validateTable(tableSelector, [
+            [{
+                colIndex: 0,
+                value: 'proj1'
+            }, {
+                colIndex: 1,
+                value: 'Very Great Skill 2ID: skill2'
+            }],
+        ], 5);
+
+        cy.get('[data-cy="quiz-resetBtn"]').click();
+
+        cy.validateTable(tableSelector, [
+            [{
+                colIndex: 0,
+                value: 'proj1'
+            }, {
+                colIndex: 1,
+                value: 'Very Great Skill 1ID: skill1'
+            }],
+            [{
+                colIndex: 0,
+                value: 'proj1'
+            }, {
+                colIndex: 1,
+                value: 'Very Great Skill 2ID: skill2'
             }],
         ], 5);
     });
