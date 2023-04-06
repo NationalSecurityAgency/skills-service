d4a55bfe05ce36e7a557e5af0ed985c0c339f166
EVOFORGE\dimay <dimay@evoforge.org>
2023-04-06 16:37:14 -0400

    #2030: quiz metrics - WIP

--- service/src/test/java/skills/intTests/quiz/QuizMetricsSpecs.groovy
+++ service/src/test/java/skills/intTests/quiz/QuizMetricsSpecs.groovy
@@ -86,26 +86,137 @@ class QuizMetricsSpecs extends DefaultIntSpec {
         q2_metrics.avgAttemptRuntimeInMs >= 810
     }
 
-    def "quiz metrics - answers: single choice answer"() {
+    def "quiz metrics - answers: single choice question"() {
         List<String> users = getRandomUsers(5, true)
 
         def quizInfo = createSimpleQuiz(1)
-        runSimpleQuiz(users[0], quizInfo, true, false, 600)
-        runSimpleQuiz(users[1], quizInfo, false, false, 700)
+        runSimpleQuizByProvidingAnswers(users[0], quizInfo, [[0], [0, 2]]) // pass quiz example
+        runSimpleQuizByProvidingAnswers(users[1], quizInfo, [[0], [0]]) // fail
+        runSimpleQuizByProvidingAnswers(users[1], quizInfo, [[1], [0, 2]]) // fail
+        runSimpleQuizByProvidingAnswers(users[1], quizInfo, [[1], [0, 2]]) // fail
+        runSimpleQuizByProvidingAnswers(users[1], quizInfo, [[0], [0, 2]]) // pass
+        runSimpleQuizByProvidingAnswers(users[2], quizInfo, [[0], [0, 2]]) // pass
+        runSimpleQuizByProvidingAnswers(users[3], quizInfo, [[1], [0, 2]]) // pass
+        runSimpleQuizByProvidingAnswers(users[4], quizInfo, [[1], [0, 2]], true) //  not complete
 
         def quizInfo2 = createSimpleQuiz(2)
-        runSimpleQuiz(users[0], quizInfo2, true, false, 800)
-        runSimpleQuiz(users[1], quizInfo2, false, false, 820)
+        runSimpleQuizByProvidingAnswers(users[0], quizInfo2, [[0], [0, 2]]) // pass quiz example
+
+        def quizInfo3 = createSimpleQuiz(3)
+        runSimpleQuizByProvidingAnswers(users[0], quizInfo3, [[1], [0, 2]])
 
         when:
         def q1_metrics = skillsService.getQuizMetrics(quizInfo.quizId)
         def q2_metrics = skillsService.getQuizMetrics(quizInfo2.quizId)
-        println JsonOutput.prettyPrint(JsonOutput.toJson(q1_metrics))
+        def q3_metrics = skillsService.getQuizMetrics(quizInfo3.quizId)
+        println JsonOutput.prettyPrint(JsonOutput.toJson(q3_metrics))
         then:
-        q1_metrics.numTaken == 2
-        q1_metrics.avgAttemptRuntimeInMs >= 650
-        q2_metrics.avgAttemptRuntimeInMs >= 810
+        q1_metrics.numTaken == 7
+        def q1_question = q1_metrics.questions.find { it.questionType == QuizQuestionType.SingleChoice.toString() }
+        q1_question.numAnsweredCorrect == 4
+        q1_question.numAnsweredWrong == 3
+        q1_question.answers[0].isCorrect == true
+        q1_question.answers[0].numAnswered == 4
+        q1_question.answers[0].numAnsweredCorrect == 4
+        q1_question.answers[0].numAnsweredWrong == 0
+        q1_question.answers[1].isCorrect == false
+        q1_question.answers[1].numAnswered == 3
+        q1_question.answers[1].numAnsweredCorrect == 0
+        q1_question.answers[1].numAnsweredWrong == 3
+
+        q2_metrics.numTaken == 1
+        def q2_question = q2_metrics.questions.find { it.questionType == QuizQuestionType.SingleChoice.toString() }
+        q2_question.numAnsweredCorrect == 1
+        q2_question.numAnsweredWrong == 0
+        q2_question.answers[0].isCorrect == true
+        q2_question.answers[0].numAnswered == 1
+        q2_question.answers[0].numAnsweredCorrect == 1
+        q2_question.answers[0].numAnsweredWrong == 0
+        q2_question.answers[1].isCorrect == false
+        q2_question.answers[1].numAnswered == 0
+        q2_question.answers[1].numAnsweredCorrect == 0
+        q2_question.answers[1].numAnsweredWrong == 0
+
+        q3_metrics.numTaken == 1
+        def q3_question = q3_metrics.questions.find { it.questionType == QuizQuestionType.SingleChoice.toString() }
+        q3_question.numAnsweredCorrect == 0
+        q3_question.numAnsweredWrong == 1
+        q3_question.answers[0].isCorrect == true
+        q3_question.answers[0].numAnswered == 0
+        q3_question.answers[0].numAnsweredCorrect == 0
+        q3_question.answers[0].numAnsweredWrong == 0
+        q3_question.answers[1].isCorrect == false
+        q3_question.answers[1].numAnswered == 1
+        q3_question.answers[1].numAnsweredCorrect == 0
+        q3_question.answers[1].numAnsweredWrong == 1
     }
+
+    def "quiz metrics - answers: multiple choice question"() {
+        List<String> users = getRandomUsers(5, true)
+
+        def quizInfo = createSimpleQuiz(1)
+        runSimpleQuizByProvidingAnswers(users[0], quizInfo, [[0], [0, 2]]) // pass quiz example
+        runSimpleQuizByProvidingAnswers(users[1], quizInfo, [[0], [0]])
+        runSimpleQuizByProvidingAnswers(users[1], quizInfo, [[0], [0, 1]])
+        runSimpleQuizByProvidingAnswers(users[1], quizInfo, [[0], [0, 3]])
+        runSimpleQuizByProvidingAnswers(users[1], quizInfo, [[0], [0, 1, 2, 3]])
+        runSimpleQuizByProvidingAnswers(users[1], quizInfo, [[0], [0, 2]])
+        runSimpleQuizByProvidingAnswers(users[2], quizInfo, [[0], [0, 1, 2, 3]], true) // not counted
+        runSimpleQuizByProvidingAnswers(users[3], quizInfo, [[0], [3]])
+        runSimpleQuizByProvidingAnswers(users[4], quizInfo, [[0], [1, 3]])
+
+        def quizInfo2 = createSimpleQuiz(2)
+        runSimpleQuizByProvidingAnswers(users[0], quizInfo2, [[0], [0, 2]]) // pass quiz example
+
+        def quizInfo3 = createSimpleQuiz(3)
+        runSimpleQuizByProvidingAnswers(users[0], quizInfo2, [[0], [1, 3]])
+
+        when:
+        def q1_metrics = skillsService.getQuizMetrics(quizInfo.quizId)
+        def q2_metrics = skillsService.getQuizMetrics(quizInfo2.quizId)
+        def q3_metrics = skillsService.getQuizMetrics(quizInfo3.quizId)
+        println JsonOutput.prettyPrint(JsonOutput.toJson(q3_metrics))
+        then:
+        q1_metrics.numTaken == 7
+        def q1_question = q1_metrics.questions.find { it.questionType == QuizQuestionType.MultipleChoice.toString() }
+        q1_question.numAnsweredCorrect == 4
+        q1_question.numAnsweredWrong == 3
+        q1_question.answers[0].isCorrect == true
+        q1_question.answers[0].numAnswered == 4
+        q1_question.answers[0].numAnsweredCorrect == 4
+        q1_question.answers[0].numAnsweredWrong == 0
+        q1_question.answers[1].isCorrect == false
+        q1_question.answers[1].numAnswered == 3
+        q1_question.answers[1].numAnsweredCorrect == 0
+        q1_question.answers[1].numAnsweredWrong == 3
+
+        q2_metrics.numTaken == 1
+        def q2_question = q2_metrics.questions.find { it.questionType == QuizQuestionType.MultipleChoice.toString() }
+        q2_question.numAnsweredCorrect == 1
+        q2_question.numAnsweredWrong == 0
+        q2_question.answers[0].isCorrect == true
+        q2_question.answers[0].numAnswered == 1
+        q2_question.answers[0].numAnsweredCorrect == 1
+        q2_question.answers[0].numAnsweredWrong == 0
+        q2_question.answers[1].isCorrect == false
+        q2_question.answers[1].numAnswered == 0
+        q2_question.answers[1].numAnsweredCorrect == 0
+        q2_question.answers[1].numAnsweredWrong == 0
+
+        q3_metrics.numTaken == 1
+        def q3_question = q3_metrics.questions.find { it.questionType == QuizQuestionType.MultipleChoice.toString() }
+        q3_question.numAnsweredCorrect == 0
+        q3_question.numAnsweredWrong == 1
+        q3_question.answers[0].isCorrect == true
+        q3_question.answers[0].numAnswered == 0
+        q3_question.answers[0].numAnsweredCorrect == 0
+        q3_question.answers[0].numAnsweredWrong == 0
+        q3_question.answers[1].isCorrect == false
+        q3_question.answers[1].numAnswered == 1
+        q3_question.answers[1].numAnsweredCorrect == 0
+        q3_question.answers[1].numAnsweredWrong == 1
+    }
+
 
     def createSimpleQuiz(Integer num) {
         def quiz = QuizDefFactory.createQuiz(num, "Fancy Description")
@@ -137,20 +248,15 @@ class QuizMetricsSpecs extends DefaultIntSpec {
         }
     }
 
-    void runSimpleQuizByProvidingAnswers(String userId, def quizInfo, List<List<Integer>> answerOptions) {
+    void runSimpleQuizByProvidingAnswers(String userId, def quizInfo, List<List<Integer>> answerOptions, boolean inProgress = false) {
         def quizAttempt =  skillsService.startQuizAttemptForUserId(quizInfo.quizId, userId).body
 
-
-        skillsService.reportQuizAnswerForUserId(quizInfo.quizId, quizAttempt.id, quizInfo.questions[0].answerOptions[0].id, userId)
-
-        skillsService.reportQuizAnswerForUserId(quizInfo.quizId, quizAttempt.id, quizInfo.questions[1].answerOptions[0].id, userId)
-        skillsService.reportQuizAnswerForUserId(quizInfo.quizId, quizAttempt.id, quizInfo.questions[1].answerOptions[2].id, userId)
-        if (!pass) {
-            skillsService.reportQuizAnswerForUserId(quizInfo.quizId, quizAttempt.id, quizInfo.questions[1].answerOptions[1].id, userId)
+        answerOptions.eachWithIndex{ List<Integer> answers, int questionNum ->
+            answers.each {
+                skillsService.reportQuizAnswerForUserId(quizInfo.quizId, quizAttempt.id, quizInfo.questions[questionNum].answerOptions[it].id, userId)
+            }
         }
-        if (sleepInMs > 0) {
-            Thread.sleep(sleepInMs)
-        }
+
         if (!inProgress) {
             skillsService.completeQuizAttemptForUserId(quizInfo.quizId, quizAttempt.id, userId).body
         }
