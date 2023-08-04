3c2d9253da57ad7936ed59070b06dd4125302277
EVOFORGE\dimay <dimay@evoforge.org>
2023-08-03 20:04:18 -0400

    #2353: refactored to break circular references

--- service/src/main/java/skills/auth/form/LocalUserDetailsService.groovy
+++ service/src/main/java/skills/auth/form/LocalUserDetailsService.groovy
@@ -19,15 +19,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
-import org.springframework.security.crypto.password.PasswordEncoder
 
 class LocalUserDetailsService implements UserDetailsService {
 
     @Autowired
     skills.auth.UserAuthService userAuthService
-
-    @Autowired
-    PasswordEncoder passwordEncoder
 
     @Override
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
