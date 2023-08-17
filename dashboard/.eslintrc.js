802a3a59b8d31ac5dbb359f6355373e893787cae
EVOFORGE\dimay <dimay@evoforge.org>
2023-08-17 09:37:25 -0400

    #2375: fixed eslint conf

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -89,7 +89,7 @@ module.exports = {
       },
     ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/multi-word-component-names': ['error', {
+    'vue/multi-word-component-names': ['off', {
       'ignores': ['default']
     }]
   },
