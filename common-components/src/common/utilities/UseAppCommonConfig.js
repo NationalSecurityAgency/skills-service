189c96f695f5ffb2f3b1cbba78e6b844e12edbb8
Dima May <sudo.may1@gmail.com>
2024-01-24 16:44:53 +0000

    #2490: refactored markdown into its own directory

---
+++ common-components/src/common/utilities/UseAppCommonConfig.js
@@ -0,0 +1,19 @@
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+export const useAppCommonConfig = () => {
+  const store = useStore()
+
+  const allowedAttachmentFileTypes = store.getters.config.allowedAttachmentFileTypes
+  const maxAttachmentSize = store.getters.config.maxAttachmentSize ? Number(store.getters.config.maxAttachmentSize) : 0;
+  const attachmentWarningMessage = store.getters.config.attachmentWarningMessage
+  const allowedAttachmentMimeTypes = store.getters.config.allowedAttachmentMimeTypes
+  const docsHost = store.getters.config.docsHost
+  return {
+    allowedAttachmentFileTypes,
+    maxAttachmentSize,
+    attachmentWarningMessage,
+    allowedAttachmentMimeTypes,
+    docsHost
+  }
+}
