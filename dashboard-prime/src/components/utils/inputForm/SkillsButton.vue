b8fb28d67fbdce9de0ac21e90dbb88b57e019f98
Dima May <sudo.may1@gmail.com>
2024-01-25 12:18:24 +0000

    #2490: created reusable SkillsButton component that support icon and loading props

--- dashboard-prime/src/components/utils/inputForm/SkillsButton.vue
+++ dashboard-prime/src/components/utils/inputForm/SkillsButton.vue
@@ -9,11 +9,15 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
 })
 </script>
 
 <template>
-  <Button>
+  <Button :disabled="disabled || loading">
     <span>{{ label }}</span><i v-if="!loading" class="ml-1" :class="icon" aria-hidden="true" style="width: 0.9rem; height: 0.9rem;"></i>
     <ProgressSpinner v-if="loading" style="width: 0.9rem; height: 0.9rem;" class="ml-1" />
   </Button>
