22eff979211f5cf8b5301dac83214a151203c3e7
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2024-03-27 13:57:52 -0400

    #2490 Add missing component

---
+++ dashboard-prime/src/components/utils/cards/SkillsCardHeader.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['title']);
+</script>
+
+<template>
+  <div class="border-bottom-1 p-3 surface-border surface-100 mt-4">
+    <span class="font-bold">{{ title }}</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
