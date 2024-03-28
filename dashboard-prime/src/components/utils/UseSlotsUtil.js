c0bdc130f5142982ac79bac1e82b11b72cf81489
rmmayo <ryan.mayo@softtech-solutions.com>
2024-03-28 12:23:12 +0000

    #2490 - adding UseSlotsUtil.js

---
+++ dashboard-prime/src/components/utils/UseSlotsUtil.js
@@ -0,0 +1,14 @@
+import { computed, useSlots } from 'vue';
+
+
+export const useSlotsUtil = () => {
+  const slots = useSlots();
+
+  const hasSlot = (name = 'default') => {
+    return !!(slots && slots[name]);
+  }
+
+  return {
+    hasSlot
+  }
+}
