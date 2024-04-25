121b2ce54cc2d12328ec62daaf93e392b22dc075
rmmayo <ryan.mayo@softtech-solutions.com>
2024-04-25 15:24:47 +0000

    #2490 - implement Quiz Runs in dashboard and skills-display - WIP

---
+++ dashboard-prime/src/skills-display/components/skills/QuizPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import { computed, onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import QuizService from '@/components/quiz/QuizService.js'
+// import QuizRun from '@/common-components/quiz/QuizRun';
+
+</script>
+
+<template>
+  <div>
+    This is the Quiz Page
+  </div>
+</template>
+
+<style scoped>
+
+</style>
