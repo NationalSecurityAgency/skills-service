3b8b44590cae037914985b9996a66157cef608ac
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2024-05-08 12:25:36 -0400

    #2490 Restore subject page setting

--- dashboard-prime/src/components/subjects/SubjectPage.vue
+++ dashboard-prime/src/components/subjects/SubjectPage.vue
@@ -28,7 +28,7 @@ onMounted(() => {
 })
 
 const isLoadingData = computed(() => {
-  return subjectState.isLoadingSubject; // || projConfig.loadingProjConfig
+  return subjectState.isLoadingSubject.value; // || projConfig.loadingProjConfig
 })
 
 const navItems = computed(() => {
