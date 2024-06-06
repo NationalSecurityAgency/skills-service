0c22edeacc2e492013a504bc7f84cd12ca42c4b7
rmmayo <ryan.mayo@softtech-solutions.com>
2024-06-06 11:56:37 +0000

    #2490 - add `/static` to the list of backendResources

--- service/src/main/java/skills/controller/filters/VueEntryPointFilterUtils.groovy
+++ service/src/main/java/skills/controller/filters/VueEntryPointFilterUtils.groovy
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Component
 @Component
 class VueEntryPointFilterUtils {
     private final List<String> backendResources =
-            Collections.unmodifiableList("/api,/admin/,/app,/assets,/themes,/clientDisplay,/favicon.ico,/skilltree.ico,/icons,/performLogin,/createAccount,/createRootAccount,/grantFirstRoot,/userExists,/oauth,/login,/logout,/root,/supervisor,/public,/skills-websocket,/resetPassword,/performPasswordReset,/metrics/global,/verifyEmail,/resendEmailVerification,/userEmailIsVerified,/actuator".split(",").toList())
+            Collections.unmodifiableList("/api,/admin/,/app,/assets,/themes,/clientDisplay,/favicon.ico,/skilltree.ico,/icons,/static,/performLogin,/createAccount,/createRootAccount,/grantFirstRoot,/userExists,/oauth,/login,/logout,/root,/supervisor,/public,/skills-websocket,/resetPassword,/performPasswordReset,/metrics/global,/verifyEmail,/resendEmailVerification,/userEmailIsVerified,/actuator".split(",").toList())
 
     boolean isFrontendResource(String pathInfo) {
         return !isBackendResource(pathInfo)
