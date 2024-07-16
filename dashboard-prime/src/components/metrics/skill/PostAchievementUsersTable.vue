fe96b19580e354f56b69ba77826e11c500ab691c
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2024-07-16 15:26:00 -0400

    #2496 Update user link in table

--- dashboard-prime/src/components/metrics/skill/PostAchievementUsersTable.vue
+++ dashboard-prime/src/components/metrics/skill/PostAchievementUsersTable.vue
@@ -98,13 +98,10 @@ const sortTable = (sortContext) => {
 };
 
 const calculateClientDisplayRoute = (props) => {
-  // ClientDisplayPreviewSkill
   return {
     name: `SkillsDisplay${SkillsDisplayPathAppendValues.SkillsDisplayPreview}`,
     params: {
       projectId: route.params.projectId,
-      subjectId: route.params.subjectId,
-      skillId: route.params.skillId,
       userId: props.userId,
       dn: props.dn,
     },
@@ -141,17 +138,9 @@ const calculateClientDisplayRoute = (props) => {
                          v-model:sort-order="sortOrder">
           <Column field="userId" header="User" sortable>
             <template #body="slotProps">
-              <div class="flex">
-                <div class="flex flex-1">
-                  {{ userInfo.getUserDisplay(slotProps.data, true) }}
-                </div>
-<!--                <router-link :to="calculateClientDisplayRoute(slotProps.data)" tabindex="-1">-->
-                  <SkillsButton size="small" class="text-secondary"
-                                :aria-label="`View details for user ${userInfo.getUserDisplay(slotProps.data)}`"
-                                data-cy="usersTable_viewDetailsBtn"><i class="fa fa-user-alt" aria-hidden="true"/><span class="sr-only">view user details</span>
-                  </SkillsButton>
-<!--                </router-link>-->
-              </div>
+              <router-link :to="calculateClientDisplayRoute(slotProps.data)" tabindex="-1">
+                {{ userInfo.getUserDisplay(slotProps.data, true) }}
+              </router-link>
             </template>
           </Column>
           <Column field="count" header="Times Performed" sortable></Column>
