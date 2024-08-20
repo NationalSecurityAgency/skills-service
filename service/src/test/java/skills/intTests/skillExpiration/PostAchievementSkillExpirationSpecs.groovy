f96c19bbc5d8528f906b61fccf8b67bfeac74ae5
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2024-08-20 12:28:48 -0400

    #2530 Add another test case

--- service/src/test/java/skills/intTests/skillExpiration/PostAchievementSkillExpirationSpecs.groovy
+++ service/src/test/java/skills/intTests/skillExpiration/PostAchievementSkillExpirationSpecs.groovy
@@ -2210,7 +2210,7 @@ class PostAchievementSkillExpirationSpecs extends DefaultIntSpec {
         newQuizRuns.totalCount == 2
     }
 
-    def "can complete quiz again if expiring in a day"() {
+    def "can complete quiz again if skill is expiring in a day"() {
         def quiz = QuizDefFactory.createQuiz(1, "Fancy Description")
         skillsService.createQuizDef(quiz)
         def questions = QuizDefFactory.createChoiceQuestions(1, 1, 2)
@@ -2246,5 +2246,42 @@ class PostAchievementSkillExpirationSpecs extends DefaultIntSpec {
         then:
         def newQuizRuns = skillsService.getQuizRuns(quiz.quizId, 10, 1, 'started', true, '')
         newQuizRuns.totalCount == 2
+    }
+
+    def "can not complete quiz again if skill is expiring in more than a day"() {
+        def quiz = QuizDefFactory.createQuiz(1, "Fancy Description")
+        skillsService.createQuizDef(quiz)
+        def questions = QuizDefFactory.createChoiceQuestions(1, 1, 2)
+        skillsService.createQuizQuestionDefs(questions)
+
+        def proj = SkillsFactory.createProject(1)
+        def subj = SkillsFactory.createSubject(1)
+        def skills = SkillsFactory.createSkills(1, 1, 1, 200, 1)
+        skills[0].selfReportingType = SkillDef.SelfReportingType.Quiz
+        skills[0].quizId = quiz.quizId
+
+        skillsService.createProject(proj)
+        skillsService.createSubject(subj)
+        skillsService.createSkills(skills)
+
+        skillsService.saveSkillExpirationAttributes( proj.projectId, "skill1", [ expirationType: ExpirationAttrs.DAILY, every: 2 ]);
+
+        def quizInfo = skillsService.getQuizInfo(quiz.quizId)
+
+        List<String> users = getRandomUsers(1, true)
+        def quizAttempt =  skillsService.startQuizAttemptForUserId(quiz.quizId, users[0], "skill1", proj.projectId).body
+        skillsService.reportQuizAnswerForUserId(quiz.quizId, quizAttempt.id, quizInfo.questions[0].answerOptions[0].id, users[0])
+        skillsService.completeQuizAttemptForUserId(quiz.quizId, quizAttempt.id, users[0]).body
+
+        when:
+        def initialQuizRuns = skillsService.getQuizRuns(quiz.quizId, 10, 1, 'started', true, '')
+        initialQuizRuns.totalCount == 1
+        initialQuizRuns.data[0].status == "PASSED"
+        def quizAttempt2 =  skillsService.startQuizAttemptForUserId(quiz.quizId, users[0], "skill1", proj.projectId).body
+
+        then:
+        SkillsClientException exception = thrown()
+        exception.httpStatus == HttpStatus.BAD_REQUEST
+        exception.message.contains('User [' + users[0] + '] already took and passed this quiz.')
     }
 }
