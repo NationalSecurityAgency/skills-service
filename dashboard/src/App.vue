5ab005977994502be3839cf89f6b6121dbd485c4
govindaraja.vanga <govindaraja.vanga@servicenow.com>
2024-09-28 00:22:21 +0530

    Removed duplicatelines

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -39,8 +39,6 @@ import { invoke, until } from '@vueuse/core'
 import DashboardFooter from '@/components/header/DashboardFooter.vue'
 import { useUserAgreementInterceptor } from '@/interceptors/UseUserAgreementInterceptor.js'
 import PkiAppBootstrap from '@/components/access/PkiAppBootstrap.vue'
-import { usePrimeVue } from 'primevue/config'
-import ScrollToTop from '@/common-components/utilities/ScrollToTop.vue'
 import IconManagerService from '@/components/utils/iconPicker/IconManagerService.js'
 import log from 'loglevel';
 
