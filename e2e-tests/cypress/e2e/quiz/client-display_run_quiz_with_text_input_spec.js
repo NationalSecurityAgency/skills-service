9d24e72a2a6f7160f77c3c8986c6429d90842d50
Dima May <sudo.may1@gmail.com>
2024-10-17 19:01:33 +0000

    #2188: max length validation for answers and associated cypress tests; added tests for multiple Input Text questions quiz

--- e2e-tests/cypress/e2e/quiz/client-display_run_quiz_with_text_input_spec.js
+++ e2e-tests/cypress/e2e/quiz/client-display_run_quiz_with_text_input_spec.js
@@ -102,6 +102,48 @@ describe('Run Quizzes With Text Input Questions', () => {
         cy.get('[data-cy="takeQuizBtn"]').should('not.exist')
     });
 
+    it('run quiz with multiple questions and multiple Text Input questions', () => {
+        cy.createQuizDef(1);
+        cy.createQuizQuestionDef(1, 1)
+        cy.createTextInputQuestionDef(1, 2)
+        cy.createQuizQuestionDef(1, 3)
+        cy.createTextInputQuestionDef(1, 4)
+        cy.createTextInputQuestionDef(1, 5)
+
+        cy.createProject(1)
+        cy.createSubject(1,1)
+        cy.createSkill(1, 1, 1, { selfReportingType: 'Quiz', quizId: 'quiz1',  pointIncrement: '150', numPerformToCompletion: 1 });
+
+        cy.cdVisit('/subjects/subj1/skills/skill1/quizzes/quiz1');
+        cy.get('[data-cy="title"]').contains('Quiz')
+        cy.get('[data-cy="quizSplashScreen"] [data-cy="quizInfoCard"] [data-cy="numQuestions"]').should('have.text', '5')
+        cy.get('[data-cy="quizSplashScreen"] [data-cy="quizInfoCard"] [data-cy="numAttempts"]').should('have.text', '0 / Unlimited')
+        cy.get('[data-cy="quizSplashScreen"] [data-cy="quizDescription"]').contains('What a cool quiz #1! Thank you for taking it!')
+
+        cy.get('[data-cy="cancelQuizAttempt"]').should('be.enabled')
+        cy.get('[data-cy="startQuizAttempt"]').should('be.enabled')
+
+        cy.get('[data-cy="startQuizAttempt"]').click()
+        cy.get('[data-cy="question_1"] [data-cy="answer_1"]').click()
+        cy.get('[data-cy="question_2"] [data-cy="markdownEditorInput"]').type('Answer # 1')
+        cy.get('[data-cy="question_3"] [data-cy="answer_3"]').click()
+        cy.get('[data-cy="question_4"] [data-cy="markdownEditorInput"]').type('Answer # 2')
+        cy.get('[data-cy="question_5"] [data-cy="markdownEditorInput"]').type('Answer # 3')
+
+        cy.get('[data-cy="completeQuizBtn"]').should('be.enabled').click()
+
+        cy.get('[data-cy="requiresManualGradingMsg"]').should( 'exist' )
+        cy.get('[data-cy="quizCompletion"]').should( 'not.contain', 'Congrats!!')
+        cy.get('[data-cy="numAttemptsInfoCard"]').should('not.exist')
+
+        cy.get('[data-cy="quizCompletion"] [data-cy="closeQuizBtn"]').should('be.enabled').click()
+        cy.get('[data-cy="skillProgressTitle"]').contains('Very Great Skill 1')
+        cy.get('[data-cy="overallPointsEarnedCard"] [data-cy="mediaInfoCardTitle"]').should( 'have.text', '0 Total')
+        cy.get('[data-cy="quizRequiresGradingMsg"]')
+        cy.get('[data-cy="takeQuizMsg"]').should('not.exist')
+        cy.get('[data-cy="takeQuizBtn"]').should('not.exist')
+    });
+
     it('show input text needs grading when quizAlwaysShowCorrectAnswers property is enabled', () => {
         cy.createQuizDef(1);
         cy.createQuizQuestionDef(1, 1)
@@ -233,6 +275,83 @@ describe('Run Quizzes With Text Input Questions', () => {
         cy.get('[data-cy="question_1"] [data-cy="markdownEditorInput"]').type('Answer # 1')
 
         cy.get('[data-cy="completeQuizBtn"]').should('be.enabled').click()
+
+        cy.get('[data-cy="requiresManualGradingMsg"]').should( 'exist' )
+        cy.get('[data-cy="quizCompletion"]').should( 'not.contain', 'Congrats!!')
+        cy.get('[data-cy="numAttemptsInfoCard"]').should('not.exist')
+    });
+
+    it('Input Text validation: max num chars', () => {
+        cy.createQuizDef(1);
+        cy.createTextInputQuestionDef(1, 1)
+
+        cy.createProject(1)
+        cy.createSubject(1,1)
+        cy.createSkill(1, 1, 1, { selfReportingType: 'Quiz', quizId: 'quiz1',  pointIncrement: '150', numPerformToCompletion: 1 });
+
+        cy.cdVisit('/subjects/subj1/skills/skill1');
+        cy.get('[data-cy="takeQuizMsg"]')
+        cy.get('[data-cy="takeQuizBtn"]').click()
+        cy.get('[data-cy="title"]').contains('Quiz')
+        cy.get('[data-cy="quizSplashScreen"]').contains('You will earn 150 points for Very Great Skill 1 skill by passing this quiz')
+        cy.get('[data-cy="quizSplashScreen"] [data-cy="quizInfoCard"] [data-cy="numQuestions"]').should('have.text', '1')
+        cy.get('[data-cy="quizSplashScreen"] [data-cy="quizInfoCard"] [data-cy="numAttempts"]').should('have.text', '0 / Unlimited')
+
+        cy.get('[data-cy="quizSplashScreen"] [data-cy="quizDescription"]').contains('What a cool quiz #1! Thank you for taking it!')
+
+        cy.get('[data-cy="cancelQuizAttempt"]').should('be.enabled')
+        cy.get('[data-cy="startQuizAttempt"]').should('be.enabled')
+
+        cy.get('[data-cy="startQuizAttempt"]').click()
+        const str = 'a'.repeat(100)
+        cy.get('[data-cy="question_1"] [data-cy="markdownEditorInput"] .toastui-editor-contents').invoke('text', str)
+        cy.get('[data-cy="question_1"] [data-cy="markdownEditorInput"]').type('b')
+        cy.get('[data-cy="question_1"] [data-cy="descriptionError"]').contains('Answer to question #1 must not exceed 100 characters')
+        cy.get('[data-cy="completeQuizBtn"]').click()
+        cy.wait(1000)
+        cy.get('[data-cy="question_1"] [data-cy="descriptionError"]').contains('Answer to question #1 must not exceed 100 characters')
+
+        cy.get('[data-cy="question_1"] [data-cy="markdownEditorInput"]').type('{backspace}')
+        cy.get('[data-cy="question_1"] [data-cy="descriptionError"]').should('not.be.visible')
+        cy.get('[data-cy="completeQuizBtn"]').click()
+
+        cy.get('[data-cy="requiresManualGradingMsg"]').should( 'exist' )
+        cy.get('[data-cy="quizCompletion"]').should( 'not.contain', 'Congrats!!')
+        cy.get('[data-cy="numAttemptsInfoCard"]').should('not.exist')
+    });
+
+    it('Input Text validation: custom description validation', () => {
+        cy.createQuizDef(1);
+        cy.createTextInputQuestionDef(1, 1)
+
+        cy.createProject(1)
+        cy.createSubject(1,1)
+        cy.createSkill(1, 1, 1, { selfReportingType: 'Quiz', quizId: 'quiz1',  pointIncrement: '150', numPerformToCompletion: 1 });
+
+        cy.cdVisit('/subjects/subj1/skills/skill1');
+        cy.get('[data-cy="takeQuizMsg"]')
+        cy.get('[data-cy="takeQuizBtn"]').click()
+        cy.get('[data-cy="title"]').contains('Quiz')
+        cy.get('[data-cy="quizSplashScreen"]').contains('You will earn 150 points for Very Great Skill 1 skill by passing this quiz')
+        cy.get('[data-cy="quizSplashScreen"] [data-cy="quizInfoCard"] [data-cy="numQuestions"]').should('have.text', '1')
+        cy.get('[data-cy="quizSplashScreen"] [data-cy="quizInfoCard"] [data-cy="numAttempts"]').should('have.text', '0 / Unlimited')
+
+        cy.get('[data-cy="quizSplashScreen"] [data-cy="quizDescription"]').contains('What a cool quiz #1! Thank you for taking it!')
+
+        cy.get('[data-cy="cancelQuizAttempt"]').should('be.enabled')
+        cy.get('[data-cy="startQuizAttempt"]').should('be.enabled')
+
+        cy.get('[data-cy="startQuizAttempt"]').click()
+        cy.get('[data-cy="question_1"] [data-cy="markdownEditorInput"]').type('some jabberwocky')
+        cy.get('[data-cy="question_1"] [data-cy="descriptionError"]').contains('Answer to question #1 - paragraphs may not contain jabberwocky')
+
+        cy.get('[data-cy="completeQuizBtn"]').click()
+        cy.wait(1000)
+        cy.get('[data-cy="question_1"] [data-cy="descriptionError"]').contains('Answer to question #1 - paragraphs may not contain jabberwocky')
+
+        cy.get('[data-cy="question_1"] [data-cy="markdownEditorInput"]').type('{backspace}')
+        cy.get('[data-cy="question_1"] [data-cy="descriptionError"]').should('not.be.visible')
+        cy.get('[data-cy="completeQuizBtn"]').click()
 
         cy.get('[data-cy="requiresManualGradingMsg"]').should( 'exist' )
         cy.get('[data-cy="quizCompletion"]').should( 'not.contain', 'Congrats!!')
