f785b7facab09918d72dc695600a7421e0870ccc
rmmayo <rmmayo@softtech-solutions.com>
2024-10-24 12:51:17 +0000

    #2930 - adding copyright header

--- dashboard/src/components/access/groups/AdminGroupQuizzes.vue
+++ dashboard/src/components/access/groups/AdminGroupQuizzes.vue
@@ -1,3 +1,18 @@
+/*
+Copyright 2024 SkillTree
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 <script setup>
 
 import { computed, onMounted, ref } from 'vue'
@@ -124,7 +139,7 @@ const removeQuizFromAdminGroup = () => {
               :totalRecords="assignedQuizzes.length"
               :rowsPerPageOptions="[5, 10, 15, 20]"
               data-cy="adminGroupQuizzesTable">
-            <Column header="Name" field="name" style="width: 40%;" sortable>
+            <Column header="Name" field="name" style="width: 40%;" :sortable="true">
               <template #body="slotProps">
                 <router-link :id="slotProps.data.quizId" :to="{ name:'QuizOverview',
                     params: { quizId: slotProps.data.quizId }}"
