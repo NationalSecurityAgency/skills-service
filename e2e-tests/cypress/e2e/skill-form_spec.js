805fd326f964f689082f089680b05c403f0a150e
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2024-11-14 15:43:49 -0500

    #2926 Add a test for recovering form values after using the back button

---
+++ e2e-tests/cypress/e2e/skill-form_spec.js
@@ -0,0 +1,19 @@
+describe('Skill Form Tests', () => {
+
+    it('Form content saved on back button press', () => {
+        cy.createProject(1);
+        cy.createSubject(1, 1);
+
+        cy.visit('/administrator/projects/proj1/subjects/subj1');
+
+        cy.get('[data-cy="newSkillButton"]').click();
+        cy.get('[data-cy="skillName"]').type('testname')
+
+        cy.go('back');
+        cy.go('forward');
+
+        cy.get('[data-cy="newSkillButton"]').click();
+        cy.get('[data-cy="contentRestoredMessage"]').contains('Form\'s values have been restored from backup.');
+        cy.get('[data-cy="skillName"]').should('have.value','testname');
+    })
+})
