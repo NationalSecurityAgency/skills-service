17d60532d2dd19ec7be1c73fee51fe1b99aae9a4
rmmayo <rmmayo@softtech-solutions.com>
2024-12-03 21:30:27 +0000

    #1604 - Archive User feature - WIP

--- service/src/test/java/skills/intTests/metrics/project/NumberUsersPerLevelMetricsBuilderSpec.groovy
+++ service/src/test/java/skills/intTests/metrics/project/NumberUsersPerLevelMetricsBuilderSpec.groovy
@@ -88,12 +88,17 @@ class NumberUsersPerLevelMetricsBuilderSpec extends DefaultIntSpec {
         achieveLevelForUsers(usersCopy, skills, 5, 4)
         achieveLevelForUsers(usersCopy,skills, 1, 5)
 
-        Map props = [:]
+        Map projectProps = [:]
+        Map subjectProps = [(MetricsParams.P_SUBJECT_ID): subj.subjectId]
 
         when:
-        def res = skillsService.getMetricsData(proj.projectId, metricsId, props)
-        props[MetricsParams.P_SUBJECT_ID] = subj.subjectId
-        def res1 = skillsService.getMetricsData(proj.projectId, metricsId, props)
+        def res = skillsService.getMetricsData(proj.projectId, metricsId, projectProps)
+        def res1 = skillsService.getMetricsData(proj.projectId, metricsId, subjectProps)
+
+        skillsService.archiveUsers([users[2]], proj.projectId)
+
+        def resAfterArchive = skillsService.getMetricsData(proj.projectId, metricsId, projectProps)
+        def res1AfterArchive = skillsService.getMetricsData(proj.projectId, metricsId, subjectProps)
 
         then:
         res.size() == 5
@@ -119,6 +124,30 @@ class NumberUsersPerLevelMetricsBuilderSpec extends DefaultIntSpec {
         res1[3].count == 5
         res1[4].value == "Level 5"
         res1[4].count == 1
+
+        resAfterArchive.size() == 5
+        resAfterArchive[0].value == "Level 1"
+        resAfterArchive[0].count == 3
+        resAfterArchive[1].value == "Level 2"
+        resAfterArchive[1].count == 2
+        resAfterArchive[2].value == "Level 3"
+        resAfterArchive[2].count == 4
+        resAfterArchive[3].value == "Level 4"
+        resAfterArchive[3].count == 5
+        resAfterArchive[4].value == "Level 5"
+        resAfterArchive[4].count == 1
+
+        res1AfterArchive.size() == 5
+        res1AfterArchive[0].value == "Level 1"
+        res1AfterArchive[0].count == 3
+        res1AfterArchive[1].value == "Level 2"
+        res1AfterArchive[1].count == 2
+        res1AfterArchive[2].value == "Level 3"
+        res1AfterArchive[2].count == 4
+        res1AfterArchive[3].value == "Level 4"
+        res1AfterArchive[3].count == 5
+        res1AfterArchive[4].value == "Level 5"
+        res1AfterArchive[4].count == 1
     }
 
     def "number of users achieved per level for subject"() {
@@ -154,13 +183,17 @@ class NumberUsersPerLevelMetricsBuilderSpec extends DefaultIntSpec {
         achieveLevelForUsers(usersCopy, skillsSubj1, 1, 4, "Subject")
         achieveLevelForUsers(usersCopy, skillsSubj1, 2, 5, "Subject")
 
-        Map props = [:]
-        props[MetricsParams.P_SUBJECT_ID] = subj.subjectId
-        when:
-        def res = skillsService.getMetricsData(proj.projectId, metricsId, props)
+        Map subjectProps = [(MetricsParams.P_SUBJECT_ID): subj.subjectId]
+        Map subject1Props = [(MetricsParams.P_SUBJECT_ID): subj1.subjectId]
 
-        props[MetricsParams.P_SUBJECT_ID] = subj1.subjectId
-        def res1 = skillsService.getMetricsData(proj.projectId, metricsId, props)
+        when:
+        def res = skillsService.getMetricsData(proj.projectId, metricsId, subjectProps)
+        def res1 = skillsService.getMetricsData(proj.projectId, metricsId, subject1Props)
+
+        skillsService.archiveUsers([users[2]], proj.projectId)
+
+        def resAfterArchive = skillsService.getMetricsData(proj.projectId, metricsId, subjectProps)
+        def res1AfterArchive = skillsService.getMetricsData(proj.projectId, metricsId, subjectProps)
 
 //        println JsonOutput.toJson(res1)
         then:
@@ -187,6 +220,30 @@ class NumberUsersPerLevelMetricsBuilderSpec extends DefaultIntSpec {
         res1[3].count == 1
         res1[4].value == "Level 5"
         res1[4].count == 2
+
+        resAfterArchive.size() == 5
+        resAfterArchive[0].value == "Level 1"
+        resAfterArchive[0].count == 3
+        resAfterArchive[1].value == "Level 2"
+        resAfterArchive[1].count == 2
+        resAfterArchive[2].value == "Level 3"
+        resAfterArchive[2].count == 4
+        resAfterArchive[3].value == "Level 4"
+        resAfterArchive[3].count == 5
+        resAfterArchive[4].value == "Level 5"
+        resAfterArchive[4].count == 1
+
+        res1AfterArchive.size() == 5
+        res1AfterArchive[0].value == "Level 1"
+        res1AfterArchive[0].count == 3
+        res1AfterArchive[1].value == "Level 2"
+        res1AfterArchive[1].count == 2
+        res1AfterArchive[2].value == "Level 3"
+        res1AfterArchive[2].count == 4
+        res1AfterArchive[3].value == "Level 4"
+        res1AfterArchive[3].count == 5
+        res1AfterArchive[4].value == "Level 5"
+        res1AfterArchive[4].count == 1
     }
 
     def "number of users achieved per level for subject - different number of levels"() {
@@ -228,27 +285,32 @@ class NumberUsersPerLevelMetricsBuilderSpec extends DefaultIntSpec {
         achieveLevelForUsers(usersCopy1, skillsSubj1, 2, 5, "Subject")
         achieveLevelForUsers(usersCopy1, skillsSubj1, 1, 6, "Subject")
 
-        Map props = [:]
+        Map projectProps = [:]
+        Map subjectProps = [(MetricsParams.P_SUBJECT_ID): subj.subjectId]
+        Map subject1Props = [(MetricsParams.P_SUBJECT_ID): subj1.subjectId]
 
         when:
-        def resOverall = skillsService.getMetricsData(proj.projectId, metricsId, props)
+        def resOverall = skillsService.getMetricsData(proj.projectId, metricsId, projectProps)
+        def resSubj1 = skillsService.getMetricsData(proj.projectId, metricsId, subjectProps)
+        def resSubj2 = skillsService.getMetricsData(proj.projectId, metricsId, subject1Props)
 
-        props[MetricsParams.P_SUBJECT_ID] = subj.subjectId
-        def resSubj1 = skillsService.getMetricsData(proj.projectId, metricsId, props)
+        skillsService.archiveUsers([users[2]], proj.projectId)
 
-        props[MetricsParams.P_SUBJECT_ID] = subj1.subjectId
-        def resSubj2 = skillsService.getMetricsData(proj.projectId, metricsId, props)
+        def resOverallAfterArchive = skillsService.getMetricsData(proj.projectId, metricsId, projectProps)
+        def resSubj1AfterArchive = skillsService.getMetricsData(proj.projectId, metricsId, subjectProps)
+        def resSubj2AfterArchive = skillsService.getMetricsData(proj.projectId, metricsId, subject1Props)
+
 
         then:
-//        resOverall.size() == 4
-//        resOverall[0].value == "Level 1"
-//        resOverall[0].count == 5
-//        resOverall[1].value == "Level 2"
-//        resOverall[1].count == 6
-//        resOverall[2].value == "Level 3"
-//        resOverall[2].count == 5
-//        resOverall[3].value == "Level 4"
-//        resOverall[3].count == 1
+        resOverall.size() == 4
+        resOverall[0].value == "Level 1"
+        resOverall[0].count == 4
+        resOverall[1].value == "Level 2"
+        resOverall[1].count == 6
+        resOverall[2].value == "Level 3"
+        resOverall[2].count == 2
+        resOverall[3].value == "Level 4"
+        resOverall[3].count == 3
 
         resSubj1.size() == 4
         resSubj1[0].value == "Level 1"
@@ -273,6 +335,40 @@ class NumberUsersPerLevelMetricsBuilderSpec extends DefaultIntSpec {
         resSubj2[4].count == 2
         resSubj2[5].value == "Level 6"
         resSubj2[5].count == 1
+
+        resOverallAfterArchive.size() == 4
+        resOverallAfterArchive[0].value == "Level 1"
+        resOverallAfterArchive[0].count == 3
+        resOverallAfterArchive[1].value == "Level 2"
+        resOverallAfterArchive[1].count == 6
+        resOverallAfterArchive[2].value == "Level 3"
+        resOverallAfterArchive[2].count == 2
+        resOverallAfterArchive[3].value == "Level 4"
+        resOverallAfterArchive[3].count == 3
+
+        resSubj1AfterArchive.size() == 4
+        resSubj1AfterArchive[0].value == "Level 1"
+        resSubj1AfterArchive[0].count == 3
+        resSubj1AfterArchive[1].value == "Level 2"
+        resSubj1AfterArchive[1].count == 2
+        resSubj1AfterArchive[2].value == "Level 3"
+        resSubj1AfterArchive[2].count == 4
+        resSubj1AfterArchive[3].value == "Level 4"
+        resSubj1AfterArchive[3].count == 5
+
+        resSubj2AfterArchive.size() == 6
+        resSubj2AfterArchive[0].value == "Level 1"
+        resSubj2AfterArchive[0].count == 1
+        resSubj2AfterArchive[1].value == "Level 2"
+        resSubj2AfterArchive[1].count == 3
+        resSubj2AfterArchive[2].value == "Level 3"
+        resSubj2AfterArchive[2].count == 2
+        resSubj2AfterArchive[3].value == "Level 4"
+        resSubj2AfterArchive[3].count == 1
+        resSubj2AfterArchive[4].value == "Level 5"
+        resSubj2AfterArchive[4].count == 2
+        resSubj2AfterArchive[5].value == "Level 6"
+        resSubj2AfterArchive[5].count == 1
     }
 
     private void achieveLevelForUsers(List<String> users, List<Map> skills, int numUsers, int level, String type = "Overall") {
