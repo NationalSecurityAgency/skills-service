9ccb8410e9f532acf22bb8a03c7211a23750a023
Dima May <sudo.may1@gmail.com>
2024-12-16 21:08:43 +0000

    #1131: added skill celebration messages; fixed responsiveness of points when honor skill is achieved without page refresh

--- dashboard/src/skills-display/components/progress/celebration/SkillAchievementMsg.vue
+++ dashboard/src/skills-display/components/progress/celebration/SkillAchievementMsg.vue
@@ -28,17 +28,27 @@ const showCelebration = computed(() => props.skill.achievedOn && timeUtils.isWit
 const storageKey = computed(() => `celebration-proj-${props.skill.projectId}-skill-${props.skill.skillId}`)
 
 
-const getPtsTag = () => {
+const ptsTag = computed(() => {
   return `<span class="p-tag p-component">${props.skill.points}</span>`
-}
+})
 const messages = [
-  `Congratulations on earning ${getPtsTag()} points! <span class="font-italic">Happy Learning!</span>`,
-  `You've earned ${getPtsTag()} points! No big deal! You'are crushing it!`,
-  `You're a rockstar! Keep earning those points!`
+  `Congratulations on earning <points/> points! <span class="font-italic">Happy Learning!</span>`,
+  `You've earned <points/> points! No big deal! You are crushing it!`,
+  `You're a rockstar! Keep earning those points!`,
+  `Way to go! <points/> points and counting!`,
+  `Unstoppable! You've collected <points/> points and you're not slowing down!`,
+  `You're on fire! Keep earning points and reaching new heights!`,
+  `Excellent work! You've earned <points/> points, something to be proud of!`,
+  `Celebrate your success! You've earned <points/> points and you deserve it!`,
+  `You collected <points/> points! Keep pushing yourself!`,
+  `You've earned <points/> points! Keep up the good work!`,
+  `You've unlocked <points/> points! Keep striving for more!`,
+  `Congratulations on <points/> points! Keep up the excellent work!`,
 ]
-const getRandomMessage = () => {
-  return messages[Math.floor(Math.random() * messages.length)]
-}
+const randomMessage = computed(() => {
+  const foundMessage = messages[Math.floor(Math.random() * messages.length)]
+  return foundMessage.replace('<points/>', ptsTag.value)
+})
 </script>
 
 <template>
@@ -48,7 +58,7 @@ const getRandomMessage = () => {
       :storage-key="storageKey" data-cy="skillAchievementCelebrationMsg">
     <template #content>
       <div class="text-left">
-        <span v-html="getRandomMessage()"/>
+        <span v-html="randomMessage"/>
       </div>
     </template>
   </achievement-msg>
