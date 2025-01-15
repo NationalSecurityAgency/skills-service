668672ffbc14689cce59d7948564690f3e0ab7bc
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2025-01-15 14:06:17 -0500

    #127 Change display of badges and ranks on client display

---
+++ dashboard/src/skills-display/components/UserProgressCard.vue
@@ -0,0 +1,114 @@
+/*
+Copyright 2024 SkillTree
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  componentName: {
+    type: String,
+    required: true,
+  },
+  userAchieved: {
+    type: String,
+    required: true,
+  },
+  totalAvailable: {
+    type: String,
+    required: true,
+  },
+  unit: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true
+  },
+  route: {
+    type: Object,
+    required: false,
+  },
+  isSummaryOnly: {
+    type: Boolean,
+    required: false,
+  }
+})
+</script>
+
+<template>
+  <Card class="skills-my-rank w-min-15rem h-full" :data-cy="componentName" :pt="{ content: { class: 'py-0' } }">
+    <template #subtitle>
+      <div class="text-center text-xl font-medium" :data-cy="`${componentName}Title`">
+        {{ title }}
+      </div>
+    </template>
+    <template #content>
+      <div class="fa-stack skills-icon user-rank-stack text-blue-300 flex flex-wrap align-items-center">
+        <i :class="`${icon} fa-stack-2x watermark-icon`" />
+
+        <div class="fa-stack-1x user-rank-text sd-theme-primary-color font-bold text-blue-700 text-lg">
+          <div class="text-3xl" style="line-height: 1.2em">{{ userAchieved }}</div>
+          <div class="mt-1">out of</div>
+          <div>{{ totalAvailable }} {{ unit }}</div>
+        </div>
+      </div>
+    </template>
+    <template #footer v-if="!isSummaryOnly">
+      <router-link
+          :to="route"
+          :aria-label="`Click to navigate to ${title} page`"
+          :data-cy="`${componentName}Btn`" tabindex="-1">
+        <Button
+            label="View"
+            icon="far fa-eye"
+            outlined class="w-full" size="small" />
+      </router-link>
+    </template>
+  </Card>
+</template>
+
+<style scoped>
+@media only screen and (min-width: 1200px) {
+  .skills-my-rank {
+    min-width: 18rem !important;
+  }
+}
+
+.skills-my-rank .skills-icon {
+  display: inline-block;
+  color: #b1b1b1;
+  margin: 5px 0;
+}
+
+.skills-my-rank .skills-icon.user-rank-stack {
+  margin: 14px 0;
+  font-size: 4.1rem;
+  width: 100%;
+  //color: #0fcc15d1;
+}
+
+.skills-my-rank .skills-icon.user-rank-stack i {
+  opacity: 0.38;
+}
+
+.skills-my-rank .user-rank-text {
+  font-size: 0.4em;
+  line-height: 0.9em;
+  background: rgba(255, 255, 255, 0.6);
+}
+</style>
--- dashboard/src/skills-display/components/rank/MyRank.vue
+++ dashboard/src/skills-display/components/rank/MyRank.vue
@@ -20,6 +20,7 @@ import { useNumberFormat } from '@/common-components/filter/UseNumberFormat.js'
 import { useSkillsDisplayInfo } from '@/skills-display/UseSkillsDisplayInfo.js'
 import { useRoute } from 'vue-router'
 import { useSkillsDisplayAttributesState } from '@/skills-display/stores/UseSkillsDisplayAttributesState.js'
+import UserProgressCard from "@/skills-display/components/UserProgressCard.vue";
 
 const attributes = useSkillsDisplayAttributesState()
 const skillsDisplayInfo = useSkillsDisplayInfo()
@@ -28,6 +29,7 @@ const numberFormat = useNumberFormat()
 const route = useRoute()
 
 const position = computed(() => numberFormat.pretty(progress.userRanking.position))
+const totalUsers = computed(() => numberFormat.pretty(progress.userRanking.numUsers))
 onMounted(() => {
   progress.loadUserSkillsRanking(route.params.subjectId)
 })
@@ -42,73 +44,17 @@ const toRankDetailsPage = computed(() => {
 </script>
 
 <template>
-<Card class="skills-my-rank w-min-15rem h-full"
-      data-cy="myRank"
-      :pt="{ content: { class: 'py-0' } }">
-  <template #subtitle>
-    <div class="text-center text-xl font-medium" data-cy="myRankTitle">
-      My Rank
-    </div>
-  </template>
-  <template #content>
-    <span class="fa-stack skills-icon user-rank-stack text-blue-300">
-        <i class="fa fa-users fa-stack-2x watermark-icon" :class="{'text-danger': progress.userRanking.optedOut}"/>
-        <strong class="fa-stack-1x text-primary user-rank-text">
-          <div v-if="!progress.loadingUserSkillsRanking">
-            <div v-if="progress.userRanking.optedOut" class="text-danger">
-              <div>Opted-Out</div>
-              <div style="font-size: 0.8rem; line-height: 1rem;" class="mb-2">Your position would be <b style="font-size: 0.9rem;" class="badge badge-danger">{{ position }}</b> if you opt-in!</div>
-            </div>
-
-            <span v-else>
-              <span class="text-blue-700 sd-theme-primary-color" data-cy="myRankPosition">{{ position }}</span>
-            </span>
-          </div>
-          <skills-spinner :is-loading="progress.loadingUserSkillsRanking"/>
-        </strong>
-      </span>
-  </template>
-  <template #footer v-if="!attributes.isSummaryOnly">
-    <router-link
-      :to="toRankDetailsPage"
-      aria-label="Click to navigate to My Rank page"
-      data-cy="myRankBtn" tabindex="-1">
-      <Button
-        label="View"
-        icon="far fa-eye"
-        outlined class="w-full" size="small"/>
-    </router-link>
-  </template>
-</Card>
+  <UserProgressCard
+      title="My Rank"
+      component-name="myRank"
+      :user-achieved="`#${position}`"
+      :total-available="totalUsers"
+      unit="users"
+      icon="fa fa-users"
+      :route="toRankDetailsPage"
+      :is-summary-only="attributes.isSummaryOnly" />
 </template>
 
 <style scoped>
-@media only screen and (min-width: 1200px) {
-  .skills-my-rank {
-    min-width: 18rem !important;
-  }
-}
 
-.skills-my-rank .skills-icon {
-  display: inline-block;
-  color: #b1b1b1;
-  margin: 5px 0;
-}
-
-.skills-my-rank .skills-icon.user-rank-stack {
-  margin: 14px 0;
-  font-size: 4.1rem;
-  width: 100%;
-  //color: #0fcc15d1;
-}
-.skills-my-rank .skills-icon.user-rank-stack i{
-  opacity: 0.38;
-}
-
-.skills-my-rank .user-rank-text {
-  font-size: 0.5em;
-  line-height: 1.2em;
-  margin-top: 1.8em;
-  background: rgba(255, 255, 255, 0.6);
-}
 </style>
