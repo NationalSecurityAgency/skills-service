c5f327373a2419b107c989be71cad146c9bd707f
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2025-02-24 15:34:21 -0500

    #3173 Modify tests with captions

--- e2e-tests/cypress/support/commands.js
+++ e2e-tests/cypress/support/commands.js
@@ -188,6 +188,13 @@ Cypress.Commands.add("enableProdMode", (projNum) => {
     });
 });
 
+Cypress.Commands.add("getDefaultCaptions", () => {
+    return 'WEBVTT\n'
+        + '\n'
+        + '1\n'
+        + '00:00:00.500 --> 00:00:04.000\n'
+        + 'This is the very first caption!\n'
+})
 
 Cypress.Commands.add("saveVideoAttrs", (projNum, skillNum, videoAttrs) => {
     const url = `/admin/projects/proj${projNum}/skills/skill${skillNum}/video`;
