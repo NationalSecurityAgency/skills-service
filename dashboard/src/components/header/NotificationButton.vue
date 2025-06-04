11f96cbd8964fafb5b89c0251b35e6e97161b8dc
Dima May <sudo.may1@gmail.com>
2025-06-04 17:51:58 +0000

    #3354: notification endpoint and display - WIP

--- dashboard/src/components/header/NotificationButton.vue
+++ dashboard/src/components/header/NotificationButton.vue
@@ -14,14 +14,58 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
+import {computed, onMounted, ref} from 'vue'
 import {useTimeoutFn} from "@vueuse/core";
 import Popover from "primevue/popover";
+import ScrollPanel from "primevue/scrollpanel";
+import MarkdownText from "@/common-components/utilities/markdown/MarkdownText.vue";
+import {useColors} from "@/skills-display/components/utilities/UseColors.js";
+import WebNotificationsService from "@/components/header/WebNotificationsService.js";
+import {useTimeUtils} from "@/common-components/utilities/UseTimeUtils.js";
+import { useStorage } from '@vueuse/core'
 
-const notifications = ref();
+const timeUtils = useTimeUtils()
+const colors = useColors()
+
+const lastViewedNotificationDate = useStorage('lastViewedNotificationDate', null)
+
+const notificationsPopover = ref();
 const toggle = (event) => {
-  notifications.value.toggle(event);
+  notificationsPopover.value.toggle(event);
+  afterNotificationsLoaded().then(() => {
+    if (latestNotification.value) {
+      lastViewedNotificationDate.value = latestNotification.value.notifiedOn
+    }
+  })
 }
+
+const latestNotification = computed(() => {
+  if (notifications.value && notifications.value.length > 0) {
+    return notifications.value.reduce((latest, current) => {
+      const currentDate = new Date(current.notifiedOn);
+      const latestDate = latest ? new Date(latest.notifiedOn) : null;
+
+      if (!latestDate || currentDate > latestDate) {
+        return current;
+      }
+      return latest;
+    }, null)
+  }
+
+  return null
+})
+const hasNonViewedNotifications = computed(() => {
+  const latestNotif = latestNotification.value
+  if (!latestNotif) {
+    return false
+  }
+
+  if (!lastViewedNotificationDate.value) {
+    return true
+  }
+
+  return latestNotif.notifiedOn > lastViewedNotificationDate.value
+})
 
 const alertNewNotif = ref(false)
 
@@ -31,66 +75,122 @@ onMounted(() => {
     alertNewNotif.value = false
   }, 5000)
 })
+
+
+const notifications = ref([])
+const loadingNotifications = ref(true)
+const afterNotificationsLoaded = () => {
+  return new Promise((resolve) => {
+    (function waitNotificationsLoaded() {
+      if (!loadingNotifications.value) return resolve(notifications.value)
+      setTimeout(waitNotificationsLoaded, 100)
+      return notifications.value
+    }())
+  })
+}
+
+onMounted(() => {
+  WebNotificationsService.getNotifications()
+      .then((res) => {
+        notifications.value = res;
+      }).finally(() => {
+    loadingNotifications.value = false
+  })
+})
+const notificationCount = computed(() => notifications.value ? notifications.value.length : 0)
+
+const acknowledgeNotification = (notification) => {
+  WebNotificationsService.ackNotification(notification.id)
+      .then(() => {
+        notifications.value = notifications.value.filter(n => n.id !== notification.id)
+      })
+}
+const loadingOrNotNotifications = computed(() => {
+  return loadingNotifications.value || notifications.value.length === 0
+})
 </script>
 
 <template>
   <div class="d-inline relative">
-    <!-- New tag element -->
-    <div v-if="alertNewNotif" class="absolute -top-1 -right-1 z-10 animate-ping">
-      <span class="bg-orange-700 text-white text-xs font-bold px-1 py-0.5 rounded">
-        new
+    <div v-if="!loadingNotifications && notificationCount > 0" class="absolute -top-2 -right-1 z-10"
+         :class="{'animate-bounce': alertNewNotif && hasNonViewedNotifications}">
+      <span class="text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center"
+            :class="{
+              'bg-orange-700 text-white': hasNonViewedNotifications,
+              'bg-green-700 text-white': !hasNonViewedNotifications
+            }">
+        {{ notificationCount > 9 ? '9+' : notificationCount }}
       </span>
     </div>
     <Button
         icon="fa-solid fa-bell"
         severity="warn"
-        badge="1"
-        badgeSeverity="green"
         outlined
         raised
         @click="toggle"
         aria-label="User Settings Button"
         aria-haspopup="true"
-        aria-controls="user_settings_menu"/>
-    <Popover ref="notifications">
-      <div class="flex flex-col w-[25rem] skills-notification">
-        <div class="flex gap-2 align-items-center border-b-1 border-b-gray-200 mb-2">
-          <div class="font-bold flex-1">Notifications</div>
-<!--          <div>-->
-<!--            <SkillsButton label="Clear All" severity="danger" icon="fa-solid fa-xmark" size="small"/>-->
-<!--          </div>-->
-        </div>
-        <div class="py-2">
-          <div class="flex gap-2 items-center">
-            <div class="font-bold flex-1 text-orange-800">SkillTree 3.6 Version Released</div>
-            <div class="text-sm text-gray-600">2023-08-28</div>
-          </div>
-          <div class="flex pl-6 mt-2 border-l-2 border-l-gray-400-400">
-            <div class="flex-1">
-              <ul class="list-disc">
-                <li>Draft mode for subject/skill creation</li>
-                <li>Audio/video in quizzes and surveys</li>
-                <li>Context-aware contact system</li>
-                <li>Keyboard shortcuts for training navigation</li>
-                <li>Screen reader-friendly headings</li>
-                <li><a href="">Learn more...</a></li>
-              </ul>
-            </div>
+        aria-controls="user_settings_menu">
+    </Button>
+    <Popover ref="notificationsPopover" aria-label="Notifications">
+      <ScrollPanel :style="`width: 100%; height: ${loadingOrNotNotifications? '200': '400'}px`">
+        <div class="flex flex-col w-[25rem] skills-notification">
+          <div class="flex gap-2 align-items-center border-b-1 border-b-gray-200 mb-2">
+            <div class="font-bold flex-1 text-orange-800 dark:text-orange-400">Notifications</div>
             <div>
-              <SkillsButton severity="danger" icon="fa-solid fa-xmark" size="small"/>
+              <!--            <SkillsButton label="Clear All" severity="danger" icon="fa-solid fa-xmark" size="small"/>-->
             </div>
           </div>
-          <!--          <div class="text-right">-->
-          <!--            <SkillsButton size="small"  label="Learn More" />-->
-          <!--          </div>-->
+
+          <div v-if="loadingNotifications" class="flex flex-col justify-center items-center pt-5">
+            <skills-spinner :is-loading="true" />
+            Loading Notifications...
+          </div>
+
+          <div v-if="!loadingNotifications && notifications.length === 0" class="py-8 text-center">
+            <div class="text-gray-400 mb-2">
+              <i class="fa-regular fa-bell-slash text-4xl"></i>
+            </div>
+            <div class="text-gray-600 dark:text-gray-300" role="alert">
+              No new notifications
+            </div>
+            <div class="text-sm text-gray-500 mt-1">
+              Check back later for updates
+            </div>
+          </div>
+
+          <div v-if="!loadingNotifications"
+              v-for="(notification, index) in notifications"
+              :key="notification.id"
+              class="mb-2 px-3 border-l-2"
+              :class="colors.getLeftBorderClass(index)">
+            <div class="flex gap-2 items-center">
+              <div class="font-bold flex-1 ">{{ notification.title }}</div>
+              <div class="text-sm text-gray-600 dark:text-gray-200">{{ timeUtils.formatDate(notification.notifiedOn, 'YYYY-MM-DD') }}</div>
+            </div>
+            <div class="flex pl-1 mt-2">
+              <div class="flex-1 ">
+                <markdown-text :text="notification.notification" data-cy="questionsText"
+                               :instance-id="`${notification.id}`"/>
+              </div>
+              <div>
+                <SkillsButton severity="danger" icon="fa-solid fa-xmark" size="small"
+                              @click="acknowledgeNotification(notification)"
+                              aria-label="Dismiss Notification"/>
+              </div>
+            </div>
+            <!--          <div class="text-right">-->
+            <!--            <SkillsButton size="small"  label="Learn More" />-->
+            <!--          </div>-->
+          </div>
         </div>
-      </div>
+      </ScrollPanel>
     </Popover>
   </div>
 </template>
 
-<style scoped>
-.skills-notification a {
-  text-decoration: underline !important;
+<style>
+.skills-notification .toastui-editor-contents a {
+  //text-decoration: underline !important;
 }
 </style>
