b312e13005c533440448caf225f37e2616d0a6e4
rmmayo <rmmayo@softtech-solutions.com>
2025-06-10 11:48:34 +0000

    #3353 - fixed userAchieved for Subjects and Badges

--- service/src/test/java/skills/intTests/clientDisplay/ClientDisplayTrainingSearchSpec.groovy
+++ service/src/test/java/skills/intTests/clientDisplay/ClientDisplayTrainingSearchSpec.groovy
@@ -54,7 +54,7 @@ class ClientDisplayTrainingSearchSpec extends DefaultIntSpec {
 
         Map badge = createBadge(1, 1)
         skillsService.createBadge(badge)
-        skillsService.assignSkillToBadge([projectId: p1.projectId, badgeId: badge.badgeId, skillId: p1Skills[0].skillId])
+        skillsService.assignSkillToBadge([projectId: p1.projectId, badgeId: badge.badgeId, skillId: p1Skills[3].skillId])
         badge.enabled = 'true'
         skillsService.createBadge(badge)
 
@@ -68,7 +68,7 @@ class ClientDisplayTrainingSearchSpec extends DefaultIntSpec {
         skills.pointIncrement.sort() == [2, 3, 4, 5, 6, 3, 4, 5, 6, 7, 0, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 0, 0].sort()
         skills.totalPoints.sort() == [4, 6, 8, 10, 12, 6, 8, 10, 12, 14, 0, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 1040, 1050].sort()
         skills.userCurrentPoints.sort() == [0, 6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0].sort()
-        skills.userAchieved == [false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+        skills.userAchieved == [false, true, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false]
         skills.skillId.sort() == ["skill3", "skill4", "skill5", "skill6", "skill7", "skill4subj2", "skill5subj2", "skill6subj2", "skill7subj2", "skill8subj2", "badge1", "skill1", "skill1subj2", "skill10", "skill10subj2", "skill2", "skill2subj2", "skill3subj2", "skill8", "skill9", "skill9subj2", "TestSubject1", "TestSubject2"].sort()
         skills.subjectId.sort() == ["TestSubject1", "TestSubject1", "TestSubject1", "TestSubject1", "TestSubject1", "TestSubject2", "TestSubject2", "TestSubject2", "TestSubject2", "TestSubject2", null, "TestSubject1", "TestSubject2", "TestSubject1", "TestSubject2", "TestSubject1", "TestSubject2", "TestSubject2", "TestSubject1", "TestSubject1", "TestSubject2", null, null].sort()
         skills.findAll { it.skillType == "Skill" }.skillId.sort() == ["skill3", "skill4", "skill5", "skill6", "skill7", "skill4subj2", "skill5subj2", "skill6subj2", "skill7subj2", "skill8subj2", "skill1", "skill1subj2", "skill10", "skill10subj2", "skill2", "skill2subj2", "skill3subj2", "skill8", "skill9", "skill9subj2"].sort()
@@ -78,8 +78,9 @@ class ClientDisplayTrainingSearchSpec extends DefaultIntSpec {
         skills.find { it.skillId == "TestSubject2" }.childAchievementCount == 0
         skills.find { it.skillId == "TestSubject2" }.totalChildCount == 10
         skills.findAll { it.skillType == "Badge" }.skillId == ["badge1"]
-        skills.find { it.skillId == "badge1" }.childAchievementCount == 0
+        skills.find { it.skillId == "badge1" }.childAchievementCount == 1
         skills.find { it.skillId == "badge1" }.totalChildCount == 1
+        skills.find { it.skillId == "badge1" }.userAchieved  == true
 
     }
 
