6ff92c40ab9c6dd5cccb5eaa500f2caa9903b374
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2025-07-28 11:23:35 -0400

    #3299 Update icon styling

--- dashboard/src/components/skills/SkillsTable.vue
+++ dashboard/src/components/skills/SkillsTable.vue
@@ -642,7 +642,7 @@ const onRowExpand = () => {
           <div v-if="slotProps.field == 'name'"
                class="flex flex-wrap items-center flex-col sm:flex-row"
                :data-cy="`nameCell_${slotProps.data.skillId}`">
-            <div class="rounded-border w-16 skill-icon-container text-primary border text-center mr-4">
+            <div class="d-inline-block rounded-border w-16 skill-icon-container text-primary text-center border text-center mr-4">
               <i class="skill-icon" :class="`${slotProps.data.iconClass ? slotProps.data.iconClass : 'fas fa-graduation-cap'}`"></i>
             </div>
             <div v-if="slotProps.data.isGroupType" class="flex-1">
@@ -941,12 +941,12 @@ const onRowExpand = () => {
 }
 
 .skill-icon {
-  height: 100%;
-  width: 100%;
+  height: 100% !important;
+  width: 100% !important;
   background-size: cover;
   background-position: center;
   font-size: 30px !important;
-  line-height: 46px;
+  line-height: 46px !important;
 }
 
 .skill-icon-container {
@@ -954,6 +954,5 @@ const onRowExpand = () => {
   max-height:48px;
   height:48px;
   width: 48px;
-  padding-top: 8px;
 }
 </style>
