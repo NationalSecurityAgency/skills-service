db291c92768765b8745d7a4e0750d3da357ee61a
rmmayo <rmmayo@softtech-solutions.com>
2025-09-08 19:19:58 +0000

    #3487 - additional UC tests for global badges

--- service/src/test/java/skills/intTests/community/globalBadge/UserCommunityGlobalBadgeAuthSpecs.groovy
+++ service/src/test/java/skills/intTests/community/globalBadge/UserCommunityGlobalBadgeAuthSpecs.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package skills.intTests.community.quiz
+package skills.intTests.community.globalBadge
 
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
