a90f4f682925332a88b233267cb07322821eb7bd
Dima May <sudo.may1@gmail.com>
2025-09-10 17:58:31 +0000

    fixed merge issue

--- service/src/test/java/skills/intTests/slides/SkillSlidesSizeAndTypeValidationIT.groovy
+++ service/src/test/java/skills/intTests/slides/SkillSlidesSizeAndTypeValidationIT.groovy
@@ -31,13 +31,10 @@ import static skills.intTests.utils.SkillsFactory.*
         'skills.config.ui.maxSlidesUploadSize=125KB',
         'skills.authorization.userInfoHealthCheckUri=https://localhost:8202/status',
         'skills.authorization.userInfoUri=https://localhost:8202/userInfo?dn={dn}',
-        'skills.authorization.userQueryUri=https://localhost:8202/userQuery?query={query}',
+        'skills.authorization.userQueryUri=https://localhost:8202/userQuery?query={query}'
 ], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SpringBootApp)
 class SlidesSizeAndTypeValidationIT extends DefaultIntSpec {
-@SpringBootTest(properties = ['skills.config.ui.maxSlidesUploadSize=125KB'], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SpringBootApp)
-class SkillSlidesSizeAndTypeValidationIT extends DefaultIntSpec {
-
-    def "cannot exceed max slide size for a skill" () {
+    def "cannot exceed max slide size for a skill"() {
         def p1 = createProject(1)
         def p1subj1 = createSubject(1, 1)
         def p1Skills = createSkills(1, 1, 1, 100)
@@ -45,17 +42,17 @@ class SkillSlidesSizeAndTypeValidationIT extends DefaultIntSpec {
 
         Resource slides44k = new ClassPathResource("/testSlides/test-slides-1.pdf")
         Resource slides160k = new ClassPathResource("/testSlides/test-slides-2.pdf")
-        skillsService.saveSlidesAttributes(p1.projectId, p1Skills[0].skillId, [ file: slides44k])
+        skillsService.saveSlidesAttributes(p1.projectId, p1Skills[0].skillId, [file: slides44k])
 
         when:
-        skillsService.saveSlidesAttributes(p1.projectId, p1Skills[0].skillId, [ file: slides160k])
+        skillsService.saveSlidesAttributes(p1.projectId, p1Skills[0].skillId, [file: slides160k])
 
         then:
         SkillsClientException skillsClientException = thrown()
         skillsClientException.message.contains("File size [157 KB] exceeds maximum file size [125 KB]")
     }
 
-    def "cannot exceed max slide size for a quiz" () {
+    def "cannot exceed max slide size for a quiz"() {
         def quiz = QuizDefFactory.createQuiz(1, "Fancy Description")
         skillsService.createQuizDef(quiz)
 
@@ -71,7 +68,7 @@ class SkillSlidesSizeAndTypeValidationIT extends DefaultIntSpec {
         skillsClientException.message.contains("File size [157 KB] exceeds maximum file size [125 KB]")
     }
 
-    def "validate supported mime types for skill" () {
+    def "validate supported mime types for skill"() {
         def p1 = createProject(1)
         def p1subj1 = createSubject(1, 1)
         def p1Skills = createSkills(1, 1, 1, 100)
@@ -79,29 +76,29 @@ class SkillSlidesSizeAndTypeValidationIT extends DefaultIntSpec {
 
         Resource mp4Type = new ClassPathResource("/testVideos/empty-quiz.mp4")
         Resource pdfType = new ClassPathResource("/testSlides/test-slides-1.pdf")
-        skillsService.saveSlidesAttributes(p1.projectId, p1Skills[0].skillId, [ file: pdfType])
+        skillsService.saveSlidesAttributes(p1.projectId, p1Skills[0].skillId, [file: pdfType])
 
         when:
-        skillsService.saveSlidesAttributes(p1.projectId, p1Skills[0].skillId, [ file: mp4Type])
+        skillsService.saveSlidesAttributes(p1.projectId, p1Skills[0].skillId, [file: mp4Type])
 
         then:
         SkillsClientException skillsClientException = thrown()
         skillsClientException.message.contains("Invalid media type [video/mp4]")
     }
 
-    def "validate supported mime types for quiz" () {
-        def quiz = QuizDefFactory.createQuiz(1, "Fancy Description")
-        skillsService.createQuizDef(quiz)
+    def "validate supported mime types for quiz"() {
+            def quiz = QuizDefFactory.createQuiz(1, "Fancy Description")
+            skillsService.createQuizDef(quiz)
 
-        Resource mp4Type = new ClassPathResource("/testVideos/empty-quiz.mp4")
-        Resource pdfType = new ClassPathResource("/testSlides/test-slides-1.pdf")
-        skillsService.saveQuizSlidesAttributes(quiz.quizId, [file: pdfType])
+            Resource mp4Type = new ClassPathResource("/testVideos/empty-quiz.mp4")
+            Resource pdfType = new ClassPathResource("/testSlides/test-slides-1.pdf")
+            skillsService.saveQuizSlidesAttributes(quiz.quizId, [file: pdfType])
 
-        when:
-        skillsService.saveQuizSlidesAttributes(quiz.quizId, [file: mp4Type])
+            when:
+            skillsService.saveQuizSlidesAttributes(quiz.quizId, [file: mp4Type])
 
-        then:
-        SkillsClientException skillsClientException = thrown()
-        skillsClientException.message.contains("Invalid media type [video/mp4]")
-    }
+            then:
+            SkillsClientException skillsClientException = thrown()
+            skillsClientException.message.contains("Invalid media type [video/mp4]")
+        }
 }
