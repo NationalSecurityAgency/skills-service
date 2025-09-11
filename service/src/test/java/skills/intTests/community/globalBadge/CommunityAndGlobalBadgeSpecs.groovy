9aedc9e0175d27e9c9d47b7bf323f09e83c35bc5
rmmayo <rmmayo@softtech-solutions.com>
2025-09-11 12:48:01 +0000

    #3487 - filter out available projects based on UC status of the badge & current user for

--- service/src/test/java/skills/intTests/community/globalBadge/CommunityAndGlobalBadgeSpecs.groovy
+++ service/src/test/java/skills/intTests/community/globalBadge/CommunityAndGlobalBadgeSpecs.groovy
@@ -39,10 +39,10 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
 
         def badge1 = createBadge(1)
         badge1.enableProtectedUserCommunity = false
-        rootUser.createGlobalBadge(badge1)
+        pristineDragonsUser.createGlobalBadge(badge1)
 
         when:
-        rootUser.assignSkillToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, skillId: p1Skills[0].skillId)
+        pristineDragonsUser.assignSkillToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, skillId: p1Skills[0].skillId)
         then:
         SkillsClientException e = thrown(SkillsClientException)
         e.message.contains("Projects with community protection can only be added to a Global Badge with community protection")
@@ -64,10 +64,10 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
 
         def badge1 = createBadge(1)
         badge1.enableProtectedUserCommunity = false
-        rootUser.createGlobalBadge(badge1)
+        pristineDragonsUser.createGlobalBadge(badge1)
 
         when:
-        rootUser.assignProjectLevelToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, level: "1")
+        pristineDragonsUser.assignProjectLevelToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, level: "1")
         then:
         SkillsClientException e = thrown(SkillsClientException)
         e.message.contains("Projects with community protection can only be added to a Global Badge with community protection")
@@ -89,10 +89,10 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
 
         def badge1 = createBadge(1)
         badge1.enableProtectedUserCommunity = true
-        rootUser.createGlobalBadge(badge1)
+        pristineDragonsUser.createGlobalBadge(badge1)
 
         when:
-        rootUser.assignSkillToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, skillId: p1Skills[0].skillId)
+        pristineDragonsUser.assignSkillToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, skillId: p1Skills[0].skillId)
         then:
         SkillsClientException e = thrown(SkillsClientException)
         e.message.contains("Projects without community protection can not be added to a Global Badge with community protection")
@@ -114,10 +114,10 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
 
         def badge1 = createBadge(1)
         badge1.enableProtectedUserCommunity = true
-        rootUser.createGlobalBadge(badge1)
+        pristineDragonsUser.createGlobalBadge(badge1)
 
         when:
-        rootUser.assignProjectLevelToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, level: "1")
+        pristineDragonsUser.assignProjectLevelToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, level: "1")
         then:
         SkillsClientException e = thrown(SkillsClientException)
         e.message.contains("Projects without community protection can not be added to a Global Badge with community protection")
@@ -242,11 +242,11 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
 
         def badge1 = createBadge(1, 1)
         def badge2 = createBadge(2, 2)
-        rootUser.createGlobalBadge(badge1)
-        rootUser.assignSkillToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, skillId: p1Skills[0].skillId)
+        pristineDragonsUser.createGlobalBadge(badge1)
+        pristineDragonsUser.assignSkillToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, skillId: p1Skills[0].skillId)
 
-        rootUser.createGlobalBadge(badge2)
-        rootUser.assignProjectLevelToGlobalBadge(projectId: p2.projectId, badgeId: badge2.badgeId, level: "1")
+        pristineDragonsUser.createGlobalBadge(badge2)
+        pristineDragonsUser.assignProjectLevelToGlobalBadge(projectId: p2.projectId, badgeId: badge2.badgeId, level: "1")
 
         p1.enableProtectedUserCommunity = true
         when:
@@ -276,11 +276,11 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
 
         def badge1 = createBadge(1, 1)
         def badge2 = createBadge(2, 2)
-        rootUser.createGlobalBadge(badge1)
-        rootUser.assignSkillToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, skillId: p1Skills[0].skillId)
+        pristineDragonsUser.createGlobalBadge(badge1)
+        pristineDragonsUser.assignSkillToGlobalBadge(projectId: p1.projectId, badgeId: badge1.badgeId, skillId: p1Skills[0].skillId)
 
-        rootUser.createGlobalBadge(badge2)
-        rootUser.assignProjectLevelToGlobalBadge(projectId: p2.projectId, badgeId: badge2.badgeId, level: "1")
+        pristineDragonsUser.createGlobalBadge(badge2)
+        pristineDragonsUser.assignProjectLevelToGlobalBadge(projectId: p2.projectId, badgeId: badge2.badgeId, level: "1")
 
         p2.enableProtectedUserCommunity = true
         when:
@@ -291,7 +291,7 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
         e.message.contains("This project is part of one or more Global Badges that has not enabled user community protection")
     }
 
-    def "available skills for global badges endpoint does NOT return skills from UC protected projects"() {
+    def "available skills endpoint for non UC badge does NOT return skills from UC protected projects"() {
         List<String> users = getRandomUsers(2)
 
         SkillsService pristineDragonsUser = createService(users[1])
@@ -309,6 +309,7 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
         pristineDragonsUser.createProjectAndSubjectAndSkills(p1, p1subj1, p1Skills)
 
         def p2 = createProject(2)
+        p2.enableProtectedUserCommunity = false
         def p2subj1 = createSubject(2, 1)
         def p2Skills = createSkills(3, 2, 1, 100, 5)
         p2Skills[0].skillId = "Skill1"
@@ -316,19 +317,16 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
         p2Skills[2].skillId = "Skill3"
         pristineDragonsUser.createProjectAndSubjectAndSkills(p2, p2subj1, p2Skills)
 
-        rootUser.pinProject(p1.projectId)
-        rootUser.pinProject(p2.projectId)
-
         def badge1 = createBadge(1)
-        def badge2 = createBadge(2)
-        rootUser.createGlobalBadge(badge1)
+        badge1.enableProtectedUserCommunity = false
+        pristineDragonsUser.createGlobalBadge(badge1)
 
         when:
-        def res = rootUser.getAvailableSkillsForGlobalBadge(badge1.badgeId, "")
-        def res1 = rootUser.getAvailableSkillsForGlobalBadge(badge1.badgeId, "2")
+        def res = pristineDragonsUser.getAvailableSkillsForGlobalBadge(badge1.badgeId, "")
+        def res1 = pristineDragonsUser.getAvailableSkillsForGlobalBadge(badge1.badgeId, "2")
 
-        def res2 = rootUser.getAvailableProjectsForGlobalBadge(badge1.badgeId, "")
-        def res3 = rootUser.getAvailableProjectsForGlobalBadge(badge1.badgeId, "1")
+        def res2 = pristineDragonsUser.getAvailableProjectsForGlobalBadge(badge1.badgeId, "")
+        def res3 = pristineDragonsUser.getAvailableProjectsForGlobalBadge(badge1.badgeId, "1")
         then:
         res.suggestedSkills.skillId.sort() == p2Skills.skillId.sort()
         res.totalAvailable == 3
@@ -337,6 +335,56 @@ class CommunityAndGlobalBadgeSpecs extends DefaultIntSpec {
         res1.totalAvailable == 1
 
         res2.projects.projectId == [p2.projectId]
+        res2.totalAvailable == 1
+
+        res3.projects == []
+        res3.totalAvailable == 0
+    }
+
+    def "available skills endpoint for UC badge does NOT return skills from non UC protected projects"() {
+        List<String> users = getRandomUsers(2)
+
+        SkillsService pristineDragonsUser = createService(users[1])
+        SkillsService rootUser = createRootSkillService()
+        rootUser.saveUserTag(pristineDragonsUser.userName, 'dragons', ['DivineDragon'])
+        rootUser.saveUserTag(rootUser.userName, 'dragons', ['DivineDragon'])
+
+        def p1 = createProject(1)
+        p1.enableProtectedUserCommunity = true
+        def p1subj1 = createSubject(1, 1)
+        def p1Skills = createSkills(3, 1, 1, 100, 5)
+        p1Skills[0].skillId = "ProtectedSkill1"
+        p1Skills[1].skillId = "ProtectedSkill2"
+        p1Skills[2].skillId = "ProtectedSkill3"
+        pristineDragonsUser.createProjectAndSubjectAndSkills(p1, p1subj1, p1Skills)
+
+        def p2 = createProject(2)
+        p2.enableProtectedUserCommunity = false
+        def p2subj1 = createSubject(2, 1)
+        def p2Skills = createSkills(3, 2, 1, 100, 5)
+        p2Skills[0].skillId = "Skill1"
+        p2Skills[1].skillId = "Skill2"
+        p2Skills[2].skillId = "Skill3"
+        pristineDragonsUser.createProjectAndSubjectAndSkills(p2, p2subj1, p2Skills)
+
+        def badge1 = createBadge(1)
+        badge1.enableProtectedUserCommunity = true
+        pristineDragonsUser.createGlobalBadge(badge1)
+
+        when:
+        def res = pristineDragonsUser.getAvailableSkillsForGlobalBadge(badge1.badgeId, "")
+        def res1 = pristineDragonsUser.getAvailableSkillsForGlobalBadge(badge1.badgeId, "2")
+
+        def res2 = pristineDragonsUser.getAvailableProjectsForGlobalBadge(badge1.badgeId, "")
+        def res3 = pristineDragonsUser.getAvailableProjectsForGlobalBadge(badge1.badgeId, "2")
+        then:
+        res.suggestedSkills.skillId.sort() == p1Skills.skillId.sort()
+        res.totalAvailable == 3
+
+        res1.suggestedSkills.skillId.sort() == [p1Skills[1].skillId]
+        res1.totalAvailable == 1
+
+        res2.projects.projectId == [p1.projectId]
         res2.totalAvailable == 1
 
         res3.projects == []
