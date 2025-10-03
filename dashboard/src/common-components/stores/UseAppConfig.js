27c52def4c2d7225cdbbe642d74863e05e35e9df
Dima May <sudo.may1@gmail.com>
2025-10-03 16:27:22 +0000

    wrapped up new strategy for performing paragraph validation; added service tests

--- dashboard/src/common-components/stores/UseAppConfig.js
+++ dashboard/src/common-components/stores/UseAppConfig.js
@@ -80,6 +80,7 @@ export const useAppConfig = defineStore('dashboardAppConfig', () => {
   const maxIdLength = computed(() => config.value.maxIdLength)
   const descriptionMaxLength = computed(() => config.value.descriptionMaxLength)
   const paragraphValidationRegex = computed(() => config.value.paragraphValidationRegex)
+  const addPrefixToInvalidParagraphsOptions = computed(() => config.value.addPrefixToInvalidParagraphsOptions)
   const formFieldDebounceInMs = computed(() => config.value.formFieldDebounceInMs || 400)
   const maxSubjectNameLength = computed(() => config.value.maxSubjectNameLength)
   const maxBadgeNameLength = computed(() => config.value.maxBadgeNameLength)
@@ -174,6 +175,7 @@ export const useAppConfig = defineStore('dashboardAppConfig', () => {
   const matomoSiteId = computed(() => config.value.matomoSiteId)
   const matomoProcessUserIdRegex = computed(() => config.value.matomoProcessUserIdRegex)
 
+
   return {
     loadConfigState,
     refreshConfig,
@@ -193,6 +195,7 @@ export const useAppConfig = defineStore('dashboardAppConfig', () => {
     descriptionMaxLength,
     formFieldDebounceInMs,
     paragraphValidationRegex,
+    addPrefixToInvalidParagraphsOptions,
     maxSubjectNameLength,
     maxBadgeNameLength,
     maxCustomLabelLength,
