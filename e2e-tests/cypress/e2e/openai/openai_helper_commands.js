dbba4a8047e4d6eb085d6a75dabe4cd9b3732ae8
Dima May <sudo.may1@gmail.com>
2025-10-16 18:23:07 +0000

    separated AI feedback into its own section after the description was changed

---
+++ e2e-tests/cypress/e2e/openai/openai_helper_commands.js
@@ -0,0 +1,9 @@
+const newDescWelcomeMsg = 'Welcome! I can help you craft a description.'
+const existingDescWelcomeMsg = 'I noticed you\'ve already started a description. I can help you refine and enhance it.'
+const chessGenValue = 'In order to learn chess you will need to get a chess board!'
+
+export {
+    newDescWelcomeMsg,
+    existingDescWelcomeMsg,
+    chessGenValue
+}
