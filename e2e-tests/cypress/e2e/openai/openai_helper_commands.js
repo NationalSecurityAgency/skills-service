ff44a1cf3743011fc2132d7ea4de9dfe4d950e55
Dima May <sudo.may1@gmail.com>
2025-10-16 19:30:31 +0000

    propagate UC to prefix controls; disabled ai prompt for email pages

--- e2e-tests/cypress/e2e/openai/openai_helper_commands.js
+++ e2e-tests/cypress/e2e/openai/openai_helper_commands.js
@@ -1,5 +1,5 @@
-const newDescWelcomeMsg = 'Welcome! I can help you craft a description.'
-const existingDescWelcomeMsg = 'I noticed you\'ve already started a description. I can help you refine and enhance it.'
+const newDescWelcomeMsg = 'Hi there! I\'m excited to help you craft something amazing'
+const existingDescWelcomeMsg = 'I noticed you\'ve already started and can help you refine and enhance!'
 const chessGenValue = 'In order to learn chess you will need to get a chess board!'
 
 export {
