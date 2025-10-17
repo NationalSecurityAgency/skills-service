528c46a6b4833ed634f4954abd5c35cdbad9a4ac
Dima May <sudo.may1@gmail.com>
2025-10-17 15:06:56 +0000

    initial take on a generic ai prompt to be reused in the descriptions and quizzes generations

--- e2e-tests/cypress/e2e/openai/openai_helper_commands.js
+++ e2e-tests/cypress/e2e/openai/openai_helper_commands.js
@@ -1,9 +1,16 @@
 const newDescWelcomeMsg = 'Hi there! I\'m excited to help you craft something amazing'
 const existingDescWelcomeMsg = 'I noticed you\'ve already started and can help you refine and enhance!'
 const chessGenValue = 'In order to learn chess you will need to get a chess board!'
-
+const completedMsg = 'Take a look at what I came up with!'
+const errMsg = 'I apologize, but I couldn\'t process your request at this time'
+const stopMsg = 'Successfully stopped.'
+const gotStartedMsg = 'Got it! I\'ll get started right away!'
 export {
     newDescWelcomeMsg,
     existingDescWelcomeMsg,
-    chessGenValue
+    chessGenValue,
+    completedMsg,
+    errMsg,
+    stopMsg,
+    gotStartedMsg
 }
