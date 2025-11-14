a0372082df1c7834e122802a03a3c1f0cceccc50
Dima May <sudo.may1@gmail.com>
2025-11-14 12:28:31 +0000

    #3592: migrated QuizAttemptsTimeChart to use Chart.js lib

--- dashboard/src/components/quiz/metrics/QuizAttemptsTimeChart.vue
+++ dashboard/src/components/quiz/metrics/QuizAttemptsTimeChart.vue
@@ -14,90 +14,117 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 <script setup>
-import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router';
-import { useNumberFormat } from "@/common-components/filter/UseNumberFormat.js";
+import {onMounted, ref} from 'vue'
+import {useRoute} from 'vue-router';
+import {useNumberFormat} from "@/common-components/filter/UseNumberFormat.js";
 import QuizService from '@/components/quiz/QuizService.js';
 import MetricsOverlay from '@/components/metrics/utils/MetricsOverlay.vue';
+import ChartDownloadControls from "@/components/metrics/common/ChartDownloadControls.vue";
+import Chart from "primevue/chart";
+import {useChartSupportColors} from "@/components/metrics/common/UseChartSupportColors.js";
+import dayjs from "dayjs";
 
 const route = useRoute()
 const numberFormat = useNumberFormat()
+const chartSupportColors = useChartSupportColors()
 
 const loading = ref(false);
 const hasData = ref(false);
-const numItems = ref(0);
-const series = ref([]);
+const chartData = ref({})
+const chartJsOptions = ref();
 
-const chartOptions = {
-  chart: {
-    height: 350,
-    type: 'line',
-    id: 'quizAttemptsTimeChart',
-    toolbar: {
-      show: true,
-      offsetY: -52,
-      autoSelected: 'zoom',
-      tools: {
-        pan: false,
-      },
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  stroke: {
-    curve: 'smooth',
-    colors: ['#28a745', '#008ffb'],
-  },
-  grid: {
-    padding: {
-      right: 30,
-      left: 20,
-    },
-  },
-  xaxis: {
-    type: 'datetime',
-  },
-  yaxis: {
-    min: 0,
-    labels: {
-      formatter(val) {
-        return numberFormat.pretty(val);
-      },
-    },
-    title: {
-      text: '# of Runs',
-    },
-  },
-  legend: {
-    position: 'top',
-  },
-}
-
-onMounted(()=> {        
+onMounted(()=> {
+  chartJsOptions.value = setChartOptions()
   loading.value = true;
   QuizService.getUsageOverTime(route.params.quizId)
       .then((res) => {
         hasData.value = res && res.length > 0;
-        numItems.value = hasData.value ? res.length : 0;
-        series.value = [{
-          data: res.map((item) => [item.value, item.count]),
-          name: 'Runs',
-        }];
+        const formatTimestamp = (timestamp) => dayjs(timestamp).format('YYYY-MM-DD')
+        chartData.value = {
+          datasets: [{
+            label: '# of Runs',
+            data: res.map((item) => {
+              return {x: formatTimestamp(item.value), y: item.count}
+            }),
+            cubicInterpolationMode: 'monotone',
+          }]
+        }
       })
       .finally(() => {
         loading.value = false;
       });
 })
+const setChartOptions = () => {
+  const colors = chartSupportColors.getColors()
+  const textColorSecondary = colors.textMutedColor
+  const surfaceBorder =  colors.contentBorderColor
+
+  return {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          unit: 'day'
+        },
+        ticks: {
+          color: textColorSecondary
+        },
+        grid: {
+          color: surfaceBorder,
+          drawOnChartArea: false  // Ensures no grid lines are drawn in the chart area
+        }
+      },
+      y: {
+        beginAtZero: true,
+        ticks: {
+          stepSize: 1,
+          color: textColorSecondary
+        },
+        grid: {
+          color: surfaceBorder
+        },
+        title: {
+          display: true,
+          text: '# of Runs',
+          color: textColorSecondary,
+          font: {
+            size: 12,
+            weight: '500'
+          },
+          padding: { left: 10, right: 5 }
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    }
+  };
+}
+
+const runsOverTimeChartRef = ref(null)
 </script>
 
 <template>
 
   <Card>
-    <template #title>Runs Over Time</template>
+    <template #header>
+      <SkillsCardHeader title="Runs Over Time">
+        <template #headerContent>
+          <chart-download-controls v-if="hasData" :vue-chart-ref="runsOverTimeChartRef" />
+        </template>
+      </SkillsCardHeader>
+    </template>
     <template #content>
-      <MetricsOverlay :loading="loading" :has-data="hasData && numItems > 1" no-data-msg="This chart needs at least 2 days worth of runs">
-        <apexchart type="line" width="100%" :height="chartOptions.chart.height" :options="chartOptions" :series="series"></apexchart>
+      <MetricsOverlay :loading="loading" :has-data="hasData" no-data-msg="This chart needs at least 2 days worth of runs">
+        <Chart ref="runsOverTimeChartRef"
+               type="line"
+               :data="chartData"
+               :options="chartJsOptions"
+               class="h-[30rem]" />
       </MetricsOverlay>
     </template>
   </Card>
