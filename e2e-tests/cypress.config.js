c7f4a0a8bd9bc1801363d15a210cad653fdcd036
Dima May <sudo.may1@gmail.com>
2025-11-25 20:26:36 +0000

    don't keep tests in memory

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   projectId: '7kivjf',
   requestTimeout: 10000,
   defaultCommandTimeout: 10000,
-  numTestsKeptInMemory: 5,
+  numTestsKeptInMemory: 0,  // Don't keep tests in memory
   chromeWebSecurity: false,
   retries: {
     runMode: 2,
