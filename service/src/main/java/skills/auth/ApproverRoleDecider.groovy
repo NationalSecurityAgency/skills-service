2639956243831af1d470a4dbc735fd64d5c3ed5c
rmmayo <rmmayo@softtech-solutions.com>
2025-12-05 17:47:56 +0000

    refactor AuthUtils and UserAuthService

--- service/src/main/java/skills/auth/ApproverRoleDecider.groovy
+++ service/src/main/java/skills/auth/ApproverRoleDecider.groovy
@@ -16,46 +16,20 @@
 package skills.auth
 
 import groovy.util.logging.Slf4j
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
+import skills.auth.AuthUtils.RequestAttributes
 import skills.storage.model.auth.UserRole
-
-import jakarta.servlet.http.HttpServletRequest
 
 @Slf4j
 @Component
 class ApproverRoleDecider {
-
-    boolean shouldGrantApproverRole(HttpServletRequest servletRequest, UserRole userRole) {
-        return isAllowedProject(servletRequest, userRole) &&
-                !AuthUtils.isDashboardActionsEndpoint(servletRequest) &&
+    static boolean shouldGrantApproverRole(RequestAttributes requestAttributes, UserRole userRole) {
+        return requestAttributes.isAllowedProject(userRole) &&
+                !requestAttributes.isDashboardActionsEndpoint() &&
                 (
-                        isSupportedHttpMethod(servletRequest)
-                        || isSelfReportAction(servletRequest)
-                        || isSelfReportEmailSubscriptionAction(servletRequest)
+                        requestAttributes.isSupportedHttpMethod()
+                        || requestAttributes.isSelfReportAction()
+                        || requestAttributes.isSelfReportEmailSubscriptionAction()
                 )
-    }
-
-    private boolean isAllowedProject(HttpServletRequest servletRequest, UserRole userRole) {
-        String projectId = AuthUtils.getProjectIdFromRequest(servletRequest)
-        return projectId && projectId.equalsIgnoreCase(userRole.projectId)
-    }
-
-    private boolean isSupportedHttpMethod(HttpServletRequest servletRequest) {
-        String method = servletRequest.method
-        boolean isGetApprovalConfEndpoint = AuthUtils.isSelfReportApproverConfEndpoint(servletRequest)
-        return method && method == HttpMethod.GET.toString() && !isGetApprovalConfEndpoint
-    }
-
-    private boolean isSelfReportAction(HttpServletRequest servletRequest) {
-        String method = servletRequest.method
-        boolean isRightMethod = method && (method == HttpMethod.POST.toString() || method == HttpMethod.PUT.toString())
-        return isRightMethod && AuthUtils.isSelfReportApproveOrRejectEndpoint(servletRequest)
-    }
-
-    private boolean isSelfReportEmailSubscriptionAction(HttpServletRequest servletRequest) {
-        String method = servletRequest.method
-        boolean isRightMethod = method && (method == HttpMethod.POST.toString() || method == HttpMethod.PUT.toString())
-        return isRightMethod && AuthUtils.isSelfReportEmailSubscriptionEndpoint(servletRequest)
     }
 }
