5e6c06985e7db4d6edcae9d2220a69d39e64478f
Dima May <sudo.may1@gmail.com>
2025-12-17 19:01:02 +0000

    clean up

--- dashboard/src/components/settings/SystemSettings.vue
+++ dashboard/src/components/settings/SystemSettings.vue
@@ -119,7 +119,7 @@ function loadSystemSettings() {
       </SkillsTextInput>
       <SkillsTextarea name="customHeader" label="Custom Header" />
       <SkillsTextarea name="customFooter" label="Custom Footer" />
-      <markdown-editor class="mt-8" name="userAgreement" label="User Agreement" :allow-attachments="false"/>
+      <markdown-editor class="mt-8" name="userAgreement" label="User Agreement" :allow-attachments="false" :disable-ai-prompt="true"/>
 
       <SkillsButton label="Save" :icon="isSaving ? 'fa fa-circle-notch fa-spin fa-3x-fa-fw' : 'fas fa-arrow-circle-right'"
                     data-cy="saveSystemSettings" @click="saveSystemSettings" :disabled="!meta.valid || !meta.dirty" />
