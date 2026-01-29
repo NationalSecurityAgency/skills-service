91489a6179855de23f1cf07807e1029cc7bb024f
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2026-01-29 12:45:46 -0500

    #3605 Modify selector

--- e2e-tests/cypress/e2e/rootUser_pinUnpin_spec.js
+++ e2e-tests/cypress/e2e/rootUser_pinUnpin_spec.js
@@ -396,7 +396,7 @@ describe('Root Pin and Unpin Tests', () => {
                 const tableSelector = '[data-cy=roleManagerTable]';
                 const rowSelector = `${tableSelector} tbody tr`;
                 cy.log('removing user');
-                cy.get(`${tableSelector} [data-cy="removeUserBtn"]`).eq(0).click();
+                cy.contains('td', 'root@skills.org').closest('tr').find('[data-cy="removeUserBtn"]').click();
 
                 cy.get('[data-cy="removalSafetyCheckMsg"]').contains('This will remove root@skills.org')
                 cy.get('[data-cy="currentValidationText"]').type('Delete Me', {delay: 0})
