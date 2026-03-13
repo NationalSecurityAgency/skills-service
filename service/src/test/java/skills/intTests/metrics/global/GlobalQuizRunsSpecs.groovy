d9217cd23ed6b9b3000279ae414e6654dda54b2b
rmmayo <rmmayo@softtech-solutions.com>
2026-03-13 13:22:37 +0000

    #1688 - renamed global metrics test package and added base class for reused data specs

--- service/src/test/java/skills/intTests/metrics/global/GlobalQuizRunsSpecs.groovy
+++ service/src/test/java/skills/intTests/metrics/global/GlobalQuizRunsSpecs.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package skills.intTests.metrics.globalUserProgress
+package skills.intTests.metrics.global
 
 import groovy.json.JsonOutput
 import org.springframework.beans.factory.annotation.Autowired
--- service/src/test/java/skills/intTests/metrics/global/GlobalUsersProgressSpecs.groovy
+++ service/src/test/java/skills/intTests/metrics/global/GlobalUsersProgressSpecs.groovy
@@ -13,13 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package skills.intTests.metrics.globalUserProgress
+package skills.intTests.metrics.global
 
-import groovy.transform.Canonical
+
 import skills.intTests.utils.*
-import skills.services.quiz.QuizQuestionType
-import skills.storage.model.auth.RoleName
-import spock.lang.IgnoreIf
 
 class GlobalUsersProgressSpecs extends DefaultIntSpec {
 
