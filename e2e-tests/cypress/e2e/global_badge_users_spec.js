1f33bd794cfe36520132902f73c49797b5cac11c
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2026-03-27 15:11:48 -0400

    #541 Add test for showing and hiding disabled badge users

--- e2e-tests/cypress/e2e/global_badge_users_spec.js
+++ e2e-tests/cypress/e2e/global_badge_users_spec.js
@@ -285,4 +285,21 @@ describe('Global Badge Users Tests', () => {
         cy.get(tableSelector).should('not.exist')
         cy.get('[data-cy="badgeNotActive"]').should('exist')
     })
+
+    it('Disabled badge does not show users table even with data', () => {
+        cy.createGlobalBadge(3)
+        cy.assignSkillToGlobalBadge(3, 1, 1);
+        cy.assignSkillToGlobalBadge(3, 2, 1);
+
+        cy.visit('/administrator/globalBadges/globalBadge3/users');
+
+        cy.get(tableSelector).should('not.exist')
+        cy.get('[data-cy="badgeNotActive"]').should('exist')
+
+        cy.enableGlobalBadge(3);
+
+        cy.visit('/administrator/globalBadges/globalBadge3/users');
+        cy.get('[data-cy="badgeNotActive"]').should('not.exist')
+        cy.get(tableSelector).should('exist')
+    })
 })
