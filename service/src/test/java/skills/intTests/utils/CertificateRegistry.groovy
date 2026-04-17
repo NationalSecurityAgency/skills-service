8dd2c86caaaa6e6fa5fb189e306f95aa636d48ef
Dima May <sudo.may1@gmail.com>
2026-04-17 16:12:27 +0000

    rolled back removing '-' from username

--- service/src/test/java/skills/intTests/utils/CertificateRegistry.groovy
+++ service/src/test/java/skills/intTests/utils/CertificateRegistry.groovy
@@ -100,7 +100,7 @@ class CertificateRegistry {
                 cn = subjectArray[0].split("=")[1]
             }
 
-            return cn?.replace('-', '')
+            return cn
         }
     }
 
