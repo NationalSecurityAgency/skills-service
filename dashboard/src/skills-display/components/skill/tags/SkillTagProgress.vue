2bf9da8cd401f9c29f813d73faef8c0582304c87
rmmayo <rmmayo@softtech-solutions.com>
2026-07-21 12:21:26 +0000

    #3962 - fixed issue with layout on small devices

--- dashboard/src/skills-display/components/skill/tags/SkillTagProgress.vue
+++ dashboard/src/skills-display/components/skill/tags/SkillTagProgress.vue
@@ -70,7 +70,7 @@ const iconClass = computed(() => {
             </div>
 
             <div class="flex-1 flex flex-col gap-2">
-              <div class="flex items-end gap-1">
+              <div class="flex flex-col md:flex-row md:items-end gap-2 md:gap-1">
                 <div class="flex-1">
                   <router-link
                       v-if="buildLink"
